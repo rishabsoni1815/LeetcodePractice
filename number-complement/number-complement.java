899592392f3d70591a1c7cd7cf14f82c7fb3bd2f
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2021-12-27 12:48:49 +0530

    Time: 0 ms (100.00%), Space: 35.6 MB (82.31%) - LeetHub

--- number-complement/number-complement.java
+++ number-complement/number-complement.java
@@ -1,11 +1,27 @@
 class Solution {
-    public int findComplement(int n) {
-      int x=1;
-        for(int i=0;i<=(int)(Math.log(n)/Math.log(2));i++){
-            // System.out.println(x+" "+n+" "+(int)(Math.log(n)/Math.log(2)));
-            n^=x;
-            x=x<<1;
+    // public int findComplement(int n) {
+    //   int x=1;
+    //     for(int i=0;i<=(int)(Math.log(n)/Math.log(2));i++){
+    //         n^=x;
+    //         x=x<<1;
+    //     }
+    //     return n;
+    // }
+    
+    // or by using fact that
+    // 100110, its complement is 011001, the sum is 111111. So we only need get the min number large or equal to num, then do substraction
+    
+    public int findComplement(int num) 
+    {
+        int i = 0;
+        int j = 0;
+        
+        while (i < num)
+        {
+            i += Math.pow(2, j);
+            j++;
         }
-        return n;
+        
+        return i - num;
     }
 }
