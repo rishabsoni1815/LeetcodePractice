992f1db42bdf38890ffca2c93a934333be100375
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-01-08 22:53:00 +0530

    Time: 0 ms (100.00%), Space: 35.9 MB (42.42%) - LeetHub

---
+++ 779-k-th-symbol-in-grammar/779-k-th-symbol-in-grammar.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int kthGrammar(int n, int k) {
+        if(n==1&&k==1) return 0;
+        if(k<=(int)(Math.pow(2,(n-2)))){
+            return kthGrammar(n-1,k);
+        }else{
+            return kthGrammar(n-1,k-(int)(Math.pow(2,(n-2)))) ^ 1;// xor with 1 as 0^1 is 0 ns 1^1 is 0 (to inverse)
+        }
+        
+        //                     1st half count    n
+        // 0                   0                 1
+        // 01                  1                 2
+        // 0110                2                 3
+        // 01101001            4                 4
+        // 0110100110010110    8                 5
+        //                     16                6
+    }
+}
