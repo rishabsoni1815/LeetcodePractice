6ccba36b7e3d795a9bbce0c7d28b438607f693e0
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-01-09 12:57:54 +0530

    Time: 1 ms (33.83%), Space: 38.8 MB (11.12%) - LeetHub

--- 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.java
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.java
@@ -2,6 +2,10 @@ class Solution {
     public boolean isRobotBounded(String s) {
         int n=s.length();
         int x=0,y=0;char dir='N';
+        // if after process same origin then true
+        // else if after operation dir is still north then he will never return
+        // else anything it is N->L ( lets say at last n changes to s) then in next S agin 
+        //changes to N and makes circle like this draw all 4 cases
         for(int i=0;i<n;i++){
             int c=s.charAt(i);
             if(c=='L'){
