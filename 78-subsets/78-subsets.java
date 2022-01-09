466f5a92824128b92164927b91d7b58eac77ab67
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-01-10 00:53:41 +0530

    Time: 2 ms (26.47%), Space: 40.1 MB (15.14%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -6,13 +6,14 @@ class Solution {
         h(a,0,n,ans,t);
         return ans;
     }
-     void h(int a[],int j,int n,List<List<Integer>>ans,List<Integer>t){
-       ans.add(new ArrayList<>(t));
-        for(int i=j;i<a.length;i++){
-            t.add(a[i]);
-            h(a,i+1,n,ans,t);
-            t.remove(t.size()-1);
-        }
-        
+     void h(int a[],int i,int n,List<List<Integer>>ans,List<Integer>t){
+        if(i>=n){
+            ans.add(new ArrayList<>(t));
+            return;
+        }//pick and non pick
+        t.add(a[i]);
+        h(a,i+1,n,ans,t);
+        t.remove(t.size()-1);
+        h(a,i+1,n,ans,t);
     }
 }
