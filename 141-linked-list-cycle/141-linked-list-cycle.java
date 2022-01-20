6e81eef8aa02bcded94e87509ac38c82b77bcf08
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-01-20 22:41:18 +0530

    Time: 1 ms (40.48%), Space: 42.8 MB (39.53%) - LeetHub

---
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }    https://www.youtube.com/watch?v=8NBkGnY5iJ8&list=PLBTCXiV4KOCRFRn0ZIoaNPX8TYHgbAVPZ&index=4&t=0s
+ * }
+ */
+public class Solution {
+    /*
+     let say when slow entern the cycle the distance between s and f is k after this f is +2 ans s is +1 so resultant dis b/w them is k+1 then k+2 ...k+x and one time k+x will be n where n is length of loop so they will meet as k is increasing by 1 always not by 2 or 3 so bount to become n (for some x k+x=n ) 
+    
+    */
+    public boolean hasCycle(ListNode head) {
+        ListNode t1=head,t2=head;
+        while(t1!=null&&t1.next!=null&&t1.next.next!=null){
+            t2=t2.next;
+            t1=t1.next.next;
+        if(t1==t2) return true;
+        }
+        return false;
+    }
+}
