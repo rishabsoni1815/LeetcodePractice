f6a4dc78e97654f82b6421a1b266992c1726c3cc
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-02-09 14:20:28 +0530

    Time: 25 ms (20.92%), Space: 47.5 MB (16.88%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.java
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.java
@@ -23,4 +23,36 @@ public class Solution {
         
         return count;
     }
+    /*
+    //one pass
+    
+    class Solution {
+    public int findPairs(int[] nums, int k) {
+        if (k < 0) {
+            return 0;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        int result = 0;
+        for (int i : nums) {
+            if (map.containsKey(i)) {
+                if (k == 0 && map.get(i) == 1) {
+                    result++;
+                }
+                map.put(i, map.get(i) + 1);
+            } else {
+                if (map.containsKey(i - k)) {
+                    result++;
+                }
+                if (map.containsKey(i + k)) {
+                    result++;
+                }
+                map.put(i, 1);
+            }
+        }
+        return result;
+    }
+}
+    
+    
+    */
 }
