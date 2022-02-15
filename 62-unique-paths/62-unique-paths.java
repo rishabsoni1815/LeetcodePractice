46be7588de4466742a6f79deae78dbdbfe240984
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-02-16 01:41:21 +0530

    Time: 1 ms (44.70%), Space: 40.7 MB (20.15%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int dp[][]=new int[m+1][n+1];
+        for(int i=m;i>0;i--){
+            for(int j=n;j>0;j--){
+                if(j==n||i==m){
+                    dp[i][j]=1;
+                }else{
+                    dp[i][j]=dp[i+1][j]+dp[i][j+1];
+                }
+            }
+        }
+        return dp[1][1];
+    }
+    /*
+     or as total down we have to take is m-1 (m is the no. of rows here) and as total right we have to take is n-1 (n is the no. of colm. here) which total is m-1+n-1= m+n-2 the answer can be (m+n-2) C (n-1) or (m+n-2) C (m-1) as out of m+n-2 directions i have to fix either n-1 then m-1 will make combinations or fix m-1 and n-1 will make diff combos , so (m+n-2) C (n-1) or (m+n-2) C (m-1)
+     
+     code--->
+      public int uniquePaths(int m, int n) {
+        int N=m+n-2;
+        int r=m-1;
+        double res=1;
+        for(int i=1;i<=r;i++){
+            res=res*(N-r+i)/i;// N-r+i as 10 C 3 =(8*9*10)/(1*2*3) so numerator is backwards not 8*9*10 so N-r+i -> for i=1 N-r=7 , 7+1=8 then 7+2=9, and so on
+        }
+        return (int)(res);
+    }
+     
+    */
+}
