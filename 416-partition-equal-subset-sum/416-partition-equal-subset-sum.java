5a03e8eca286e4f2ac7327a1b824db1860842e1a
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-02-18 01:13:05 +0530

    Time: 40 ms (67.17%), Space: 42.2 MB (71.98%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -1,19 +1,42 @@
 class Solution {
-    public boolean canPartition(int[] nums) {
+    // public boolean canPartition(int[] nums) {
+    //     int sum=0;
+    //     int n=nums.length;
+    //     for(int i:nums)sum+=i;
+    //     if(sum%2!=0) return false;
+    //     int target=sum/2;
+    //     boolean dp[][]=new boolean[n][target+1];
+    //     for(int i=0;i<n;i++) dp[i][0]=true;
+    //    if(nums[0]<=target) dp[0][nums[0]]=true;
+    //     for(int i=1;i<n;i++){
+    //         for(int j=0;j<=target;j++){
+    //             dp[i][j]=dp[i-1][j];
+    //             if(j>=nums[i]) dp[i][j]|=dp[i-1][j-nums[i]];
+    //         }
+    //     }
+    //     return dp[n-1][target];
+    // }
+    
+    
+    // can space as only i-1 required
+        public boolean canPartition(int[] nums) {
         int sum=0;
         int n=nums.length;
         for(int i:nums)sum+=i;
         if(sum%2!=0) return false;
         int target=sum/2;
-        boolean dp[][]=new boolean[n][target+1];
-        for(int i=0;i<n;i++) dp[i][0]=true;
-       if(nums[0]<=target) dp[0][nums[0]]=true;
+        boolean prev[]=new boolean[target+1];
+        // for(int i=0;i<n;i++) dp[i][0]=true; as this was so pre[0] would be true always
+       if(nums[0]<=target) prev[nums[0]]=true;
         for(int i=1;i<n;i++){
+        boolean cur[]=new boolean[target+1];
             for(int j=0;j<=target;j++){
-                dp[i][j]=dp[i-1][j];
-                if(j>=nums[i]) dp[i][j]|=dp[i-1][j-nums[i]];
+                if(j==0) prev[j]=true;
+                cur[j]=prev[j];
+                if(j>=nums[i]) cur[j]|=prev[j-nums[i]];
             }
+            prev=cur;
         }
-        return dp[n-1][target];
+        return prev[target];
     }
 }
