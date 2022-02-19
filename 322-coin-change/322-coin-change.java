154b7554d1a0837f1940eb3cb64ce524400c6e7a
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-02-20 00:15:07 +0530

    Time: 29 ms (50.80%), Space: 55.3 MB (11.63%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,46 +1,62 @@
 class Solution {
+    
+    /*
+    memoisation
+    
     public int coinChange(int[] coins, int amount) {
         int dp[][]=new int[amount+1][coins.length+1];
         for(int i=0;i<dp.length;i++){
             for(int j=0;j<dp[i].length;j++)dp[i][j]=-1;
         }
-          int minCoins = memoization(coins, amount, coins.length ,dp);
+          int minCoins = memoization(coins, amount, coins.length-1 ,dp);
         return minCoins == Integer.MAX_VALUE - 1 ? -1 : minCoins;    
     }
     int memoization(int wt[], int w, int n,int dp[][])
     {
-        if (n == 0 || w == 0)
-            return (w == 0) ? 0 : Integer.MAX_VALUE - 1;
+        if(n==0){
+            if(w%wt[n]==0){
+                return w/wt[n];
+            }else{
+                return Integer.MAX_VALUE-1;
+            }
+        }
         
         if (dp[w][n] != -1)
             return dp[w][n];
 			
-        if (wt[n - 1] > w) 
+        if (wt[n] > w) 
             return dp[w][n] = 0 + memoization(wt, w - 0, n - 1,dp);
         else 
-            return dp[w][n] = Math.min(0 + memoization(wt, w - 0, n - 1,dp), 1 + memoization(wt, w - wt[n - 1], n,dp));
+            return dp[w][n] = Math.min(0 + memoization(wt, w - 0, n - 1,dp), 1 + memoization(wt, w - wt[n], n,dp));
     }
-    /*
-     saving space as only calling dp[][n-1] and dp[][n]
-     public int coinChange(int[] c, int t) {
-         int n=c.length;
-        int dp[][]=new int[2][t+1];
-        dp[0][0]=0;
-        for(int i=1;i<=t;i++){
-            dp[0][i]=Integer.MAX_VALUE-1;
-        }
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=t;j++){
-                dp[i%2][0]=0;
-                if(c[i-1]<=j){
-                    dp[i%2][j]=Math.min(1+dp[i%2][j-c[i-1]],dp[(i+1)%2][j]);
-                }else{
-                     dp[i%2][j]=dp[(i+1)%2][j];
+    */
+    
+    
+    public int coinChange(int[] coins, int amount) {
+        int dp[][]=new int[coins.length][amount+1];
+        for(int i=0;i<=amount;i++){
+            if(i%coins[0]==0){
+                dp[0][i]=i/coins[0];
+            }else{
+                dp[0][i]=Integer.MAX_VALUE-1;// as 1+ Int_max mmay be error
+            }
+        }   
+        for(int i=1;i<coins.length;i++){
+            for(int j=0;j<=amount;j++){
+                int notTake=0+dp[i-1][j];
+                int take=Integer.MAX_VALUE;
+                if(coins[i]<=j){
+                    take=1+dp[i][j-coins[i]];
                 }
+                dp[i][j]=Math.min(notTake,take);
             }
         }
-        return dp[n%2][t]==Integer.MAX_VALUE-1?-1:dp[n%2][t];
+        if(dp[coins.length-1][amount]==Integer.MAX_VALUE-1){
+            return -1;
+        }else{
+            return dp[coins.length-1][amount];
+        }
     }
     
-    */
+    
 }
