db3ad2b390bc573703c62449dea9cbf6f5171827
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-02-20 01:26:52 +0530

    Time: 13 ms (41.07%), Space: 51.3 MB (13.05%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.java
+++ 518-coin-change-2/518-coin-change-2.java
@@ -1,4 +1,6 @@
 class Solution {
+    /*
+    memoisation
     public int change(int amount, int[] coins) {
     int dp[][]=new int[amount+1][coins.length+1];
         for(int i=0;i<dp.length;i++){
@@ -25,4 +27,30 @@ class Solution {
         else 
             return dp[w][n] = memoization(wt, w - 0, n - 1,dp)+ memoization(wt, w - wt[n], n,dp);
     }
+    */
+    
+    // tablutaion o(n*m) space and time
+    public int change(int amount, int[] coins) {
+   int dp[][]=new int[coins.length][amount+1];
+        for(int i=0;i<=amount;i++){
+            if(i%coins[0]==0){
+                dp[0][i]=1;
+            }else{
+                dp[0][i]=0;
+            }
+        }   
+        for(int i=1;i<coins.length;i++){
+            for(int j=0;j<=amount;j++){
+                int notTake=0+dp[i-1][j];
+                int take=0;
+                if(coins[i]<=j){
+                    take=dp[i][j-coins[i]];
+                }
+                dp[i][j]=notTake+take;
+            }
+        }
+        return dp[coins.length-1][amount];
+    }
+    
+    
 }
