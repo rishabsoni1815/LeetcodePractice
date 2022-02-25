b4bfcb3dd230f4a965d88b2a03c0edeaeb801331
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-02-26 02:34:19 +0530

    Time: 0 ms (100.00%), Space: 43.1 MB (14.52%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.java
+++ 162-find-peak-element/162-find-peak-element.java
@@ -6,7 +6,7 @@ public class Solution {
             int mid = (l + r) / 2;
             if(mid>0&&mid<nums.length-1){
                 if(nums[mid]>nums[mid-1]&&nums[mid]>nums[mid+1]) return mid;
-                else if(nums[mid-1]>nums[mid]){//coz if on the left the arr. is increasing then there are more chances that we will find a peak so r=mid-1
+                else if(nums[mid-1]>nums[mid]){//coz if on the left the arr. is increasing then there are more chances that we will find a peak so r=mid-1 as at last nums[-1] = nums[n] = -∞. will be true so will make a[0] as peak if we keep moving left by the cond. that on left array is inc. so there is chance of getting peak on left so do r=mid-1
                   r=mid-1;
                 }else{
                   l=mid+1;
