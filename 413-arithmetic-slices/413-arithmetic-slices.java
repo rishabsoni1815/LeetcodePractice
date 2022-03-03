4854a56a89312de19a2804270ed1c2a66493e5b9
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-03-03 12:42:47 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (32.16%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.java
+++ 413-arithmetic-slices/413-arithmetic-slices.java
@@ -1,17 +1,37 @@
 class Solution {
-    public int numberOfArithmeticSlices(int[] a) {
-        int n=a.length;
-        if(n<=2) return 0;
-        int dp[]=new int[n];
-        for(int i=2;i<n;i++){
-            dp[i]=dp[i-1];
-            int c=1,diff=a[i]-a[i-1];
-            for(int j=i;j>0;j--){
-                if(a[j]-a[j-1] == diff) c++;
-                else break;
-            }
-            if(c>=3) dp[i]+=(c-2);
+    //o(n^2)
+    // public int numberOfArithmeticSlices(int[] a) {
+    //     int n=a.length;
+    //     if(n<=2) return 0;
+    //     int dp[]=new int[n];
+    //     for(int i=2;i<n;i++){
+    //         dp[i]=dp[i-1];
+    //         int c=1,diff=a[i]-a[i-1];
+    //         for(int j=i;j>0;j--){
+    //             if(a[j]-a[j-1] == diff) c++;
+    //             else break;
+    //         }
+    //         if(c>=3) dp[i]+=(c-2);
+    //     }
+    //     return dp[n-1];
+    // }
+    
+   // o(n)
+    /*
+      // if A[i-2], A[i-1], A[i] are arithmetic, then the number of arithmetic slices ending with A[i] (dp[i]) equals to: the number of arithmetic slices ending with A[i-1] (dp[i-1], all these arithmetic slices appending A[i] are also arithmetic) + A[i-2], A[i-1], A[i] (a brand new arithmetic slice) so  dp[i] = dp[i-1] + 1 
+    
+    */
+    public int numberOfArithmeticSlices(int[] A) {
+       int []dp=new int[A.length];//dp[i] -> no of subarrays ending at i
+    int count=0;    
+    for(int i=2;i<A.length;i++)
+    {
+        if(A[i] - A[i-1] == A[i-1] - A[i-2])
+        {
+           dp[i]=dp[i-1]+1;       
+           count+=dp[i]; 
         }
-        return dp[n-1];
+    }   
+     return count;  
     }
 }
