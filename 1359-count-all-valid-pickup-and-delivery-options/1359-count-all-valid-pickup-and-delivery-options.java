55ddc8dc17be3b6bcd4727791ad914e101b76028
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-03-06 15:59:13 +0530

    Time: 295 ms (5.37%), Space: 48.5 MB (18.22%) - LeetHub

---
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
@@ -0,0 +1,17 @@
+class Solution {
+    int mod=(int) 1e9+7;
+    public int countOrders(int n) {
+        long dp[][]=new long[n+1][n+1];
+        for(int i=0;i<=n;i++)Arrays.fill(dp[i],-1);
+       return (int)help(n,n,dp)%mod;
+    }
+    long help(int up,int ud,long dp[][]){
+        if(up==0 && ud==0) return 1;
+        if(up<0 || ud<0) return 0;
+        if(dp[up][ud]!=-1) return dp[up][ud];
+        long pick=(up%mod*help(up-1,ud,dp)%mod)%mod;
+        
+        long deliver=((ud-up)%mod*help(up,ud-1,dp)%mod)%mod;
+        return dp[up][ud]=(pick%mod+deliver%mod)%mod;
+    }
+}
