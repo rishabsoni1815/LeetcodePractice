5dbeb86c9c5d248ed74d14922a72d21fb762d96b
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-03-21 16:47:34 +0530

    Time: 9 ms (34.97%), Space: 43.4 MB (12.38%) - LeetHub

--- 763-partition-labels/763-partition-labels.java
+++ 763-partition-labels/763-partition-labels.java
@@ -5,12 +5,12 @@ class Solution {
         int j=-1;
         int n=s.length();
         for(int i=0;i<n;i++){
-            last.put(s.charAt(i),i);
+            last.put(s.charAt(i),i);//remebering the last occurance of a char so that when we encounter it then we can ask will it come in future if yes then continue else add the ans to list 
         }
         int start=0;
         for(int i=0;i<n;i++){
             char c=s.charAt(i);
-            j=Math.max(j,last.get(c));
+            j=Math.max(j,last.get(c));//this is before the check if i==j as for a case ababcbacadefegehijhklij d only occurs once so we have to update it before checking i==j
             if(i==j) {
                 ans.add(j-start+1);
                 j=-1;
