85034fe62954ba11dec9ff0783c1ae19226e2c40
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-03-27 09:42:25 +0530

    Time: 96 ms (20.00%), Space: 51.5 MB (100.00%) - LeetHub

---
+++ 2217-find-palindrome-with-fixed-length/2217-find-palindrome-with-fixed-length.java
@@ -0,0 +1,34 @@
+class Solution {
+    public long[] kthPalindrome(int[] a, int l) {
+        int n=a.length;
+        long x=1;
+        if(l%2==0){
+        for(int i=0;i<l/2-1;i++){
+            x=x*10+0;
+        }
+        }else{
+        for(int i=0;i<l/2;i++){
+            x=x*10+0;
+        }
+        }
+        long h=(x*10 - x);
+        long ans[]=new long[n];
+        // System.out.println(h);
+        for(int i=0;i<n;i++){
+            if(a[i]>h) {
+                ans[i]=-1;
+                continue;
+            }
+            String g=String.valueOf(a[i]+x-1);
+            // System.out.println(g);
+            int j=g.length()-1;
+            if(l%2!=0){
+                j--;
+            }
+            for(;j>=0;j--) g+=g.charAt(j);
+            // System.out.println(g);
+            ans[i]=Long.valueOf(g);
+        }
+        return ans;
+    }
+}
