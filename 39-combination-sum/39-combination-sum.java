9610ad0975ac48c8119dfcb8337ea35f2d1d0b8c
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-05-21 16:44:35 +0530

    Time: 4 ms (76.96%), Space: 42.3 MB (95.55%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,19 +1,20 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] a, int t) {
+    public List<List<Integer>> combinationSum(int[] a, int k) {
         List<List<Integer>>ans=new ArrayList<>();
-        List<Integer>cur=new ArrayList<>();
-        help(ans,0,t,a,cur);
+        List<Integer>t=new ArrayList<>();
+        help(ans,t,0,k,a);
         return ans;
     }
-    void help(List<List<Integer>>ans,int i,int t,int a[],List<Integer>cur){
-        if(i>=a.length) return;
-        if(t==0){
-            ans.add(new ArrayList<>(cur));
+    void help(List<List<Integer>>ans,List<Integer>t,int i,int sum,int a[]){
+        if(sum==0 && i==a.length){
+            ans.add(new ArrayList<>(t));
             return;
         }
-        help(ans,i+1,t,a,cur);
-        cur.add(a[i]);
-        if(t-a[i]>=0) help(ans,i,t-a[i],a,cur);
-        cur.remove(cur.size()-1);
+        if(sum<0 || i>=a.length) return;
+        
+        t.add(a[i]);
+        help(ans,t,i,sum-a[i],a);
+        t.remove(t.size()-1);
+        help(ans,t,i+1,sum,a);
     }
 }
