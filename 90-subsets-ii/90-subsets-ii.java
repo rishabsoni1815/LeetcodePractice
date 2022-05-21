27bd19a00f79a758c5d52fafe015d551f80206c2
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-05-21 16:32:43 +0530

    Time: 2 ms (75.89%), Space: 44.4 MB (23.86%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -1,21 +1,19 @@
 class Solution {
-    public List<List<Integer>> subsetsWithDup(int[] nums) {
-         Arrays.sort(nums);        
-        List<List<Integer>> ans=new ArrayList<>();
-        List<Integer>b=new ArrayList<>();
-        f(ans,b,nums,0);
-        return ans;
+    public List<List<Integer>> subsetsWithDup(int[] a) {
+         Arrays.sort(a);
+         List<List<Integer>>ans=new ArrayList<>();
+         List<Integer>t=new ArrayList<>();
+         help(ans,t,0,a);
+         return ans;
     }
-    void f(List<List<Integer>>ans,List<Integer>t,int a[],int j){
+    void help(List<List<Integer>>ans,List<Integer>t,int i,int a[]){
         ans.add(new ArrayList<>(t));
-        for(int i=j;i<a.length;i++){
-            if(i!=j&&a[i]==a[i-1]){
-                continue;
+        for(int j=i;j<a.length;j++){//taking next as only one of all 2's present 
+            if(j==i || (a[j]!=a[j-1])){
+                t.add(a[j]);
+                help(ans,t,j+1,a);
+                t.remove(t.size()-1);
             }
-            t.add(a[i]);
-            f(ans,t,a,i+1);
-            t.remove(t.size()-1);
         }
-        
     }
 }
