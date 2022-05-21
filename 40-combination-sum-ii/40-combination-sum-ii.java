cfbdc0b81596811ba5d82fa219cef49647845236
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-05-21 17:25:01 +0530

    Time: 11 ms (14.19%), Space: 43.1 MB (72.06%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.java
+++ 40-combination-sum-ii/40-combination-sum-ii.java
@@ -1,22 +1,22 @@
 class Solution {
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        Arrays.sort(candidates);
+    public List<List<Integer>> combinationSum2(int[] a, int k) {
         List<List<Integer>>ans=new ArrayList<>();
         List<Integer>t=new ArrayList<>();
-        f(ans,t,candidates,0,target);
+        Arrays.sort(a);
+        help(ans,t,0,k,a);
         return ans;
     }
-   void f(List<List<Integer>>ans,List<Integer>t,int a[],int j,int s){
-        if(s==0){
-           ans.add(new ArrayList<>(t));
-            return;
-        }else if(s<0) return ;
-        for(int i=j;i<a.length;i++){
-            if(i!=j&&a[i]==a[i-1]) continue;
-              t.add(a[i]);
-              f(ans,t,a,i+1,s-a[i]);
-              t.remove(t.size()-1);
-          }
-           // f(ans,t,a,i+1,s);///not doing as in for loop we are checking every possibility // see subsetsII question and tuf video
+   void help(List<List<Integer>>ans,List<Integer>t,int i,int sum,int a[]){
+        if(sum==0) {
+            ans.add(new ArrayList<>(t));
         }
+       if(sum<0) return;
+        for(int j=i;j<a.length;j++){
+            if(j==i || (a[j]!=a[j-1])){
+                t.add(a[j]);
+                help(ans,t,j+1,sum-a[j],a);
+                t.remove(t.size()-1);
+            }
+        }
+    }
 }
