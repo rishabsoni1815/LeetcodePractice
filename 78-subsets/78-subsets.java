e882d636c1caf1982310a9fac6a403dbdc975957
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-05-21 15:36:27 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (95.39%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,19 +1,36 @@
 class Solution {
+    //backtracking
+    // public List<List<Integer>> subsets(int[] a) {
+    //     int n=a.length;
+    //     List<List<Integer>>ans=new ArrayList<>();
+    //     List<Integer>t=new ArrayList<>();
+    //     help(ans,t,0,a);
+    //     return ans;
+    // }
+    // void help(List<List<Integer>>ans,List<Integer>t,int i,int a[]){
+    //     if(i>=a.length){
+    //         ans.add(new ArrayList<>(t));
+    //         return;
+    //     }
+    //     t.add(a[i]);
+    //     help(ans,t,i+1,a);
+    //     t.remove(t.size()-1);
+    //     help(ans,t,i+1,a);
+    // }
+    
+    //bits
     public List<List<Integer>> subsets(int[] a) {
         int n=a.length;
         List<List<Integer>>ans=new ArrayList<>();
-        List<Integer>t=new ArrayList<>();
-        help(ans,t,0,a);
-        return ans;
-    }
-    void help(List<List<Integer>>ans,List<Integer>t,int i,int a[]){
-        if(i>=a.length){
-            ans.add(new ArrayList<>(t));
-            return;
+        for(int i=0;i<(Math.pow(2,n));i++){
+            List<Integer>t=new ArrayList<>();
+            for(int j=0;j<n;j++){
+                if(((1<<j)&i)>0){
+                    t.add(a[j]);
+                }
+            }
+            ans.add(t);
         }
-        t.add(a[i]);
-        help(ans,t,i+1,a);
-        t.remove(t.size()-1);
-        help(ans,t,i+1,a);
+        return ans;
     }
 }
