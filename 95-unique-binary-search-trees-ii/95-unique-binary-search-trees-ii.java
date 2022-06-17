7d85cda33c53008c3d393bf9a6dbee5b23a4df8b
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-06-17 17:22:57 +0530

    Time: 12 ms (5.22%), Space: 45.5 MB (70.99%) - LeetHub

--- 95-unique-binary-search-trees-ii/95-unique-binary-search-trees-ii.java
+++ 95-unique-binary-search-trees-ii/95-unique-binary-search-trees-ii.java
@@ -13,19 +13,52 @@
  *     }
  * }
  */
+
+
+//recurssive but i,j may repeat
+
+// class Solution {
+//     public List<TreeNode> generateTrees(int n) {
+//        return help(1,n);
+//     }
+//     List<TreeNode> help(int i,int j){
+//         List<TreeNode>ans=new ArrayList<>();
+//         if(i>j){
+//             ans.add(null);
+//             return ans;
+//         }
+//         for(int k=i;k<=j;k++){
+//             List<TreeNode>left=help(i,k-1);
+//             List<TreeNode>right=help(k+1,j);
+//             for(TreeNode l:left){
+//                 for(TreeNode r:right){
+//                     TreeNode root=new TreeNode(k);
+//                     root.left=l;root.right=r;
+//                     ans.add(root);
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
+//dp memoising i and j values
 class Solution {
     public List<TreeNode> generateTrees(int n) {
-       return help(1,n);
+        HashMap<String,List<TreeNode>>dp=new HashMap<>();
+       return help(1,n,dp);
     }
-    List<TreeNode> help(int i,int j){
+    List<TreeNode> help(int i,int j,HashMap<String,List<TreeNode>>dp){
         List<TreeNode>ans=new ArrayList<>();
         if(i>j){
             ans.add(null);
             return ans;
         }
+        String cur=i+"-"+j;
+        if(dp.containsKey(cur)) return dp.get(cur);
         for(int k=i;k<=j;k++){
-            List<TreeNode>left=help(i,k-1);
-            List<TreeNode>right=help(k+1,j);
+            List<TreeNode>left=help(i,k-1,dp);
+            List<TreeNode>right=help(k+1,j,dp);
             for(TreeNode l:left){
                 for(TreeNode r:right){
                     TreeNode root=new TreeNode(k);
@@ -34,6 +67,7 @@ class Solution {
                 }
             }
         }
+        dp.put(cur,ans);
         return ans;
     }
 }
