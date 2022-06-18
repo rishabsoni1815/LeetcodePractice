7c9629d8204f93689eb4873513c6481e89373990
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-06-18 19:36:08 +0530

    Time: 0 ms (100.00%), Space: 43.8 MB (29.00%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.java
+++ 968-binary-tree-cameras/968-binary-tree-cameras.java
@@ -15,15 +15,20 @@ class Solution {
     {
         if(root == null)
             return 0;
+        
+        //post order traversal as then only have to get idea of childs and pass info to parent
         int left = getCamera(root.left);
         int right = getCamera(root.right);
-        if(left==0 && right == 0) 
+        
+        if(left==0 && right == 0) //need a camera
             return -1;
-        if(left == -1 || right == -1)
+        
+        if(left == -1 || right == -1)//child need camera so i will take camera as always wise to five parent camera rather than child as parent can cover both childs
         {
             camera++;
             return 1;
         }
-        return 0;
+        
+        return 0;//i am in range of a camera ( left==1 || right==1)
     }
 }
