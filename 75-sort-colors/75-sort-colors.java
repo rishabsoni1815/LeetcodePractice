87fbde64c7193a2a2c9c28e30ce521a02869c020
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-06-26 23:31:31 +0530

    Time: 1 ms (38.73%), Space: 41.9 MB (77.87%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,7 +1,8 @@
 class Solution {
     public void sortColors(int[] a) {
-        int n=a.length,i=0,j=n-1,m=0,k=0;
-        while(k<=j){
+        // make all zeroes come to start and all 2's to go at end one will come to there correct position automatically by using 3 pointers start=0,mid=0,end=n-1 when a[mid] is 0 swap with start and dpo mid++,if it is 1 do mid++,if it is 2 swap with end and do end--
+        int n=a.length,i=0,j=n-1,k=0;
+        while(k<=j){//as then it will replace all j's with 1 run tc then see
             if(a[k]==0){
                 int t=a[k];
                 a[k]=a[i];
