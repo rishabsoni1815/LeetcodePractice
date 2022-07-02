f9002d10a7310b6c3bbb90df1587f60885f4189e
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-07-02 23:28:45 +0530

    Time: 16 ms (90.58%), Space: 49.4 MB (80.34%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.java
+++ 115-distinct-subsequences/115-distinct-subsequences.java
@@ -1,22 +1,19 @@
 class Solution {
     public int numDistinct(String s, String t) {
-        int n=s.length(),m=t.length();
-        int dp[][]=new int[n][m];
-        for(int i=0;i<n;i++) Arrays.fill(dp[i],-1);
-        help(s,t,0,0,dp);
-        return dp[0][0];
+        int n=s.length();
+        int m=t.length();
+        Integer dp[][]=new Integer[n][m];
+       return help(n-1,m-1,s,t,dp);
     }
-    int help(String s,String t,int i,int j,int dp[][]){
-        if(j==t.length()){
-            return 1;
-        }
-        if(i>=s.length()||j>=t.length()) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
+    int help(int i,int j,String s,String t,Integer dp[][]){
+        if(j<0) return 1;
+        if(i<0) return 0;
         int x=0;
+        if(dp[i][j]!=null) return dp[i][j];
         if(s.charAt(i)==t.charAt(j)){
-            x+=help(s,t,i+1,j+1,dp);
+            x+=help(i-1,j-1,s,t,dp);
         }
-            x+=help(s,t,i+1,j,dp);
+            x+=help(i-1,j,s,t,dp);
         return dp[i][j]=x;
     }
 }
