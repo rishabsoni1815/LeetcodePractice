b651d9f084483e8dc60406b8ff667edc0c2bf8b0
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-07-03 21:25:08 +0530

    Time: 46 ms (45.68%), Space: 54.7 MB (11.37%) - LeetHub

---
+++ 44-wildcard-matching/44-wildcard-matching.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int n=s.length(),m=p.length();
+        Boolean dp[][]=new Boolean[n][m];
+        return help(n-1,m-1,s,p,dp);
+    }
+    boolean help(int i,int j,String s,String p,Boolean dp[][]){
+        if (i < 0 && j < 0)
+          return true;
+        if (j < 0 && i >= 0)
+          return false;
+        if (i < 0 && j >= 0)
+          return isAllStars(p,j);
+        if(dp[i][j]!=null) return dp[i][j];
+        if(s.charAt(i)==p.charAt(j) || p.charAt(j)=='?'){
+            return dp[i][j]=help(i-1,j-1,s,p,dp);
+        }else{
+            if(p.charAt(j)=='*'){
+                return dp[i][j]=help(i-1,j,s,p,dp)||help(i,j-1,s,p,dp);
+            }else{
+                return dp[i][j]=false;
+            }
+        }
+    }
+    boolean isAllStars(String S1, int i) {
+        for (int j = 0; j <= i; j++) {
+          if (S1.charAt(j) != '*')
+            return false;
+        }
+        return true;
+    }
+}
