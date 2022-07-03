c5fed5f570ce460a29483f5b091ca2d927c3ce13
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-07-03 23:48:28 +0530

    Time: 476 ms (5.03%), Space: 151.9 MB (5.14%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,12 +1,27 @@
 class Solution {
-   public int maxProfit(int[] prices) {
-		int sell1 = 0, sell2 = 0, buy1 = Integer.MIN_VALUE, buy2 = Integer.MIN_VALUE;
-		for (int i = 0; i < prices.length; i++) {
-			buy1 = Math.max(buy1, -prices[i]);//taking max as -ve's min is max (we want to buy at as max as possible)
-		    sell1 = Math.max(sell1, buy1 + prices[i]);
-			buy2 = Math.max(buy2, sell1 - prices[i]);//now taking max as now buy2 has our 1st trans.'s profit included
-			sell2 = Math.max(sell2, buy2 + prices[i]);
-		}
-		return sell2;
-	}
+    public int maxProfit(int[] a) {
+        int n=a.length;
+        Integer dp[][][]=new Integer[n][2][3];
+        return help(0,a,1,2,n,dp);//at 0 will sell and not n-1 as we don't know that everytime he will sell only at n-1 as can only do 1 trans and but at n-1
+    }
+    int help(int i,int a[],int buy,int transLeft,int n,Integer dp[][][]){
+        if(transLeft==0 || i==n) return 0;
+        if(dp[i][buy][transLeft]!=null) return dp[i][buy][transLeft];
+        
+        //buy
+        if(buy==1){// buy
+            //buying at this index
+            int b=-a[i]+help(i+1,a,0,transLeft,n,dp);//now can't buy as can have one stock at a time and transLeft will dec. on selling 
+            //not buying at this index
+            int nb=0+help(i+1,a,1,transLeft,n,dp);//can buy
+            return dp[i][buy][transLeft]=Math.max(b,nb);
+            
+        }else{// sell
+            //selling at this index
+            int s=a[i]+help(i+1,a,1,transLeft-1,n,dp);//now can buy and transLeft will dec. by 1 as selling 
+            //not selling at this index
+            int ns=0+help(i+1,a,0,transLeft,n,dp);//can't buy as not yet sold
+            return dp[i][buy][transLeft]=Math.max(s,ns);
+        }
+    }
 }
