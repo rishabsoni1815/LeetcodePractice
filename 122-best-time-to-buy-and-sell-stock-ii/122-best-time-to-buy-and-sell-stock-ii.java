f58b2b279f7521fee295b66b8677b03ef131d93a
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-07-03 22:44:30 +0530

    Time: 2643 ms (5.41%), Space: 44.4 MB (42.85%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,11 +1,16 @@
 class Solution {
-    public int maxProfit(final int[] A) {
-       if (A.length <= 1)
-            return 0;
-        int maxprofit = 0;
-        for (int i = 1; i < A.length; i++) {
-            maxprofit += Math.max(0, A[i] - A[i - 1]);
+    public int maxProfit(int[] a) {
+        int n=a.length;
+        Integer dp[]=new Integer[n];
+        return help(n-1,a,dp);
+    }
+    int help(int i,int a[],Integer dp[]){
+        if(i<=0) return 0;
+        int max=0;
+        if(dp[i]!=null) return dp[i];
+        for(int j=0;j<=i;j++){
+            max=Math.max(max,a[i]-a[j]+help(j-1,a,dp));
         }
-        return maxprofit;
+        return dp[i]=max;
     }
 }
