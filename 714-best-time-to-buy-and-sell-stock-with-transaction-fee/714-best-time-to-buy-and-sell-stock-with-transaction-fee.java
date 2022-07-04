c03600414cbd5571be94edfda21be62e06ae7e6e
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-07-04 22:32:25 +0530

    Time: 19 ms (36.77%), Space: 49.6 MB (99.37%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,24 +1,42 @@
 class Solution {
+    
+    //i+1 so can do space optimise 
     public int maxProfit(int[] a, int fee) {
-        //memoisation
         int n=a.length;
-        Integer dp[][]=new Integer[n][2];
-        return help(0,a,1,dp,fee);
-    }
-    int help(int i,int a[],int buy,Integer dp[][],int fee){
-        if(i>=a.length) return 0;
-        if(dp[i][buy]!=null) return dp[i][buy];
-        if(buy==1){// buy
-                 //buying at this index
-            int b=-a[i]+help(i+1,a,0,dp,fee);//now can't buy as can have one stock at a time and transLeft will dec. on selling 
-            //not buying at this index
-            int nb=help(i+1,a,1,dp,fee);//can buy
-            return dp[i][buy]=Math.max(b,nb);
-        }else{// sell
-            //selling at this index
-            int s=a[i]+-fee+help(i+1,a,1,dp,fee);//now can buy and pay fee and i will inc. by 2 as cooldown 
-            int ns=help(i+1,a,0,dp,fee);//not sell
-            return dp[i][buy]=Math.max(s,ns);
+        int dp[][]=new int[n+1][2];
+        //n+1 case base case has value 0
+        for(int i=n-1;i>=0;i--){
+            for(int buy=0;buy<2;buy++){
+                    if(buy==1){
+                        dp[i][buy]=Math.max(-a[i]+dp[i+1][0],dp[i+1][1]);
+                    }else{
+                        dp[i][buy]=Math.max(a[i]-fee+dp[i+1][1],dp[i+1][0]);
+                    }
+            }
         }
+        return dp[0][1];//at 0 always buy as no choice of sell
     }
+    
+    // public int maxProfit(int[] a, int fee) {
+    //     //memoisation
+    //     int n=a.length;
+    //     Integer dp[][]=new Integer[n][2];
+    //     return help(0,a,1,dp,fee);
+    // }
+    // int help(int i,int a[],int buy,Integer dp[][],int fee){
+    //     if(i>=a.length) return 0;
+    //     if(dp[i][buy]!=null) return dp[i][buy];
+    //     if(buy==1){// buy
+    //              //buying at this index
+    //         int b=-a[i]+help(i+1,a,0,dp,fee);//now can't buy as can have one stock at a time and transLeft will dec. on selling 
+    //         //not buying at this index
+    //         int nb=help(i+1,a,1,dp,fee);//can buy
+    //         return dp[i][buy]=Math.max(b,nb);
+    //     }else{// sell
+    //         //selling at this index
+    //         int s=a[i]+-fee+help(i+1,a,1,dp,fee);//now can buy and pay fee and i will inc. by 2 as cooldown 
+    //         int ns=help(i+1,a,0,dp,fee);//not sell
+    //         return dp[i][buy]=Math.max(s,ns);
+    //     }
+    // }
 }
