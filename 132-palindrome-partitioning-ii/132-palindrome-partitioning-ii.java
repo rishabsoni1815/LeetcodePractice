814cb029f114b6e80e182e862e280a114fccb62c
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-07-05 19:06:25 +0530

    Time: 2307 ms (5.02%), Space: 41.6 MB (92.14%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.java
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.java
@@ -1,28 +1,29 @@
 class Solution {
     public int minCut(String s) {
-        int dp[]=new int[s.length()];
-        Arrays.fill(dp,-1);
-       return help(s,s.length(),0,dp)-1;//subtracting partition at end
+        Integer dp[]=new Integer[s.length()];
+        return help(0,s,dp)-1;
     }
-   int help(String s,int n,int i,int dp[]){
-       if(i==n) return 0;
-       if(dp[i]!=-1) return dp[i];
-       int min=Integer.MAX_VALUE;
-       for(int j=i;j<n;j++){
-           if(is(s,i,j)){
-               min=Math.min(min,1+help(s,n,j+1,dp));
-           }
-       }
-       return dp[i]=min;
-    }
-     boolean is(String str, int start, int end){
-    while(start<=end){
-        if(str.charAt(start)!=str.charAt(end)){
-            return false;
+     int help(int i,String s,Integer dp[]){
+		if(i==s.length()) {
+			return 0;
+		}
+         if(dp[i]!=null) return dp[i];
+         int min=Integer.MAX_VALUE;
+        for(int j=i;j<s.length();j++){
+            if(check(s,i,j)) {//don't pass substring as it takes n^2
+            	min=Math.min(min,1+help(j+1,s,dp));
+            }
         }
-        start++;
-        end--;
+         return dp[i]=min;
     }
-    return true;
-}
+	boolean check(String str,int start,int end) {
+        while(start<=end){
+            if(str.charAt(start)!=str.charAt(end)){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+	}
 }
