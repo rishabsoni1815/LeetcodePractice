371c5cc0d899b56d1d24868c324f53716625086d
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-07-05 17:43:52 +0530

    Time: 26 ms (71.21%), Space: 42.7 MB (78.60%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
@@ -7,8 +7,22 @@ class Solution {
         c[l+1]=n;
         for(int i=1;i<=l;i++) c[i]=a[i-1];
         Arrays.sort(c);
-        Integer dp[][]=new Integer[l+1][l+1];
-        return help(1,l,c,dp);
+        
+        // Integer dp[][]=new Integer[l+1][l+1]; // memoisation
+        // return help(1,l,c,dp);
+        
+        int dp[][]=new int[l+2][l+2];
+        for(int i=l;i>=1;i--){//as required dp[k+1]
+            for(int j=1;j<=l;j++){
+                if(i>j) continue;
+                int min=Integer.MAX_VALUE;
+                for(int k=i;k<=j;k++){
+                    min=Math.min(min,c[j+1]-c[i-1]+dp[i][k-1]+dp[k+1][j]);
+                }
+                dp[i][j]=min;
+            }
+        }
+        return dp[1][l];
     }
     int help(int i,int j,int c[],Integer dp[][]){
         if(i>j) return 0;
