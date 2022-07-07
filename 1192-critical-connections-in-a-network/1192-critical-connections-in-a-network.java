f8d47bac2ed95d740544a8f110a5283e4d694447
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-07-08 02:33:40 +0530

    Time: 167 ms (65.15%), Space: 297 MB (60.74%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.java
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.java
@@ -10,38 +10,34 @@ class Solution {
             g.get(v).add(u);
         }
         printBridges(g, n,ans);
-        // System.out.println(ans.size());
         return ans;
     }
     void dfs(int node, int parent, int vis[], int tin[], int low[], List<List<Integer>> adj, int timer, List<List<Integer>> ans) {
-		vis[node] = 1; 
-		tin[node] = low[node] = timer++; 
+		vis[node] = 1; //visited marked
+		tin[node] = low[node] = timer++; //time inc so timer++ low ans tin both initialised by times
 
 		for(Integer it: adj.get(node)) {
-			if(it == parent) continue; 
+			if(it == parent) continue; //as we won't update low for parent
 
-			if(vis[it] == 0) {
+			if(vis[it] == 0) {//unvisited so make a dfs
 				dfs(it, node, vis, tin, low, adj, timer,ans); 
-				low[node] = Math.min(low[node], low[it]); 
-
-				if(low[it] > tin[node]) {
+				if(low[it] > tin[node]) {// check as a parent if my this neighbour and mine's edge is a bridge if low[it]>=tin[node] then it ins't a bridge low[it]-> lowest time in neighbours except parent
                     List<Integer>temp=new ArrayList<>();
                     temp.add(it);
                     temp.add(node);
                     ans.add(new ArrayList<>(temp));
-					// System.out.println(it + " " +node); 
-                     // System.out.println(ans.size());
 				}
+				low[node] = Math.min(low[node], low[it]); //update by lowest time from all my neighbour
 			} else {
-				low[node] = Math.min(low[node], tin[it]); 
+				low[node] = Math.min(low[node], tin[it]); //visited but still i can update my lowest time
 			}
 		}
 	}
     void printBridges(List<List<Integer>> adj, int n, List<List<Integer>> ans)
     {
         int vis[] = new int[n]; 
-        int tin[] = new int[n];
-        int low[] = new int[n]; 
+        int tin[] = new int[n];//finding time (first visit time)
+        int low[] = new int[n];//lowest time in neighbours except parent
 
         int timer = 0; 
         for(int i = 0;i<n;i++) {
