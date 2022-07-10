dc43099c6a17d80e1461476f22e330f4a0ecfb0b
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-07-10 23:20:11 +0530

    Time: 60 ms (13.85%), Space: 55.1 MB (39.81%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.java
+++ 503-next-greater-element-ii/503-next-greater-element-ii.java
@@ -1,14 +1,14 @@
 public class Solution {
-
+//o(n)
     public int[] nextGreaterElements(int[] nums) {
         int[] res = new int[nums.length];
         Stack<Integer> stack = new Stack<>();//like concatinate array with its copy to reduce space can do below else brute can be to apply on 2*n array size
         for (int i = 2 * nums.length - 1; i >= 0; --i) {
-            while (!stack.empty() && nums[stack.peek()] <= nums[i % nums.length]) {
+            while (!stack.empty() && stack.peek() <= nums[i % nums.length]) {
                 stack.pop();
-            }
-            res[i % nums.length] = stack.empty() ? -1 : nums[stack.peek()];
-            stack.push(i % nums.length);
+            }//this loop in total will nun only for n so overall complexity of problem is O(2n+2n) not O(n^2)
+            if(i<nums.length) res[i] = stack.empty() ? -1 : stack.peek();
+            stack.push(nums[i%nums.length]);
         }
         return res;
     }
