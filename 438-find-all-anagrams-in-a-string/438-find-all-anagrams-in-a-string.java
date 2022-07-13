44974e8dea3eb2708ddb3538d3beafbe0d5932d6
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-07-13 22:34:46 +0530

    Time: 14 ms (66.29%), Space: 47.9 MB (50.50%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -6,30 +6,20 @@ class Solution {
         for(int i=0;i<p.length();i++){
             h[p.charAt(i)-'a']++;
         }
-        int l=0,r=0,c=p.length();
-        while(r<s.length()){
-        //move right everytime, if the character exists in p's hash, decrease the count
-        //current hash value >= 1 means the character is existing in p
-            if(h[s.charAt(r)-'a']>=1){
+        int i=0,j=0,c=p.length(),n=s.length();
+        while(j<n){
+            if(h[s.charAt(j)-'a']>=1){
                 c--;
             }
-            h[s.charAt(r)-'a']--;
-            r++;
-        //when the count is down to 0, means we found the right anagram
-        //then add window's left to result list
-            if(c==0){
-                ans.add(l);
-            }
-        //if we find the window's size equals to p, then we have to move left (narrow the window) to find the new match window
-        //++ to reset the hash because we kicked out the left
-        //only increase the count if the character is in p
-        //the count >= 0 indicate it was original in the hash, cuz it won't go below 0
-            if((r-l) == p.length()){
-                if(h[s.charAt(l)-'a']>=0){
-                    c++;
-                }
-                h[s.charAt(l)-'a']++;
-                l++;
+            h[s.charAt(j)-'a']--;
+            if(j-i+1<p.length()){
+                j++;
+            }else{
+                if(c==0) ans.add(i);
+                if(h[s.charAt(i)-'a']>=0) c++;
+                h[s.charAt(i)-'a']++;
+                i++;
+                j++;
             }
         }
         return ans;
