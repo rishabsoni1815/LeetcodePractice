61f3615642b974898c165b9b0e545d35d53d5931
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-07-14 01:19:01 +0530

    Time: 59 ms (62.41%), Space: 51.9 MB (87.72%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.java
+++ 904-fruit-into-baskets/904-fruit-into-baskets.java
@@ -2,6 +2,8 @@ class Solution {
     public int totalFruit(int[] a) {
      return largestSubstringWithAtMostKUniqueCharacters(a,2);
     }
+    
+    /*
     int largestSubstringWithAtMostKUniqueCharacters(int a[],int k){
         int n=a.length;
         int i=0,j=0;
@@ -17,6 +19,30 @@ class Solution {
                 while(c>k){
                     h.put(a[i],h.get(a[i])-1);
                     if(h.get(a[i])==0) c--;
+                    i++;
+                }
+                j++;
+            }
+        }
+        return ans;
+    }
+    */
+    
+    //c->unique chars which is actually h.size() and for decreasing c we can remove from h so time wil also decrease so use h.size instead of c 
+    int largestSubstringWithAtMostKUniqueCharacters(int a[],int k){
+        int n=a.length;
+        int i=0,j=0;
+        HashMap<Integer,Integer>h=new HashMap<>();
+        int ans=0;
+        while(j<n){
+            h.put(a[j],h.getOrDefault(a[j],0)+1);
+            if(h.size()<=k){
+                ans=Math.max(ans,j-i+1);
+                j++;
+            }else if(h.size()>k){//k=2 here
+                while(h.size()>k){
+                    h.put(a[i],h.get(a[i])-1);
+                    if(h.get(a[i])==0) h.remove(a[i]);
                     i++;
                 }
                 j++;
