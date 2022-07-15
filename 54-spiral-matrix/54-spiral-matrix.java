1bda6c766f25bfbd96bc93eb8ea5a32cf3c8c0b0
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-07-15 16:15:53 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (35.50%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.java
+++ 54-spiral-matrix/54-spiral-matrix.java
@@ -5,31 +5,29 @@ class Solution {
          int d=0;
        ArrayList<Integer>ans=new ArrayList<>();
        int left=0,right=c-1,up=0,down=r-1;
-        // d=3 up, d=1 down, d=2 left , d=0 right
-       while(left<=right&&up<=down){
-           if(d==3){                                    //   up->
+        //d=0->left , d=1->down , d=2-> left, d=3 ->up
+       while(left<=right&&up<=down){if(d==0){
+               for(int i=left;i<=right;i++){
+                   ans.add(a[up][i]);
+               }
+                  up++;
+           }else if(d==1){
+               for(int i=up;i<=down;i++){
+                   ans.add(a[i][right]);
+               }
+                  right--;
+           }else if(d==2){
+                 for(int i=right;i>=left;i--){
+                   ans.add(a[down][i]);
+               }
+                  down--;
+           }else if(d==3){                             //   up->
                for(int i=down;i>=up;i--){              //   .
                                                        //   .
                    ans.add(a[i][left]);                //   down->
                                                        //          ^     .....   ^
                }                                       //         left          right
                   left++;
-           }else if(d==1){
-               for(int i=up;i<=down;i++){
-                   ans.add(a[i][right]);
-               }
-                  right--;
-           }
-           else if(d==0){
-               for(int i=left;i<=right;i++){
-                   ans.add(a[up][i]);
-               }
-                  up++;
-           }else if(d==2){
-                 for(int i=right;i>=left;i--){
-                   ans.add(a[down][i]);
-               }
-                  down--;
            }
            d=(d+1)%4;
        }
