b21999a3b03899dc92f8f792a5b37c5e761ef8a9
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-07-16 01:14:03 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (5.14%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.java
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.java
@@ -9,11 +9,27 @@
  * }
  */
 class Solution {
+    //recursive sol.
+    // public ListNode swapPairs(ListNode head) {
+    //     if(head==null||head.next==null) return head;
+    //     ListNode t=head.next;
+    //     head.next=swapPairs(head.next.next);
+    //     t.next=head;
+    //     return t;
+    // }
+    
     public ListNode swapPairs(ListNode head) {
-        if(head==null||head.next==null) return head;
-        ListNode t=head.next;
-        head.next=swapPairs(head.next.next);
+        ListNode t=new ListNode(-1);
         t.next=head;
-        return t;
+        ListNode prev=t;
+        while(head!=null && head.next!=null){
+            ListNode next=head.next;
+            head.next=next.next;//attaching 3rd no, to 1st next
+            next.next=head;//attaching 2nd number's next to 1st
+            prev.next=next;//prev.next is now the 2nd no.(new head)
+            prev=head;//pre for next pair is 1st no. (ie.e head)
+            head=head.next;//head will now be 3rd no. i.e head.next
+        }
+        return t.next;
     }
 }
