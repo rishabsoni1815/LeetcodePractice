71c82547fa4986217534b7e0703efbb774775791
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-07-15 16:31:21 +0530

    Time: 20 ms (26.08%), Space: 53.8 MB (60.57%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
@@ -6,30 +6,24 @@ class Solution {
         Integer dp[][]=new Integer[n][m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                boolean v[][]=new boolean[n][m];
-                help(i,j,v,a,n,m,dp);
+                help(i,j,a,n,m,dp);//no need of visited and doing backtracking by first making it true and then false again as don't need backtracking here
                 ans=Math.max(ans,1+dp[i][j]);
             }
         }
         return ans;
     }
-    int help(int i,int j,boolean v[][],int a[][],int n,int m,Integer dp[][]){
-        if(i<0||i>=n||j<0||j>=m||v[i][j]==true){
-            return 0;
-        }
+    int help(int i,int j,int a[][],int n,int m,Integer dp[][]){
         if(dp[i][j]!=null) return dp[i][j];
-        v[i][j]=true;
         int max=0;
         int dirx[]={-1,1,0,0};
         int diry[]={0,0,1,-1};
         for(int ii=0;ii<4;ii++){
             int x=i+dirx[ii];
             int y=j+diry[ii];
-            if(x>=0 && x<n && y<m && y>=0 && v[x][y]==false && a[x][y]>a[i][j]){
-                max=Math.max(max,1+help(x,y,v,a,n,m,dp));
+            if(x>=0 && x<n && y<m && y>=0 && a[x][y]>a[i][j]){
+                max=Math.max(max,1+help(x,y,a,n,m,dp));
             }
         }
-        v[i][j]=false;
         return dp[i][j]=max;
     }
 }
