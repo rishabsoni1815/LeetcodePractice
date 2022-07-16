aff5da016ef63e3e8acea7ef69c9c258d40a0efe
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-07-17 03:35:00 +0530

    Time: 2 ms (84.59%), Space: 54.7 MB (12.85%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.java
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.java
@@ -1,21 +1,24 @@
 class Solution {
     public int findUnsortedSubarray(int[] a) {
         int n=a.length;
-        int l=n,r=0;
-        Stack<Integer>s=new Stack<>();
+        int l=0,r=0;
+        //imp point-> that subarray is random it is not sorted desc. order
+        //so to find end of array imp is after that all elements to right should be greater than all till found so there should be no a[i] which is les than max found till now
+        //to find start, every element to left should be smaller than all found til now (travrse from right side) so no element should be greater than min found till now 
+        int max=Integer.MIN_VALUE;
         for(int i=0;i<n;i++){
-            while(s.size()>0&&a[s.peek()]>a[i]){
-                l=Math.min(l,s.pop());
+            if(a[i]<max){
+                r=i;
             }
-            s.push(i);
+            max=Math.max(max,a[i]);
         }
-        s.clear();
+        int min=Integer.MAX_VALUE;
         for(int i=n-1;i>=0;i--){
-            while(s.size()>0&&a[s.peek()]<a[i]){
-                r=Math.max(r,s.pop());
+            if(a[i]>min){
+                l=i;
             }
-            s.push(i);
+            min=Math.min(min,a[i]);
         }
-        return r-l>0 ? r-l+1 : 0;
+        return r-l >0 ? r-l+1 : 0;
     }
 }
