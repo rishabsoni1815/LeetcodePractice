b0e46c77c1da8333168c531488fca61d8ab924d8
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-07-24 01:25:22 +0530

    Time: 6 ms (65.41%), Space: 42.5 MB (57.15%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.java
+++ 518-coin-change-2/518-coin-change-2.java
@@ -1,18 +1,41 @@
 class Solution {
-    public int change(int t, int[] a) {
+   public int change(int t, int[] a) {
          int n=a.length;
-        int dp[][]=new int[n+1][t+1];
-        for(int i=0;i<=n;i++) dp[i][0]=1;
+        int dp[]=new int[t+1];
         for(int i=1;i<=n;i++){
+            int prev[]=new int[t+1];
+            prev[0]=1;
             for(int j=1;j<=t;j++){
-                int nt=dp[i-1][j];
+                int nt=dp[j];
                 int ta=0;
-                if(j-a[i-1]>=0) ta=dp[i][j-a[i-1]];
-                dp[i][j]=ta+nt;
+                if(j-a[i-1]>=0) ta=prev[j-a[i-1]];
+                prev[j]=ta+nt;
             }
+            dp=prev;
         }
-        return dp[n][t];
+        return dp[t];
     }
+    
+    
+    
+        //can optimise as i-1 only required
+ //   public int change(int t, int[] a) {
+//          int n=a.length;
+//         int dp[][]=new int[n+1][t+1];
+//         for(int i=0;i<=n;i++) dp[i][0]=1;
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=t;j++){
+//                 int nt=dp[i-1][j];
+//                 int ta=0;
+//                 if(j-a[i-1]>=0) ta=dp[i][j-a[i-1]];
+//                 dp[i][j]=ta+nt;
+//             }
+//         }
+//         return dp[n][t];
+//     }
+    
+    
+    
     /*
     //memoisation
     static long help(int i,int t,int a[],Long dp[][]){
