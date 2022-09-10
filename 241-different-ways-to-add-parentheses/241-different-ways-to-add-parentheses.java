87c89b14bd3fed03fab1a03b2931b42c2f0e7674
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-09-10 16:53:35 +0530

    Time: 11 ms (6.55%), Space: 42.7 MB (64.65%) - LeetHub

--- 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.java
+++ 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.java
@@ -1,19 +1,19 @@
 class Solution {
     public List<Integer> diffWaysToCompute(String s) {
         int n=s.length();
-        // 3-4*5
-        // 3-20 -> -17
-        // -1*5 -> -5
-       return help(0,n-1,s);
+        HashMap<String,List<Integer>>dp=new HashMap<>();//memoise strings
+       return help(0,n-1,s,dp);
     }
-    List<Integer> help(int i,int j,String s){
+    List<Integer> help(int i,int j,String s,HashMap<String,List<Integer>>dp){
         List<Integer>ans=new ArrayList<>();
         boolean flag=false;
+        String cdp=i+"#"+j;
+        if(dp.containsKey(cdp)) return dp.get(cdp);
         for(int k=i;k<=j;k++){
             if(s.charAt(k)=='+'||s.charAt(k)=='-'||s.charAt(k)=='*'){
                 flag=true;
-                List<Integer>l=help(i,k-1,s);
-                List<Integer>r=help(k+1,j,s);
+                List<Integer>l=help(i,k-1,s,dp);
+                List<Integer>r=help(k+1,j,s,dp);
                 for(int ll:l){
                     for(int rr:r){
                         if(s.charAt(k)=='*') ans.add(ll*rr);
@@ -27,6 +27,7 @@ class Solution {
             int cur=Integer.valueOf(s.substring(i,j+1));
             ans.add(cur);
         }
+        dp.put(cdp,ans);
         return ans;
     }
 }
