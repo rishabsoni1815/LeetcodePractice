ef4915e3424b9d161f875f405febf302970bf388
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-09-22 10:39:32 +0530

    Time: 24 ms (36.13%), Space: 51.8 MB (41.76%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.java
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.java
@@ -1,17 +1,17 @@
 class Solution {
     public String reverseWords(String s) {
        int n=s.length(),i=0,j=0;
-        String ans="";
+       StringBuilder ans=new StringBuilder();
        while(j<n&&i<n){
            StringBuilder t=new StringBuilder();
            while(j<n && s.charAt(j)!=' '){
                t.append(s.charAt(j));
                j++;
            }
-           ans+=t.reverse().toString();
-          if(j<n) ans+=s.charAt(j);
+           ans.append(t.reverse().toString());
+          if(j<n) ans.append(s.charAt(j));
            j++;
        }
-        return ans;
+        return ans.toString();
     }
 }
