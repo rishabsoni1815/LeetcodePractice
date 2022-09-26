1c2aeafe3573414cf4c9e9bfca658444bf5644c6
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-09-26 12:39:41 +0530

    Time: 12 ms (71.57%), Space: 42.5 MB (93.38%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.java
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.java
@@ -1,58 +1,21 @@
-public class Solution {
-    public int findPairs(int[] nums, int k) {
-        if (nums == null || nums.length == 0 || k < 0)   return 0;
-        
-        Map<Integer, Integer> map = new HashMap<>();
-        int count = 0;
-        for (int i : nums) {
-            map.put(i, map.getOrDefault(i, 0) + 1);
+class Solution {
+    public int findPairs(int[] a, int k) {
+        int n=a.length;
+        HashMap<Integer,Integer>h=new HashMap<>();
+        for(int i=0;i<n;i++){
+            h.put(a[i],h.getOrDefault(a[i],0)+1);
         }
-        
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (k == 0) {
-                //count how many elements in the array that appear more than twice.
-                if (entry.getValue() >= 2) {
-                    count++;
-                } 
-            } else {
-                if (map.containsKey(entry.getKey() + k)) {
-                    count++;
+        int ans=0;
+        for(int i:h.keySet()){
+            int f=h.get(i);
+            if(k==0){
+                if(f>1) ans++;
+            }else{
+                if(h.containsKey(i-k)){
+                    ans++;
                 }
             }
         }
-        
-        return count;
+        return ans;
     }
-    /*
-    //one pass
-    
-    class Solution {
-    public int findPairs(int[] nums, int k) {
-        if (k < 0) {
-            return 0;
-        }
-        Map<Integer, Integer> map = new HashMap<>();
-        int result = 0;
-        for (int i : nums) {
-            if (map.containsKey(i)) {
-                if (k == 0 && map.get(i) == 1) {
-                    result++;
-                }
-                map.put(i, map.get(i) + 1);
-            } else {
-                if (map.containsKey(i - k)) {
-                    result++;
-                }
-                if (map.containsKey(i + k)) {
-                    result++;
-                }
-                map.put(i, 1);
-            }
-        }
-        return result;
-    }
-}
-    
-    
-    */
 }
