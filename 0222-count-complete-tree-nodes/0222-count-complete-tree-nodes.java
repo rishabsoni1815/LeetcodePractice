319ef0583f90ce01e82ec88a581144c265597110
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-11-15 20:37:47 +0530

    Time: 0 ms (100.00%), Space: 49.9 MB (41.59%) - LeetHub

---
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {//tuf code tc->(logn)^2 as finding height(which takes logn) for at modst logn nodes as complete binary tree
+    public int countNodes(TreeNode root) {
+        if(root == null) return 0;
+        
+        int left = getHeightLeft(root);
+        int right = getHeightRight(root);
+        
+        //If left and right are equal it means that the tree is complete and hence go for 2^h -1.
+        if(left == right) return ((2<<(left)) -1);
+            
+        //else recursively calculate the number of nodes in left and right and add 1 for root.
+        else return countNodes(root.left)+ countNodes(root.right)+1;
+    }
+    
+    
+    public int getHeightLeft(TreeNode root){
+        int count=0;
+        while(root.left!=null){
+            count++;
+            root = root.left;
+        }
+        return count;
+    }
+    
+    
+    public int getHeightRight(TreeNode root){
+        int count=0;
+        while(root.right!=null){
+            count++;
+            root = root.right;
+        }
+        return count;
+    }
+}
