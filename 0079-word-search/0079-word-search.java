b35c87b40b381a58882eb6cea994fe9428fc442d
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2022-11-24 09:55:54 +0530

    Time: 146 ms (77.13%), Space: 42.3 MB (46.21%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,37 @@
+public class Solution {
+    static boolean[][] visited;//trie solution see submission
+    public boolean exist(char[][] board, String word) {
+        visited = new boolean[board.length][board[0].length];
+        
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[i].length; j++){
+                if((word.charAt(0) == board[i][j]) && search(board, word, i, j, 0)){
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+    //time complexity - > one has 4 direction every time and calling string length times so 4^l and this is done on every i and j so m*n multiplied by 4^l so -> O(n*m*(4^l)) 
+    private boolean search(char[][]board, String word, int i, int j, int index){
+        if(index == word.length()){
+            return true;
+        }
+        
+        if(i >= board.length || i < 0 || j >= board[i].length || j < 0 || board[i][j] != word.charAt(index) || visited[i][j]){
+            return false;
+        }
+        
+        visited[i][j] = true;
+        if(search(board, word, i-1, j, index+1) || 
+           search(board, word, i+1, j, index+1) ||
+           search(board, word, i, j-1, index+1) || 
+           search(board, word, i, j+1, index+1)){
+            return true;
+        }
+        
+        visited[i][j] = false;
+        return false;
+    }
+}
