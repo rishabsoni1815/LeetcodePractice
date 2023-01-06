bca35f3d95b741b25be33a57d7001062fe0a3479
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2023-01-06 21:37:14 +0530

    Time: 42 ms (51.66%), Space: 78 MB (19.43%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -1,22 +1,17 @@
 class Solution {
-    public int maxIceCream(int[] c, int k) {
-        int n=c.length;
-        Arrays.sort(c);
-        long a[]=new long[n];
-        a[0]=c[0];
-        for(int i=1;i<n;i++){
-            a[i]=c[i]+a[i-1];
-        }
-        int ans=-1,l=0,r=n-1;
-        while(l<=r){
-            int m=l+(r-l)/2;
-            if(a[m]<=k){
-                ans=m;
-                l=m+1;
+    public int maxIceCream(int[] a, int p) {
+        Arrays.sort(a);
+        int n=a.length;
+        int ans=0;
+        long sum=0;
+        for(int i=0;i<n;i++){
+            sum+=a[i];
+            if(sum<=p){
+                ans++;
             }else{
-                r=m-1;
+                break;
             }
         }
-        return ans+1;
+        return ans;
     }
 }
