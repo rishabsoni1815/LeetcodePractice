43367f5c91a80558ef1ec7a1ccbca56b9d467596
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2023-03-08 15:51:27 +0530

    Time: 39 ms (55.10%), Space: 43.6 MB (23.25%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,11 +1,11 @@
 class Solution {
     public int minEatingSpeed(int[] a, int h) {
         int n=a.length;
-        long sum=0;
-        for(int x:a) sum+=x;
-        long l=0,r=sum,ans=sum;
+        int max=a[0];
+        for(int x:a) max=Math.max(max,x);
+        int l=0,r=max,ans=max;
         while(l<=r){
-            long m=l+(r-l)/2;
+            int m=l+(r-l)/2;
             if(h(m,a,h)){
                 ans=m;
                 r=m-1;
@@ -13,9 +13,9 @@ class Solution {
                 l=m+1;
             }
         }
-        return (int)ans;
+        return ans;
     }
-    boolean h(long m,int a[],int h){
+    boolean h(int m,int a[],int h){
         int k=0;
         for(int i=0;i<a.length;i++){
             k+=(Math.ceil((double)a[i]/m));
