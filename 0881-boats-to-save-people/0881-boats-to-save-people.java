c0c58fd52949232223f13e19faf8a1d903c2aaea
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2023-04-03 23:13:16 +0530

    Time: 17 ms (98.31%), Space: 51.4 MB (6.93%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -1,18 +1,33 @@
+/*
+
+Let's talk about what we'll do when we face possibility no. 1.0 (people[left] + people[right]<=limit)
+If that's the case then we only require single boat & increment the boatCount, left pointer & decrement the right pointer
+
+Let's talk about what we'll do when we face possibility no. 2.0
+If that's the case then we only one person will go in boat & increment the boatCount & decrement the right pointer
+
+*/
+
 class Solution {
-    public int numRescueBoats(int[] a, int l) {
-        Arrays.sort(a);
-        int i=0,j=a.length-1;
-        int ans=0;
-        while(i<=j){
-            if(a[i]+a[j] <=l){
-                i++;
-                j--;
-                ans++;
-            }else if(a[j]<=l){
-                j--;
-                ans++;
+    public int numRescueBoats(int[] people, int limit) {
+        int boatCount = 0;
+        Arrays.sort(people);
+        
+        int left = 0;
+        int right = people.length - 1;
+        
+        while(left <= right){
+            int sum = people[left] + people[right];
+            if(sum <= limit){
+                boatCount++;
+                left++;
+                right--;
+            }
+            else{
+                boatCount++;
+                right--;
             }
         }
-        return ans;
+        return boatCount;
     }
 }
