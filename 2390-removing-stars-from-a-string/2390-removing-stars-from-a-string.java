5ffa2bf888673dc7fbf8246081be742e955fd891
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2023-04-11 22:07:30 +0530

    Time: 217 ms (44.18%), Space: 44.8 MB (37.60%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
@@ -19,3 +19,21 @@ class Solution {
         return answer.reverse().toString();
     }
 }
+/*
+
+class Solution {
+    public String removeStars(String s) {
+        int j = 0;
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '*') {
+                answer.deleteCharAt(answer.length() - 1);
+            } else {
+                answer.append(s.charAt(i));
+            }
+        }
+        return answer.toString();
+    }
+}
+
+*/
