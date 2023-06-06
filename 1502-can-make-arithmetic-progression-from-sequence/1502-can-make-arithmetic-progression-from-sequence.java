6cab80b74e4e05391b3dd6c29767b04ae0bb2cba
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2023-06-06 21:44:01 +0530

    Time: 1 ms (99.24%), Space: 40.3 MB (100.00%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
@@ -1,10 +1,32 @@
 class Solution {
     public boolean canMakeArithmeticProgression(int[] a) {
         int n=a.length;
-        Arrays.sort(a);
-        int x=a[1]-a[0];
-        for(int i=2;i<n;i++){
-            if((a[i]-a[i-1])!=x) return false;
+        int min=a[0],max=a[0];
+        for(int i=0;i<n;i++){
+            min=Math.min(min,a[i]);
+            max=Math.max(max,a[i]);
+        }
+
+        int d=0,x=max-min;
+        if(x%(n-1)==0){
+            d=x/(n-1);
+        }else{
+            return false;
+        }        
+        for(int i=0;i<n;){
+            int y=min+(i*d);
+            if(a[i]!=y){
+                int ni=-1,nx=a[i]-min;
+                if(nx%d==0){
+                    ni=nx/d;
+                    if(ni<i || a[ni]==a[i]) return false;//prev are correct so cant be possible also if correct element already present this means duplicate present 
+                    int u=a[ni];
+                    a[ni]=a[i];
+                    a[i]=u;
+                }else return false;
+            }else{
+                i++;
+            }
         }
         return true;
     }
