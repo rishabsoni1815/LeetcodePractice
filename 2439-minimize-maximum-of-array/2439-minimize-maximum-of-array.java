8ff0fbef86f4acb2b9c7d7de04b84d10d32ed758
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2023-06-07 15:54:53 +0530

    Time: 10 ms (36.61%), Space: 56.2 MB (96.26%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.java
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.java
@@ -1,10 +1,29 @@
 class Solution {
-    public int minimizeArrayValue(int[] A) {
-        long sum = 0, res = 0;
-        for (int i = 0; i < A.length; ++i) {
-            sum += A[i];
-            res = Math.max(res, (sum + i) / (i + 1));
+    public int minimizeArrayValue(int[] a) {
+        int n=a.length;
+        int l=0,r=0;
+        for(int x:a){
+            r=Math.max(r,x);
         }
-        return (int)res;
+        if(a[0]==r) return r;
+        int ans=r;
+        while(l<=r){
+            int m=l+(r-l)/2;
+            if(help(a,m,n)){
+                ans=m;
+                r=m-1;
+            }else{
+                l=m+1;
+            }
+        }
+        return ans;
+    }
+    boolean help(int a[],int m,int n){
+        long s=0;
+        for(int i=0;i<n;i++){
+            s+=a[i];
+            if(s>(long)(i+1)*m) return false;
+        }
+        return true;
     }
 }
