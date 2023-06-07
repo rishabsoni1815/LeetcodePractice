847b93e83fe18d319e044831e029c26acbc4ba56
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2023-06-07 20:37:53 +0530

    Time: 0 ms (100.00%), Space: 38.5 MB (100.00%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.java
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.java
@@ -37,8 +37,8 @@ class Solution {
 /*
 
 // method 2 ->
-Step 1: a | b is what we have while c is what we want. An XOR operation finds all different bits, i.e. (a | b) ^ c sets the bits where flip(s) is needed. Then we count the set bits.
-Step 2: There is only one case when two flips are needed: a bit is 0 in c but is 1 in both a and b. An AND operation finds all common 1 bits, i.e. a & b & ((a | b) ^ c) sets the common 1 bits in a, b and the must-flip bits found in Step 1.
+// Step 1: a | b is what we have while c is what we want. An XOR operation finds all different bits, i.e. (a | b) ^ c sets the bits where flip(s) is needed. Then we count the set bits.
+// Step 2: There is only one case when two flips are needed: a bit is 0 in c but is 1 in both a and b. An AND operation finds all common 1 bits, i.e. a & b & ((a | b) ^ c) sets the common 1 bits in a, b and the must-flip bits found in Step 1.
 
 // TC -> O(1) as In Java, Integer.bitCount() also uses a precomputed table of bit counts for every possible 16-bit integer value  
     public int minFlips(int a, int b, int c) {
