c5798bd70c6ad3e5ab48f3ade3804c8145a5d1b1
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2023-06-12 01:03:18 +0530

    Time: 10 ms (100.00%), Space: 44.4 MB (50.00%) - LeetHub

--- 2730-find-the-longest-semi-repetitive-substring/2730-find-the-longest-semi-repetitive-substring.java
+++ 2730-find-the-longest-semi-repetitive-substring/2730-find-the-longest-semi-repetitive-substring.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int longestSemiRepetitiveSubstring(String s) {
+    public int longestSemiRepetitiveSubstring(String s) {//problem is no >1 pair of repeated nos. so ans of 333 is 2 not 3 as 333 has more than a pair (total consecutive should not be more than 2 i.e pair)
         int n=s.length();
         int ans=1;
         int i=0,c=0,j=0;
