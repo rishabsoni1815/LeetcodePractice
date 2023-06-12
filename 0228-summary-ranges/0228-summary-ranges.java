55bea2f54e069d2d79f52727f0450fe739b7fd7f
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2023-06-12 21:24:42 +0530

    Time: 5 ms (83.94%), Space: 40.7 MB (85.78%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -3,24 +3,17 @@ class Solution {
         int n=a.length;
         List<String>ans=new ArrayList<>();
         if(n==0) return ans;
-        int px=a[0],x=a[0];
-        for(int i=1;i<n;i++){
-            if((x+1)!=a[i]){
-                if(px==x){
-                    ans.add(""+x);
-                }else{
-                    ans.add(""+px+"->"+x);
-                }
-                px=a[i];
+        for(int i=0;i<n;i++){
+            int x=a[i];
+            while(i+1<n && a[i]+1==a[i+1]){
+                i++;
             }
-            x=a[i];
+            if(x!=a[i]){
+                ans.add(x+"->"+a[i]);
+            }else{
+                ans.add(String.valueOf(x));
+            }
         }
-        
-                if(px==x){
-                    ans.add(""+x);
-                }else{
-                    ans.add(""+px+"->"+x);
-                }
         return ans;
     }
 }
