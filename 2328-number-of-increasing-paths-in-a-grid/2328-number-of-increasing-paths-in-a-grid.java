58fe11480b0a863ea10c733c860fd6c2d8f476a9
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2023-06-18 20:35:25 +0530

    Time: 73 ms (21.58%), Space: 57.2 MB (74.10%) - LeetHub

--- 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.java
+++ 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.java
@@ -1,28 +1,30 @@
 class Solution {
+    int mod=1000000007;
     public int countPaths(int[][] a) {
-         int n=a.length;int mod=(int)(1e9+7);
+        int n=a.length;
         int m=a[0].length;
         int ans=0;
-        int dp[][]=new int[n][m];
+        Integer dp[][]=new Integer[n][m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                ans=(ans%mod+(1+help(i,j,a,n,m,dp,mod)%mod)%mod)%mod;
+                help(i,j,a,n,m,dp);
+                ans=(ans%mod+dp[i][j]%mod)%mod;
             }
         }
-        return ans%mod;
+        return ans;
     }
-    int help(int i,int j,int a[][],int n,int m,int dp[][],int mod){
-        if(dp[i][j]!=0) return dp[i][j];
-        int res=0;
+    int help(int i,int j,int a[][],int n,int m,Integer dp[][]){
+        if(dp[i][j]!=null) return dp[i][j];
+        int ans=1;
         int dirx[]={-1,1,0,0};
         int diry[]={0,0,1,-1};
         for(int ii=0;ii<4;ii++){
             int x=i+dirx[ii];
             int y=j+diry[ii];
             if(x>=0 && x<n && y<m && y>=0 && a[x][y]>a[i][j]){
-                res=(res%mod+(1+help(x,y,a,n,m,dp,mod))%mod)%mod;
+                ans=(ans%mod+help(x,y,a,n,m,dp)%mod)%mod;
             }
         }
-        return dp[i][j]=res%mod;
+        return dp[i][j]=ans%mod;
     }
 }
