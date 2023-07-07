56e7629f286e70980c22fd1326113f27ccca60b5
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2023-07-07 23:48:48 +0530

    Time: 27 ms (18.13%), Space: 43.7 MB (65.00%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
@@ -1,4 +1,35 @@
 class Solution {
+    //we can run sliding window on finding max length for all T and F separately return max
+    public int maxConsecutiveAnswers(String s, int k) {
+        int n=s.length();
+        return Math.max(help(s,n,k,true),help(s,n,k,false));
+    }
+    int help(String s,int n,int k,boolean f){
+        int j=0,i=0,max=0;
+        while(i<n){
+                if(s.charAt(i)=='F' && f==true){
+                    k--;
+                }else if(s.charAt(i)=='T' && f==false){
+                    k--;
+                }
+            while(k<0 && j<=i){
+                if(s.charAt(j)=='T' && f==false){
+                    k++;
+                }else if(s.charAt(j)=='F' && f==true){
+                    k++;
+                }
+                j++;
+            }
+            max=Math.max(max,i-j+1);
+            i++;
+        }
+        return max;
+    }
+    
+    
+    
+    //binary search on finding if can we find m length consecutive T/F
+    /*
     public int maxConsecutiveAnswers(String s, int k) {
         int n=s.length();
         int l=0,r=n,ans=0;
@@ -13,6 +44,7 @@ class Solution {
         }
         return ans;
     }
+    // m length window possible within k or not
     boolean help(String s,int n,int k,int m,boolean f){
         int x=0,i=0;
         while(i<n){
@@ -31,4 +63,5 @@ class Solution {
         }
         return false;
     }
+    */
 }
