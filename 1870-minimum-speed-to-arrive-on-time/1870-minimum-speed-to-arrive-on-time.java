352e31169aadd49051fb816af22bf0d8f0dd69cb
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2023-07-26 18:52:59 +0530

    Time: 1790 ms (5.54%), Space: 58.9 MB (81.66%) - LeetHub

---
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minSpeedOnTime(int[] a, double h) {
+        int n=a.length;
+        int l=1,r=1000000001,ans=-1;
+        while(l<=r){
+            int m= l+(r-l)/2;
+            if(help(a,h,m)){
+                ans=m;
+                r=m-1;
+            }else{
+                l=m+1;
+            }
+        }
+        return ans;
+    }
+    boolean help(int a[],double h,int m){
+        double k=0.0;
+        for(int i=0;i<a.length;i++){
+            if(k%1!=0){
+                k=Math.ceil(k);
+            }
+            double x=(double)a[i]/m;
+            k+=x;
+        }
+        return k<=h;
+    }
+}
