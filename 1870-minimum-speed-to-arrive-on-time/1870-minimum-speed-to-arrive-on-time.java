dbfc16d9b8b0a6fe36f85f7a018f44440cb5be54
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2023-07-26 18:55:08 +0530

    Time: 210 ms (60.21%), Space: 59.4 MB (17.65%) - LeetHub

--- 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.java
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.java
@@ -1,27 +1,21 @@
 class Solution {
-    public int minSpeedOnTime(int[] a, double h) {
-        int n=a.length;
-        int l=1,r=10000001,ans=-1;
-        while(l<=r){
-            int m= l+(r-l)/2;
-            if(help(a,h,m)){
-                ans=m;
-                r=m-1;
+    public int minSpeedOnTime(int[] dist, double hour) {
+        int n = dist.length;
+        int min = 1, max = 10000005;
+        int ans = -1;
+        while(min <= max){
+            int mid = (max + min)/2;
+            double sum = 0;
+            for(int i = 0; i<n-1; ++i){
+                sum += Math.ceil( ( (double) dist[i]) /mid);
+            }
+            sum = sum + ( ( (double) dist[n-1]) /mid);
+            if(sum > hour){
+                min = mid+1;
             }else{
-                l=m+1;
+                max = mid-1;
             }
         }
-        return ans;
-    }
-    boolean help(int a[],double h,int m){
-        double k=0.0;
-        for(int i=0;i<a.length;i++){
-            if(k%1!=0){
-                k=Math.ceil(k);
-            }
-            double x=(double)a[i]/m;
-            k+=x;
-        }
-        return k<=h;
+        return min>=10000005?-1: min;
     }
 }
