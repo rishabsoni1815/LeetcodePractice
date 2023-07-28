530b64534fa5328ec6c24f99147c742287a2cd43
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2023-07-28 21:57:00 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (86.62%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -3,16 +3,17 @@ class Solution {
         int n=a.length;
         int s=0;
         for(int u:a) s+=u;
-        int y=help(0,n-1,a,0);
-        System.out.println(y+" ans");
+        Integer dp[][]=new Integer[n][n];
+        int y=help(0,n-1,a,0,dp);
         if(2*y>=s) return true;
         return false;
         
     }
-    int help(int i,int j,int a[],int b){
+    int help(int i,int j,int a[],int b,Integer dp[][]){
         if(i>j) return 0;
-        int x=a[i]+Math.min(help(i+2,j,a,1-b),help(i+1,j-1,a,1-b));
-        int y=a[j]+Math.min(help(i,j-2,a,1-b),help(i+1,j-1,a,1-b));
-        return Math.max(x,y);
+        if(dp[i][j]!=null) return dp[i][j];
+        int x=a[i]+Math.min(help(i+2,j,a,1-b,dp),help(i+1,j-1,a,1-b,dp));
+        int y=a[j]+Math.min(help(i,j-2,a,1-b,dp),help(i+1,j-1,a,1-b,dp));
+        return dp[i][j]=Math.max(x,y);
     }
 }
