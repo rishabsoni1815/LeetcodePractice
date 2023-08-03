926855879e2b699ad725d1b8334ee4884e8f1e48
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2023-08-04 01:26:32 +0530

    Time: 0 ms (100.00%), Space: 39.9 MB (72.52%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,8 +1,16 @@
 class Solution {
     public int rob(int[] a) {
         int n=a.length;
-        Integer dp[]=new Integer [n];
-        return help(0,n,a,dp);
+        // Integer dp[]=new Integer [n];
+        // return help(0,n,a,dp);
+        int dp[]=new int[n];
+        dp[0]=a[0];
+        for(int i=1;i<n;i++){
+            int r=a[i];
+            dp[i]=Math.max(r,dp[i-1]);
+            if(i-2>=0) dp[i]=Math.max(dp[i],r+dp[i-2]);
+        }
+        return dp[n-1];
     }
     int help(int i,int n,int a[],Integer dp[]){
         if(i>=n) return 0;
