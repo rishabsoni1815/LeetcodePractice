c1949188f4d06028fb3a74f0d4bdae5010db3107
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2023-08-04 02:35:08 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (19.39%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -2,18 +2,24 @@ class Solution {
     public int uniquePathsWithObstacles(int[][] a) {
         int n=a.length;
         int m=a[0].length;
-        int dp[][]=new int[n][m];
-        for(int i=0;i<n;i++) Arrays.fill(dp[i],-1);
-        return help(0,0,a,n,m,dp);
-    }
-    int help(int i,int j,int a[][],int n,int m,int dp[][]){
-        if(i>=n ||j>=m || a[i][j]==1){
-            return 0;
+        int dp[][]=new int[n+1][m+1];
+        for(int i=n;i>0;i--){
+            for(int j=m;j>0;j--){
+                if(a[i-1][j-1]==1) {
+                    dp[i][j]=0;
+                    continue;
+                }
+                if(i==n && j==m){
+                    dp[i][j]=(a[i-1][j-1]==1?(0):(1));
+                }
+                if(j==m||i==n){
+                    if(j==m && i!=n) dp[i][j]=dp[i+1][j];
+                    if(i==n && j!=m) dp[i][j]=dp[i][j+1];
+                }else{
+                    dp[i][j]=dp[i+1][j]+dp[i][j+1];
+                }
+            }
         }
-        if(i==n-1 && j==m-1) {
-            return (a[i][j]==0?1:0);
-        }
-        if(dp[i][j]!=-1) return dp[i][j];
-        return dp[i][j]=help(i+1,j,a,n,m,dp)+help(i,j+1,a,n,m,dp);
+        return dp[1][1];
     }
 }
