505748cf7f04e63f2cf6b0227884971d665426bf
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2023-08-04 02:36:29 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (41.43%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -2,24 +2,26 @@ class Solution {
     public int uniquePathsWithObstacles(int[][] a) {
         int n=a.length;
         int m=a[0].length;
-        int dp[][]=new int[n+1][m+1];
+        int dp[]=new int[m+1];
         for(int i=n;i>0;i--){
+            int prev[]=new int[m+1];
             for(int j=m;j>0;j--){
                 if(a[i-1][j-1]==1) {
-                    dp[i][j]=0;
+                    prev[j]=0;
                     continue;
                 }
                 if(i==n && j==m){
-                    dp[i][j]=(a[i-1][j-1]==1?(0):(1));
+                    prev[j]=(a[i-1][j-1]==1?(0):(1));
                 }
                 if(j==m||i==n){
-                    if(j==m && i!=n) dp[i][j]=dp[i+1][j];
-                    if(i==n && j!=m) dp[i][j]=dp[i][j+1];
+                    if(j==m && i!=n) prev[j]=dp[j];
+                    if(i==n && j!=m) prev[j]=prev[j+1];
                 }else{
-                    dp[i][j]=dp[i+1][j]+dp[i][j+1];
+                    prev[j]=dp[j]+prev[j+1];
                 }
             }
+            dp=prev;
         }
-        return dp[1][1];
+        return dp[1];
     }
 }
