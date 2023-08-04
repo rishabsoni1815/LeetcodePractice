18613a5a95c4fcacb56a84d294a5c4ef18283aac
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2023-08-04 17:55:20 +0530

    Time: 6 ms (64.15%), Space: 43.4 MB (58.79%) - LeetHub

---
+++ 0139-word-break/0139-word-break.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> a) {
+        int n=s.length();
+        HashSet<String>h=new HashSet<>();
+        for(String y:a) h.add(y);
+        Boolean dp[]=new Boolean[n];
+        return help(0,s.length(),s,h,dp);
+    }
+    boolean help(int i,int n,String s,HashSet<String>h,Boolean dp[]){
+        if(i>=n) return true;
+        StringBuilder temp=new StringBuilder();
+        if(dp[i]!=null) return dp[i];
+        boolean ans=false;
+        for(int j=i;j<n;j++){
+            temp.append(s.charAt(j));
+            if(h.contains(temp.toString())){
+                ans|=help(j+1,n,s,h,dp);
+            }
+        }
+        return dp[i]=ans;
+    }
+}
