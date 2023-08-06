c50b30f65207e3e83ade799207c34adcd5696ff0
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2023-08-07 02:00:20 +0530

    Time: 3 ms (87.91%), Space: 40.7 MB (76.92%) - LeetHub

--- 0920-number-of-music-playlists/0920-number-of-music-playlists.java
+++ 0920-number-of-music-playlists/0920-number-of-music-playlists.java
@@ -1,23 +1,21 @@
 class Solution {
-    public int numMusicPlaylists(int n, int goal, int k) {
-        int MOD = 1_000_000_007;
-
-        // Initialize the DP table
-        long[][] dp = new long[goal + 1][n + 1];
-        dp[0][0] = 1;
-
-        for (int i = 1; i <= goal; i++) {
-            for (int j = 1; j <= Math.min(i, n); j++) {
-                // The i-th song is a new song
-                dp[i][j] = dp[i - 1][j - 1] * (n - j + 1) % MOD;
-                // The i-th song is a song we have played before
-                if (j > k) {
-                    dp[i][j] = (dp[i][j] + dp[i - 1][j] * (j - k)) % MOD;
-                }
-            }
-        }
-
-        return (int) dp[goal][n];
+    public int numMusicPlaylists(int n, int g, int k) {
+        int mod=1000000007;
+        Long dp[][]=new Long[g+1][n+1];
+        return (int)help(n,0,0,g,k,mod,dp)%mod;
     }
-
+    long help(int n,int used,int i,int g,int k,int mod,Long dp[][]){
+        if(i==g) {
+            if(used==n) return 1;
+            return 0;
+        }
+        if(used>n) return 0;
+        // System.out.println(i+" "+" "+used);
+        if(dp[i][used]!=null) return dp[i][used];
+        
+        long use=(help(n,used,i+1,g,k,mod,dp)*(Math.max(0,used-k)))%mod;//placing used song at this index has this many options
+        long notuse=(help(n,used+1,i+1,g,k,mod,dp)*(n-used))%mod;//place not used at this plcae
+        
+        return dp[i][used]=(use+notuse)%mod;
+    }
 }
