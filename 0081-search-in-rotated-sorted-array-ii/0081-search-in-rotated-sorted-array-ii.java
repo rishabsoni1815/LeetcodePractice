501702cedd7ed7a8de262f98af41af4404dc21bf
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2023-08-10 03:02:36 +0530

    Time: 0 ms (100.00%), Space: 43.6 MB (7.65%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean search(int[] a, int t) {
+        //same as this - https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+       // in this we just added a[i]==a[m] && a[m]==a[j] condition for https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/
+        
+        int n=a.length;
+        int l=0,r=n-1;
+        while(l<=r){
+            int m=l+(r-l)/2;
+            if(a[m]==t) return true;
+            if(a[l]==a[m] && a[m]==a[r]){
+                l++;
+                r--;
+                continue;  //as then next if statement will also be checked
+            }
+            if(a[l]<=a[m]){//left sorted
+                if(t>=a[l] && t<=a[m]){//t in left sorted half so search in it
+                    r=m-1;
+                }else{//t not lie in left sorted half
+                    l=m+1;
+                }
+            }else{//right sorted
+                if(t>=a[m] && t<=a[r]){//t in right sorted half so search in it
+                    l=m+1;
+                }else{//t not lie in right sorted half
+                    r=m-1;
+                }
+            }
+        }
+        return false;
+    }
+}
