bee9eb041bcfbf1c78920ec388a287f0da629962
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2023-08-10 02:56:55 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (70.94%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,28 +1,26 @@
 class Solution {
-    public int search(int[] a, int target) {
-         int low = 0, high = a.length - 1; 
-        while(low <= high) {
-            int mid = (low + high) >> 1; 
-            if(a[mid] == target) return mid; 
-            
-            // the left side is sorted then check if target is in left half if not then l=m+1 is only option
-            if(a[low] <= a[mid]) {
-                if(target >= a[low] && target <= a[mid]) {
-                    high = mid - 1; 
+    public int search(int[] a, int t) {
+        //option1 - find min in sorted rotated then search in left/right half acc to condition in which target lies
+        //optio 2- direct check if which side is sorted and if t lies in it or not then apply cases
+        int n=a.length;
+        int l=0,r=n-1;
+        while(l<=r){
+            int m=l+(r-l)/2;
+            if(a[m]==t) return m;
+            if(a[l]<=a[m]){//left sorted
+                if(t>=a[l] && t<=a[m]){//t in left sorted half so search in it
+                    r=m-1;
+                }else{//t not lie in left sorted half
+                    l=m+1;
                 }
-                else {
-                    low = mid + 1; 
-                }
-            }
-            else {//if right half is sorted then chechk if it is in right half,if not then h=m+1
-                if(target >= a[mid] && target <= a[high]) {
-                    low = mid + 1; 
-                }
-                else {
-                    high = mid - 1; 
+            }else{//right sorted
+                if(t>=a[m] && t<=a[r]){//t in right sorted half so search in it
+                    l=m+1;
+                }else{//t not lie in right sorted half
+                    r=m-1;
                 }
             }
-        } 
-        return -1; 
+        }
+        return -1;
     }
 }
