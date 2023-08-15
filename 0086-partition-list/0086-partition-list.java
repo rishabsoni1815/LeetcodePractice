53368e32d72d6a2c9653cdc1b798507ce18a166f
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2023-08-15 15:04:03 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (36.82%) - LeetHub

--- 0086-partition-list/0086-partition-list.java
+++ 0086-partition-list/0086-partition-list.java
@@ -10,22 +10,40 @@
  */
 class Solution {
     public ListNode partition(ListNode head, int x) {
-        ListNode dhead=new ListNode(-1);
-        ListNode head1=new ListNode(-1);
-        ListNode temp=dhead;
-        ListNode temp1=head1;
-        while(head!=null){
-            if(head.val<x){
-                dhead.next=head;
-                dhead=dhead.next;
-            }else{
-                head1.next=head;
-                head1=head1.next;
+
+        // before and after are the two pointers used to create the two list
+        // before_head and after_head are used to save the heads of the two lists.
+        // All of these are initialized with the dummy nodes created.
+        ListNode before_head = new ListNode(0);
+        ListNode before = before_head;
+        ListNode after_head = new ListNode(0);
+        ListNode after = after_head;
+
+        while (head != null) {
+
+            // If the original list node is lesser than the given x,
+            // assign it to the before list.
+            if (head.val < x) {
+                before.next = head;
+                before = before.next;
+            } else {
+                // If the original list node is greater or equal to the given x,
+                // assign it to the after list.
+                after.next = head;
+                after = after.next;
             }
-            head=head.next;
+
+            // move ahead in the original list
+            head = head.next;
         }
-        head1.next=null;
-        dhead.next=temp1.next;
-        return temp.next;
+
+        // Last node of "after" list would also be ending node of the reformed list
+        after.next = null;
+
+        // Once all the nodes are correctly assigned to the two lists,
+        // combine them to form a single list which would be returned.
+        before.next = after_head.next;
+
+        return before_head.next;
     }
 }
