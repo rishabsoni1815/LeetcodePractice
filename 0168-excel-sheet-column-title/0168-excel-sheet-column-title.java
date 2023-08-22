380ee04ac2a0523d7a0c64cd74394829def4168b
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2023-08-22 19:07:29 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (91.43%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
@@ -1,4 +1,5 @@
 class Solution {
+    //https://leetcode.com/problems/excel-sheet-column-title/discuss/441430/Detailed-Explanation-Here's-why-we-need-n-at-first-of-every-loop-(JavaPythonC%2B%2B)
     public String convertToTitle(int a) {
        // a--;//not one time as every time our system is added by number one when one cycle is completed as if __ two letter word is there for 1 index 0-26 is compensated but for next also there system starts from 1 so have to compensate for that so evey time decrease by one
         StringBuilder s=new StringBuilder("");
