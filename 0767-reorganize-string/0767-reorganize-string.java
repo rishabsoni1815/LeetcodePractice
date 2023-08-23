b16889f757286507345fdea86cdfc21511431107
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2023-08-23 16:10:38 +0530

    Time: 1 ms (87.28%), Space: 41 MB (33.48%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.java
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -1,38 +1,36 @@
 class Solution {
     public String reorganizeString(String s) {
         int h[]=new int[26];
+        int max=0;
+        char maxv='a';
         for(int i=0;i<s.length();i++){
             h[s.charAt(i)-'a']++;
-        }
-        PriorityQueue<int []>q=new PriorityQueue<>((x,y)->(Integer.compare(y[1],x[1])));
-        for(int i=0;i<26;i++){
-            if(h[i]>0) q.add(new int[]{i,h[i]});
-        }
-        // while(q.size()>0){
-        //     int v[]=q.poll();
-        //     System.out.println(v[0]+" "+v[1]);
-        // }
-        StringBuilder ans=new StringBuilder();
-        while(q.size()>0){
-            int c[]=q.poll();
-            char cur=(char)(c[0]+'a');
-            // System.out.println(cur);
-            if(ans.length()>0 && ans.charAt(ans.length()-1)==cur){
-                if(q.size()>0){
-                    int d[]=q.poll();
-                    ans.append((char)(d[0]+'a'));
-                    d[1]--;
-                    if(d[1]!=0) q.add(d);
-                    q.add(c);
-                }else{
-                    return "";
-                }
-            }else{
-                    ans.append((char)(c[0]+'a'));
-                    c[1]--;
-                    if(c[1]!=0) q.add(c);
+            if(h[s.charAt(i)-'a']>max){
+                max=h[s.charAt(i)-'a'];
+                maxv=s.charAt(i);
             }
         }
-        return ans.reverse().toString();
+        int n=s.length();
+        char c[]=new char[n];
+        if(max>(n+1)/2) return "";
+        int idx=0;
+        while(max>0){
+            c[idx]=maxv;
+            idx+=2;
+            max--;
+        }
+        for(int i=0;i<26;i++){
+            if(i!=(maxv-'a')){
+                while(h[i]>0){
+                    if(idx>=n){
+                        idx=1;
+                    }
+                    c[idx]=(char)(i+'a');
+                    idx+=2;
+                    h[i]--;
+                }
+            }
+        }
+        return String.valueOf(c);
     }
 }
