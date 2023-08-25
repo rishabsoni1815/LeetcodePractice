03657f6b3b84a4da029d1e220a6806d5d2372a96
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2023-08-25 22:12:34 +0530

    Time: 1 ms (91.57%), Space: 40.7 MB (77.17%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -2,15 +2,15 @@ class Solution {
     public boolean isInterleave(String a, String b, String c) {
         int n=a.length(),m=b.length(),l=c.length();
         Boolean dp[][]=new Boolean[n+1][m+1];
-        return help(a,b,c,n,m,l,0,0,0,dp);
+        return help(a,b,c,n,m,l,0,0,dp);
     }
-    boolean help(String a,String b,String c,int n,int m,int l,int i,int j,int k,Boolean dp[][]){
-        if(k>=l && i>=n && j>=m) return true;
-        if(k>=l) return false;
+    boolean help(String a,String b,String c,int n,int m,int l,int i,int j,Boolean dp[][]){
+        if((i+j)>=l && i>=n && j>=m) return true;
+        if((i+j)>=l) return false;
         if(dp[i][j]!=null) return dp[i][j];
         boolean ans=false;
-        if(i<n && a.charAt(i)==c.charAt(k)) ans|=help(a,b,c,n,m,l,i+1,j,k+1,dp);
-        if(j<m && b.charAt(j)==c.charAt(k)) ans|=help(a,b,c,n,m,l,i,j+1,k+1,dp);
+        if(i<n && a.charAt(i)==c.charAt(i+j)) ans|=help(a,b,c,n,m,l,i+1,j,dp);
+        if(j<m && b.charAt(j)==c.charAt(i+j)) ans|=help(a,b,c,n,m,l,i,j+1,dp);
         return dp[i][j]=ans;
     }
 }
