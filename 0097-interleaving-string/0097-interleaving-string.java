50baa4e0576308b591951f36c31eecf6092f45fb
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2023-08-25 23:22:23 +0530

    Time: 7 ms (33.37%), Space: 40.4 MB (92.33%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,8 +1,34 @@
 class Solution {
     public boolean isInterleave(String a, String b, String c) {
         int n=a.length(),m=b.length(),l=c.length();
-        Boolean dp[][]=new Boolean[n+1][m+1];
-        return help(a,b,c,n,m,l,0,0,dp);
+        //i+1 or i-1 only so can space optimise
+        if(n+m!=l) return false;
+        boolean dp[][]=new boolean[n+1][m+1];
+        
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                if(i==0||j==0){
+                    if(i==0 && j==0){
+                        dp[i][j]=true;   
+                    }else if(i==0){
+                        if(b.charAt(j-1)==c.charAt(i+j-1)) dp[i][j]=dp[i][j-1];
+                    }else{
+                        if(a.charAt(i-1)==c.charAt(i+j-1)) dp[i][j]=dp[i-1][j];
+                    }
+                }
+                if(i>0 && a.charAt(i-1)==c.charAt(i+j-1)){
+                    dp[i][j]|=dp[i-1][j];
+                }
+                if(j>0 && b.charAt(j-1)==c.charAt(i+j-1)){
+                    dp[i][j]|=dp[i][j-1];
+                }
+            }
+        }
+        return dp[n][m];
+        
+        
+        // Boolean dp[][]=new Boolean[n+1][m+1];
+        // return help(a,b,c,n,m,l,0,0,dp);
     }
     boolean help(String a,String b,String c,int n,int m,int l,int i,int j,Boolean dp[][]){
         if((i+j)>=l && i>=n && j>=m) return true;
