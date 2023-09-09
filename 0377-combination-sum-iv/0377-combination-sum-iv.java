6c916322066cc91802d7bfdd0215b1b793f0699e
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2023-09-09 15:06:18 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (73.34%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.java
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -1,17 +1,19 @@
 class Solution {
     public int combinationSum4(int[] a, int t) {
         int n=a.length;
-        Integer dp[][]=new Integer[n][t+1];
+        // Integer dp[][]=new Integer[n][t+1];//i doesn't matter as trying all nos in help fn of the array see through observation that we only need t
+        
+        Integer dp[]=new Integer[t+1];
         return help(a,0,n,t,dp);
     }
-    int help(int a[],int i,int n,int t,Integer dp[][]){
+    int help(int a[],int i,int n,int t,Integer dp[]){
         if(t==0) return 1;
         if(t<0 || i>=n) return 0;
         int ans=0;
-        if(dp[i][t]!=null) return dp[i][t];
+        if(dp[t]!=null) return dp[t];
         for(int j=0;j<n;j++){
             ans+=help(a,j,n,t-a[j],dp);
         }
-        return dp[i][t]=ans;
+        return dp[t]=ans;
     }
 }
