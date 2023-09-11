1b6f8b897e7a5020e8c53847f2196914d423ecc6
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2023-09-11 14:25:23 +0530

    Time: 5 ms (93.77%), Space: 44.4 MB (59.12%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.java
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.java
@@ -2,27 +2,16 @@ class Solution {
     public List<List<Integer>> groupThePeople(int[] a) {
         int n=a.length;
         HashMap<Integer,List<Integer>>h=new HashMap<>();
-        for(int i=0;i<n;i++){
-            if(h.containsKey(a[i])){
-                h.get(a[i]).add(i);
-            }else{
-                List<Integer>g=new ArrayList<>();
-                g.add(i);
-                h.put(a[i],g);
-            }
-        }
         List<List<Integer>>ans=new ArrayList<>();
-        for(int k:h.keySet()){
-            List<Integer>c=h.get(k);
-            List<Integer>temp=new ArrayList<>();
-            int s=0;
-            while(s<c.size()){
-                temp.add(c.get(s));
-                s++;
-                if(s%k==0){
-                    ans.add(new ArrayList<>(temp));
-                    temp=new ArrayList<>();
-                }
+        for(int i=0;i<n;i++){
+            if(!h.containsKey(a[i])){
+                h.put(a[i],new ArrayList<>());
+            }
+            List<Integer>g=h.get(a[i]);
+            g.add(i);
+            if(g.size()==a[i]){
+                ans.add(g);
+                h.put(a[i],new ArrayList<>());
             }
         }
         return ans;
