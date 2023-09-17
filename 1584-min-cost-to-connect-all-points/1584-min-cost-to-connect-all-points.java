5a8678bc8961fd1b8de7af620b231c52c7e6f3cb
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2023-09-18 01:02:43 +0530

    Time: 495 ms (22.54%), Space: 60.7 MB (41.17%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,30 +1,45 @@
+class Pair {
+    int node;
+    int distance;
+    public Pair(int distance, int node) {
+        this.node = node;
+        this.distance = distance;
+    }
+}
 class Solution {
-    public int minCostConnectPoints(int[][] points) {
-        int n = points.length, ans = 0;
-        HashSet<Integer> mst = new HashSet<>();
-        mst.add(0);
-        int[] dist = new int[n];
-        for(int i = 1; i < n; i++) dist[i] = findDist(points, 0, i);
-        while(mst.size() != n) {
-            // Find the node that has shortest distance
-            int next = -1;
-            for(int i = 0; i < n; i++) {
-                if(mst.contains(i)) continue;
-                if(next == -1 || dist[next] > dist[i]) next = i;
-            }
-            // Put the node into the Minning Spanning Tree
-            mst.add(next);
-            ans += dist[next];
-            // Update distance array
-            for(int i = 0; i < n; i++) {
-                if(!mst.contains(i)) {
-                    dist[i] = Math.min(dist[i], findDist(points, i, next));
+    public int minCostConnectPoints(int[][] p) {
+        return spanningTree(p,p.length);
+    }
+     static int spanningTree(int p[][],int n) {//prims add an node and then add closest of all its neighbour and keep on repeating -> https://www.youtube.com/watch?v=mJcZjjKzeqk&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=45
+        PriorityQueue<Pair> pq =
+            new PriorityQueue<Pair>((x, y) -> x.distance - y.distance);
+
+        int[] vis = new int[n];
+        // {wt, node}
+        pq.add(new Pair(0, 0));
+        int sum = 0;
+        while (pq.size() > 0) {
+            int wt = pq.peek().distance;
+            int node = pq.peek().node;
+            int nx=p[node][0];
+            int ny=p[node][1];
+            pq.remove();
+
+            if (vis[node] == 1) continue;
+            // add it to the mst
+            vis[node] = 1;
+            sum += wt;
+
+            for (int i = 0; i < n; i++) {
+                if (vis[i] == 0) {
+                    int edW = findDist(nx,ny,p[i][0],p[i][1]);
+                    pq.add(new Pair(edW, i));
                 }
             }
         }
-        return ans;
+        return sum;
     }
-    private int findDist(int[][] points, int a, int b) {
-        return Math.abs(points[a][0] - points[b][0]) + Math.abs(points[a][1] - points[b][1]);
+   static int findDist(int x,int y, int a, int b) {
+        return Math.abs(x - a) + Math.abs(y - b);
     }
 }
