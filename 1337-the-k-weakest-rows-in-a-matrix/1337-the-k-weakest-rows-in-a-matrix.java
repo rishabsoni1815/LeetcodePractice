c96d9302b1f97a8ebcf6d38ccd8dc30d53ea3694
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2023-09-18 14:38:09 +0530

    Time: 1 ms (99.31%), Space: 44.6 MB (59.35%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -1,35 +1,32 @@
-class Solution{
-    public int[] kWeakestRows(int[][] mat, int k) {
-        int[] result = new int[k];
-
-        //gather soldiers count into single dimensional array(per row)
-        int[] counts = new int[mat.length];
-        for (int i = 0; i < mat.length; i++) {
-            int h=0;
-            for (int j = 0; j < mat[i].length; j++) {
-                // if (mat[i][j] == 1) {
-                //     counts[i]++;
-                // }
-                h+=mat[i][j];
+class Solution {
+    public int[] kWeakestRows(int[][] a, int k) {
+        int n=a.length;
+        int m=a[0].length;
+        PriorityQueue<int []>q=new PriorityQueue<>((x,y)->(x[1]==y[1]?(y[0]-x[0]):(y[1]-x[1])));//max heap as needed k minimum and 0->index,1->freq of 1
+        for(int i=0;i<n;i++){
+            int c=find(a[i],m)+1;
+            q.add(new int[]{i,c});
+            if(q.size()>k){
+                q.poll();
             }
-            counts[i]=h;
         }
-
-        int indexer = 0;
-        while (k > 0) {
-            //make linear search k times and take minimum index to fill in result array
-            int minIndex = 0;
-            for (int i = 0; i < counts.length; i++) {
-                if (counts[i] < counts[minIndex]) {
-                    minIndex = i;
-                }
+        int ans[]=new int[k];
+        for(int i=k-1;i>=0;i--){
+            ans[i]=q.poll()[0];
+        }
+        return ans;
+    }
+    int find(int a[],int n){
+        int l=0,r=n-1,ans=-1;
+        while(l<=r){
+            int m=l+(r-l)/2;
+            if(a[m]==1){
+                ans=m;
+                l=m+1;
+            }else{
+                r=m-1;
             }
-
-            result[indexer++] = minIndex;
-            counts[minIndex] = Integer.MAX_VALUE;
-            k--;
         }
-
-        return result;
+        return ans;
     }
 }
