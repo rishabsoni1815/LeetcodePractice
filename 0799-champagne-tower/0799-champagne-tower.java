f5d1091ea908b5751a194d43b6f3b42388ed208b
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2023-09-24 17:33:49 +0530

    Time: 3 ms (84.41%), Space: 43 MB (81.32%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.java
+++ 0799-champagne-tower/0799-champagne-tower.java
@@ -1,24 +1,27 @@
 class Solution {
     public double champagneTower(int p, int r, int c) {
-        double dp[][]=new double[r+2][r+2];
+        double dp[]=new double[r+2];
         for(int i=0;i<=r;i++){
+            double next[]=new double[r+2];
             for(int j=0;j<=i;j++){
                 if(i==0){
-                    dp[i][j]=p;
+                    dp[j]=p;
                     if(p>1){
-                        dp[i+1][j]+=((p-1)/2.0);
-                        dp[i+1][j+1]+=((p-1)/2.0);
-                        dp[i][j]=1;
+                        next[j]+=((p-1)/2.0);
+                        next[j+1]+=((p-1)/2.0);
+                        dp[j]=1;
                     }
                 }else{
-                    if(dp[i][j]>1){
-                        dp[i+1][j]+=((dp[i][j]-1)/2.0);
-                        dp[i+1][j+1]+=((dp[i][j]-1)/2.0);
-                        dp[i][j]=1;
+                    if(dp[j]>1){
+                        next[j]+=((dp[j]-1)/2.0);
+                        next[j+1]+=((dp[j]-1)/2.0);
+                        dp[j]=1;
                     }
                 }
             }
+            if(i==r) return dp[c];//as else dp will be modified with next and dp[c] will be next[c] on line 25
+            dp=next;
         }
-        return dp[r][c];
+        return dp[c];
     }
 }
