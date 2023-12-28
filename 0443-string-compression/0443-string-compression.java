881cc68060e392f300c1b96b474dc7e2d349a4a7
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2023-12-28 16:19:58 +0530

    Time: 1 ms (99.61%), Space: 43.9 MB (18.83%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,18 +1,32 @@
 class Solution {
-    public int compress(char[] chars) {
-        int indexAns = 0, index = 0;
-        while(index < chars.length){
-            char currentChar = chars[index];
-            int count = 0;
-            while(index < chars.length && chars[index] == currentChar){
-                index++;
-                count++;
+    public int compress(char[] a) {
+        int n=a.length;
+        int i=0,j=0,c=1;
+        while(j<n){
+            j++;
+            while(j<n && a[j]==a[j-1]){
+                c++;
+                j++;
             }
-            chars[indexAns++] = currentChar;
-            if(count != 1)
-                for(char c : Integer.toString(count).toCharArray()) 
-                    chars[indexAns++] = c;
+            i=fill(i,a,j-1,c);
+            c=1;
         }
-        return indexAns;
+        return i;
+    }
+    int fill(int i,char a[],int j,int c){
+        if(c==1){
+            a[i]=a[j];
+            i++;
+            return i;
+        }else{
+            a[i]=a[j];
+            i++;
+            String s=String.valueOf(c);
+            for(int y=0;y<s.length();y++){
+                a[i]=s.charAt(y);
+                i++;
+            }
+            return i;
+        }
     }
 }
