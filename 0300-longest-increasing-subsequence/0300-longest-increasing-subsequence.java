04a8c4845324644234d20ec832686c22cba42e06
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2024-01-05 21:52:31 +0530

    Time: 8 ms (77.28%), Space: 44.6 MB (15.94%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,16 +1,29 @@
 class Solution {
     public int lengthOfLIS(int[] a) {
         int n=a.length;
-        int max=0;
-        int dp[]=new int[n];
+        ArrayList<Integer>c=new ArrayList<>();
         for(int i=0;i<n;i++){
-            for(int j=i;j>=0;j--){
-                if(a[j]<a[i]){
-                    dp[i]=Math.max(dp[i],dp[j]+1);
-                }
-            }           
-            max=Math.max(dp[i],max);
+            if(c.size()==0) c.add(a[i]);
+            else if(c.size()>0 && a[i]>c.get(c.size()-1)){
+                c.add(a[i]);
+            }else if(c.size()>0 && a[i]<c.get(c.size()-1)){
+                int x=find(a[i],c);
+                c.set(x,a[i]);
+            }
         }
-        return max+1;
+        return c.size();
+    }
+    int find(int x,ArrayList<Integer>a){
+        int ans=0,l=0,r=a.size()-1;
+        while(l<=r){
+            int m=l+(r-l)/2;
+            if(a.get(m)>=x){
+                ans=m;
+                r=m-1;
+            }else{
+                l=m+1;
+            }
+        }
+        return ans;
     }
 }
