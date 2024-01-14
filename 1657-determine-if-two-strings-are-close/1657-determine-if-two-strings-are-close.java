3ee87450b5f6e1124f6e43f2b26c8d10642e919e
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2024-01-14 15:10:08 +0530

    Time: 12 ms (69.52%), Space: 45.6 MB (20.97%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
@@ -9,6 +9,10 @@ class Solution {
             h2[b.charAt(i)-'a']++;
             if(h1[b.charAt(i)-'a']==0) return false;		// if one has a letter which another one doesn't have, dont exist as Operation 2: is on char both existing not with any char (swap btw itself) 
         }
+        
+        // ***************** instead of doing this can also sort both h1 and h2 and check as tc = o(nlogn)=o(1) only as size is 26 *******************
+        
+        
         HashMap<Integer,Integer>m1=new HashMap<>();
         for(int i=0;i<26;i++){
             if(h1[i]>0) m1.put(h1[i],m1.getOrDefault(h1[i],0)+1);
