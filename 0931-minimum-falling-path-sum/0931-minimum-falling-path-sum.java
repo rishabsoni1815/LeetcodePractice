723832d41a7edd517b3e331378289c0d3b3a5f24
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2024-01-19 16:32:14 +0530

    Time: 1 ms (99.30%), Space: 45.5 MB (19.62%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,20 +1,21 @@
 class Solution {
     public int minFallingPathSum(int[][] a) {
         int n=a.length;
-        int ans=Integer.MAX_VALUE;
-        Integer dp[][]=new Integer[n][n];
-        for(int i=0;i<n;i++){
-            ans=Math.min(ans,help(0,i,a,n,dp));
+        int m=a[0].length;
+        Integer dp[][]=new Integer[n][m];
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<m;i++){
+            min=Math.min(min,help(0,i,a,dp));
         }
-        return ans;
+        return min;
     }
-    int help(int i,int j,int a[][],int n,Integer dp[][]){
-        if(i>=n || j>=n || i<0 || j<0) return Integer.MAX_VALUE;
-        if(i==n-1) return a[i][j];
+    int help(int i,int j,int a[][],Integer dp[][]){
+        if(i==a.length-1) return a[i][j];
         if(dp[i][j]!=null) return dp[i][j];
-        int o1=help(i+1,j-1,a,n,dp);
-        int o2=help(i+1,j,a,n,dp);
-        int o3=help(i+1,j+1,a,n,dp);
-        return dp[i][j]=a[i][j]+Math.min(o1,Math.min(o2,o3));
+        int min=Integer.MAX_VALUE;
+        if(i+1<a.length) min=Math.min(min,help(i+1,j,a,dp));
+        if(i+1<a.length && j-1>=0) min=Math.min(min,help(i+1,j-1,a,dp));
+        if(i+1<a.length && j+1<(a[0].length)) min=Math.min(min,help(i+1,j+1,a,dp));
+        return dp[i][j]=min+a[i][j];
     }
 }
