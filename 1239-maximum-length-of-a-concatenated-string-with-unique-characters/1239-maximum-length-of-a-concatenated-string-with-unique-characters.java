3fc115aa09c3e8d3261f809829913f4478177c27
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2024-01-23 18:59:14 +0530

    Time: 1 ms (100.00%), Space: 40.7 MB (95.97%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -1,27 +1,27 @@
 class Solution {
     public int maxLength(List<String> a) {
+        //learn dp and dp with bitmask
         int n=a.size();
-        int c[]=new int[26];
-        return dfs(0,n,a,c);
+        int h=1<<26;
+        return dfs(0,n,a,h);
     }
-    int dfs(int i,int n,List<String>a,int c[]){
+    int dfs(int i,int n,List<String>a,int h){
         if(i==n) return 0;
+        int ch=h;
         String s=a.get(i);
-        int c1[]=new int[26];
-        for(int j=0;j<26;j++) c1[j]=c[j];
-        boolean flag=true;
+        boolean flag=false;
         for(int j=0;j<s.length();j++){
-            c[s.charAt(j)-'a']++;
-            if(c[s.charAt(j)-'a']>1) {
-                flag=false;
+            int x=s.charAt(j)-'a';
+            int y=((1<<x)&ch);//checking if xth bit is set in ch
+            if(y>0){
+                flag=true;
                 break;
             }
+            ch=((1<<x)|ch);//setting xth bit of ch to 1
         }
         int res=0;
-        if(flag){
-            res = s.length()+dfs(i+1,n,a,c);
-        }
-        res=Math.max(res,dfs(i+1,n,a,c1));
+        if(flag==false) res= s.length()+dfs(i+1,n,a,ch);
+        res=Math.max(res,dfs(i+1,n,a,h));
         return res;
     }
 }
