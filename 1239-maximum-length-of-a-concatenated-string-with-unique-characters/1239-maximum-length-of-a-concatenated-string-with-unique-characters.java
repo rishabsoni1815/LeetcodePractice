4140560a3160acc0c6a8d9c32366eaab9770a5f7
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2024-01-23 11:15:03 +0530

    Time: 4 ms (93.95%), Space: 44.7 MB (37.80%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -1,37 +1,27 @@
 class Solution {
-    public int maxLength(List<String> arr) {
-        int[] ch = new int[26];
-        Arrays.fill(ch, 0);
-        return dfs(arr, 0, ch);
+    public int maxLength(List<String> a) {
+        int n=a.size();
+        int c[]=new int[26];
+        return dfs(0,n,a,c);
     }
-    
-    HashMap<String, Integer> memo = new HashMap<>();
-    public int dfs(List<String> arr, int idx, int[] ch) {
-        if (idx == arr.size()) {
-            return 0;
-        }
-        String key = "" + idx+","+ch.toString();
-        if (memo.containsKey(key)) return memo.get(key);
-        int result = Integer.MIN_VALUE;
-        String s = arr.get(idx);
-        boolean flag = true;
-        int[] ch1 = new int[26];
-        for (int i = 0; i < 26; i++) ch1[i] = ch[i];
-        for (int j = 0; j < s.length(); j++) {
-            int c = s.charAt(j) - 'a';
-            ch1[c]++;
-            if (ch1[c] > 1) {
-                flag = false;
+    int dfs(int i,int n,List<String>a,int c[]){
+        if(i==n) return 0;
+        String s=a.get(i);
+        int c1[]=new int[26];
+        for(int j=0;j<26;j++) c1[j]=c[j];
+        boolean flag=true;
+        for(int j=0;j<s.length();j++){
+            c[s.charAt(j)-'a']++;
+            if(c[s.charAt(j)-'a']>1) {
+                flag=false;
                 break;
             }
         }
-        
-        if (flag) {
-            result = Math.max(result, s.length() + dfs(arr, idx+1, ch1));
+        int res=0;
+        if(flag){
+            res = s.length()+dfs(i+1,n,a,c);
         }
-        
-        result = Math.max(result, dfs(arr, idx+1, ch));
-        memo.put(key, result);
-        return result;
+        res=Math.max(res,dfs(i+1,n,a,c1));
+        return res;
     }
 }
