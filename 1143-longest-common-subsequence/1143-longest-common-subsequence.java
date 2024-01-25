3231b35bc499b60fb4b083549e395a303226b33f
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2024-01-25 18:15:18 +0530

    Time: 29 ms (25.14%), Space: 62.8 MB (5.01%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,46 +1,18 @@
 class Solution {
-    public int longestCommonSubsequence(String s, String t) {
-        int n=s.length(),m=t.length();
-        int dp[]=new int[m+1];
-        for(int i=1;i<=n;i++){
-            int prev[]=new int[m+1];
-            for(int j=1;j<=m;j++){
-                if(s.charAt(i-1)==t.charAt(j-1)){
-                   prev[j]=1+dp[j-1];
-               }else{
-                  prev[j]=Math.max(dp[j],prev[j-1]);
-               }
-            }
-            dp=prev;
-        }
-       return dp[m];
+    public int longestCommonSubsequence(String a, String b) {
+        Integer dp[][]=new Integer[a.length()][b.length()];
+        return help(a,b,a.length()-1,b.length()-1,dp);
     }
-    /*
-    only i-1 so optimise
-    public static int lcs(String s, String t) {
-		int n=s.length(),m=t.length();
-        int dp[][]=new int[n+1][m+1];
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(s.charAt(i-1)==t.charAt(j-1)){
-                   dp[i][j]=1+dp[i-1][j-1];
-               }else{
-                  dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
-               }
-            }
+    int help(String a,String b,int n,int m,Integer dp[][]){
+        if(n==-1 && m==-1) return 0;
+        if(n<0 || m<0){
+            return 0;
         }
-       return dp[n][m];
+        if(dp[n][m]!=null) return dp[n][m];
+        if(a.charAt(n)==b.charAt(m)){
+            return dp[n][m]=1+help(a,b,n-1,m-1,dp);
+        }else{
+            return dp[n][m]=Math.max(help(a,b,n-1,m,dp),help(a,b,n,m-1,dp));
+        }
     }
-    
-    
-    */
-   static int help(int i,int j,String s,String t,Integer dp[][]){
-       if(i<0||j<0) return 0;
-       if(dp[i][j]!=null) return dp[i][j];
-       if(s.charAt(i)==t.charAt(j)){
-           return dp[i][j]=1+help(i-1,j-1,s,t,dp);
-       }else{
-           return dp[i][j]=Math.max(help(i-1,j,s,t,dp),help(i,j-1,s,t,dp));
-       }
-   }
 }
