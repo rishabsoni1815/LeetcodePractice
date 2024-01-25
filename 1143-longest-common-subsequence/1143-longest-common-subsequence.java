8e57dff3a7f03c2468cdabec91e052daf35da9d0
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2024-01-25 18:38:28 +0530

    Time: 18 ms (93.57%), Space: 45.2 MB (92.80%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -4,19 +4,37 @@ class Solution {
         // Integer dp[][]=new Integer[a.length()][b.length()];
         // return help(a,b,a.length()-1,b.length()-1,dp);
         
-        //tabulation
+         //tabulation with space optimisation as above only needed dp[i-1]
         int n=a.length(),m=b.length();
-        int dp[][]=new int[n+1][m+1];//as have to handle 0,0 in dp[i-1][j-1]
+        int dp[]=new int[m+1];//as have to handle 0,0 in dp[j-1]
         for(int i=1;i<=n;i++){
+            int prev[]=new int[m+1];
             for(int j=1;j<=m;j++){
                 if(a.charAt(i-1)==b.charAt(j-1)){//as 1 based indexing
-                    dp[i][j]=1+dp[i-1][j-1];
+                    prev[j]=1+dp[j-1];
                 }else{
-                    dp[i][j]=Math.max(dp[i][j-1],dp[i-1][j]);
-                }                
+                    prev[j]=Math.max(prev[j-1],dp[j]);
+                }
             }
+            dp=prev;
         }
-        return dp[n][m];
+        return dp[m];
+        
+        //tabulation
+        // int n=a.length(),m=b.length();
+        // int dp[][]=new int[n+1][m+1];//as have to handle 0,0 in dp[i-1][j-1]
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=m;j++){
+        //         if(a.charAt(i-1)==b.charAt(j-1)){//as 1 based indexing
+        //             dp[i][j]=1+dp[i-1][j-1];
+        //         }else{
+        //             dp[i][j]=Math.max(dp[i][j-1],dp[i-1][j]);
+        //         }                
+        //     }
+        // }
+        // return dp[n][m];
+        
+        
     }
     
     
