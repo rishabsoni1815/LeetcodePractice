a3660d9e80dc38091a1caa45b56488c08ffe7a12
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2024-02-12 11:04:25 +0530

    Time: 2 ms (78.11%), Space: 51.2 MB (5.71%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,17 +1,16 @@
 class Solution {
     public int majorityElement(int[] a) {
-        int cans=a[0],ans=a[0];
+        int ans=a[0];
         int sum=0;
         for(int i=0;i<a.length;i++){
-            if(a[i]==cans){
+            if(a[i]==ans){
                 sum++;
             }
             else{
                 sum--;
                 if(sum==0){
                     sum=1;
-                    ans=a[i];
-                    cans=a[i];   
+                    ans=a[i]; 
                 }
             }
         }
@@ -21,7 +20,7 @@ class Solution {
                 csum++;
             }
         }
-        if(csum>=(a.length/2)) return ans;
+        if(csum>(a.length/2)) return ans;
         return -1;
     }
 }
