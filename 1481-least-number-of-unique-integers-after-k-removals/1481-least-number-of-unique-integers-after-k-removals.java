0a197f2c76cfb1a1adf8e7788bd67b8ef46d4786
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2024-02-16 12:47:00 +0530

    Time: 35 ms (94.08%), Space: 58.3 MB (50.65%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.java
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.java
@@ -7,22 +7,49 @@ class Pair{
     }
 }
 class Solution {
+    
+    // public int findLeastNumOfUniqueInts(int[] a, int k) {
+    //     PriorityQueue<Pair>q=new PriorityQueue<>((x,y)->(x.freq-y.freq));
+    //     HashMap<Integer,Integer>h=new HashMap<>();
+    //     for(int i=0;i<a.length;i++){
+    //         h.put(a[i],h.getOrDefault(a[i],0)+1);
+    //     }
+    //     if(k==0) return h.size();
+    //     for(int key:h.keySet()){
+    //         int v=h.get(key);
+    //         q.add(new Pair(key,v));
+    //     }
+    //     while(k>0){
+    //         int cfreq=q.poll().freq;
+    //         k-=cfreq;
+    //         if(k<=0) return q.size()+(k==0?0:1);
+    //     }
+    //     return 0;
+    // }
+    
+    
+    // one thing to note is the fre of frequencies is btw 0 to n so can optimise this using tha
     public int findLeastNumOfUniqueInts(int[] a, int k) {
-        PriorityQueue<Pair>q=new PriorityQueue<>((x,y)->(x.freq-y.freq));
         HashMap<Integer,Integer>h=new HashMap<>();
         for(int i=0;i<a.length;i++){
             h.put(a[i],h.getOrDefault(a[i],0)+1);
         }
         if(k==0) return h.size();
+        int ffreq[]=new int[a.length+1];
         for(int key:h.keySet()){
             int v=h.get(key);
-            q.add(new Pair(key,v));
+            ffreq[v]++;
         }
-        while(k>0){
-            int cfreq=q.poll().freq;
-            k-=cfreq;
-            if(k<=0) return q.size()+(k==0?0:1);
+        
+        int cur=0,rem=h.size();
+        for(int i=1;i<ffreq.length;i++){
+            if(i*ffreq[i]<=k){
+                k-=i*ffreq[i];//i*ffreq[i] is the number of elements with frequency as i
+                rem-=ffreq[i];
+            }else{
+                return rem-(k/i);
+            }
         }
-        return 0;
+        return rem;
     }
 }
