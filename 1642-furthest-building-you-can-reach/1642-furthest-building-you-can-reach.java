bebccbaab60e9d7fb20789d46e4a96096706e834
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2024-02-17 16:47:26 +0530

    Time: 21 ms (20.22%), Space: 61 MB (6.65%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.java
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.java
@@ -1,28 +1,19 @@
 class Solution {
-    /*
-    
-    Heap heap store k height difference that we need to use ladders.
-Each move, if the height difference d > 0,
-we push d into the priority queue pq.
-If the size of queue exceed ladders,
-it means we have to use bricks for one move.
-Then we pop out the smallest difference, and reduce bricks.
-If bricks < 0, we can't make this move, then we return current index i.
-If we can reach the last building, we return A.length - 1.
-    
-    */
-    public int furthestBuilding(int[] heights, int bricks, int ladders) {
-        PriorityQueue<Integer>q=new PriorityQueue<>();
-        for(int i=0;i<heights.length-1;i++){
-            int d=heights[i+1]-heights[i];
-            if(d>0){
-                q.add(d);
+    public int furthestBuilding(int[] h, int b, int l) {
+        int n=h.length;
+        int csum=0;
+        PriorityQueue<Integer>q=new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=0;i<n;i++){
+            if(i+1<n && h[i]<h[i+1]){
+                q.add((h[i+1]-h[i]));
+                csum+=(h[i+1]-h[i]);
             }
-            if(q.size()>ladders){
-                bricks-=q.remove();
+            if(csum>b){
+                if(l==0) return i;
+                l--;
+                if(q.size()>0) csum-=q.poll();
             }
-            if(bricks<0) return i;
         }
-        return heights.length-1;
+        return n-1;
     }
 }
