b1bdf966d03e61128a42659ac63329eb71bd41ef
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2024-04-07 11:35:54 +0530

    Time: 19 ms (66.51%), Space: 45.2 MB (70.23%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -1,5 +1,5 @@
 class Solution {
-    public String minRemoveToMakeValid(String s) {
+/*    public String minRemoveToMakeValid(String s) {//delte will take more complexity so we will just mark and in last make new string
         int n=s.length();
         Stack<Integer>c=new Stack<>();
         StringBuilder sb = new StringBuilder("");
@@ -17,5 +17,32 @@ class Solution {
             sb.deleteCharAt(c.pop());
         }
         return sb.toString();
+    }
+    */
+    public String minRemoveToMakeValid(String s) {
+        Stack<Integer>stack=new Stack<>();
+        int n=s.length();
+        char c[]=s.toCharArray();
+        StringBuilder ans=new StringBuilder();
+        for(int i=0;i<n;i++){
+            if(c[i]=='('){
+                stack.push(i);
+            }else if(c[i]==')'){
+                if(stack.size()>0 && c[stack.peek()]=='('){
+                    stack.pop();
+                }else{
+                    c[i]='*';//as it is invalid and we will remove it afterwords
+                }
+            }
+        }
+        while(stack.size()>0){
+            c[stack.pop()]='*';//this is why we were putting index into stack
+        }
+        for(int i=0;i<n;i++){
+            if(c[i]!='*'){
+                ans.append(c[i]);
+            }
+        }
+        return ans.toString();
     }
 }
