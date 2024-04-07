e3b036381e9b778f4200f4624b4b502de9552d1d
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2024-04-07 11:09:15 +0530

    Time: 2 ms (85.73%), Space: 42.3 MB (43.23%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.java
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -1,22 +1,15 @@
 class Solution {
-    public String makeGood(String a) {
-        StringBuilder s=new StringBuilder();
-        int i=0,n=a.length();
-        while(i<n){
-            char c=a.charAt(i);
-            char p='-';
-            if(s.length()!=0) {
-                p=s.charAt(s.length()-1);
-                if(c-'a'==p-'A'||c-'A'==p-'a'){
-                    s.deleteCharAt(s.length()-1);
-                }else{
-                    s.append(c);
-                }
-            }else{
-                s.append(c);
-            }
-            i++;
-        }
-        return s.toString();
-    }
+public String makeGood(String s) {
+        
+    StringBuilder sb = new StringBuilder("");
+        
+    for(int i=0; i< s.length(); i++){
+        if(sb.length() !=0 && Math.abs(sb.charAt(sb.length()-1) - s.charAt(i)) ==32){
+            sb.deleteCharAt(sb.length()-1);
+            continue;
+        } 
+        sb.append(Character.toString(s.charAt(i)));
+    }        
+        return sb.toString();
+    } 
 }
