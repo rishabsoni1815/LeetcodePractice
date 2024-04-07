d30c9feba3f39f2d9dc60404e62731f629ce1901
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2024-04-07 11:34:05 +0530

    Time: 28 ms (26.62%), Space: 45.3 MB (56.14%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -1,28 +1,21 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        Stack<Integer>stack=new Stack<>();
         int n=s.length();
-        char c[]=s.toCharArray();
-        StringBuilder ans=new StringBuilder();
-        for(int i=0;i<n;i++){
-            if(c[i]=='('){
-                stack.push(i);
-            }else if(c[i]==')'){
-                if(stack.size()>0 && c[stack.peek()]=='('){
-                    stack.pop();
-                }else{
-                    c[i]='*';//as it is invalid and we will remove it afterwords
-                }
+        Stack<Integer>c=new Stack<>();
+        StringBuilder sb = new StringBuilder("");
+        for(int i=0;i<n;i++){//idea is to just keep track of unwanted braces in stack with there indices and remove at last
+            if(s.charAt(i)=='('){
+                c.push(i);
+            }else if(s.charAt(i)==')'){
+                if(c.size()>0 && sb.charAt(c.peek())=='('){
+                    c.pop();    
+                }else c.push(i);
             }
+            sb.append(s.charAt(i));
         }
-        while(stack.size()>0){
-            c[stack.pop()]='*';//this is why we were putting index into stack
+        while(c.size()>0){
+            sb.deleteCharAt(c.pop());
         }
-        for(int i=0;i<n;i++){
-            if(c[i]!='*'){
-                ans.append(c[i]);
-            }
-        }
-        return ans.toString();
+        return sb.toString();
     }
 }
