c3d543cf93435ea9172c0070efcada44cad6c7a1
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2024-04-14 14:34:47 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (83.55%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
@@ -16,13 +16,13 @@
 class Solution {
     public int sumOfLeftLeaves(TreeNode root) {
         if(root==null) return 0;
-        if(root.left==null) return sumOfLeftLeaves(root.right);
-        else {
+       
+        else if(root.left!=null){
             if(root.left.left==null&&root.left.right==null){
                 return root.left.val+sumOfLeftLeaves(root.right);
-            }else{
-                return sumOfLeftLeaves(root.left)+sumOfLeftLeaves(root.right);
             }
         }
+         return sumOfLeftLeaves(root.left)+sumOfLeftLeaves(root.right);
+            
     }
 }
