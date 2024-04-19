4d404639a09ce3276147360d08c191cf51104959
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2024-04-19 12:18:46 +0530

    Time: 0 ms (100.00%), Space: 44.4 MB (95.87%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,27 +1,62 @@
 class Solution {
     public boolean isBipartite(int[][] g) {
-        Queue<Integer>q=new LinkedList<>();
-        HashMap<Integer,Integer>v=new HashMap<>();
-        for(int j=0;j<g.length;j++){
-            if(!v.containsKey(j)){
-                v.put(j,0);
-                q.add(j);
-                while(q.size()>0){
-                    int s=q.size();
-                    for(int i=0;i<s;i++){
-                        int c=q.poll();
-                        for(int nei:g[c]){
-                            if(v.containsKey(nei)==false){
-                                v.put(nei,1-v.get(c));
-                                q.add(nei);
-                            }else{
-                                if(v.get(c)==v.get(nei)) return false;
-                            }
-                        }
-                    }
-                }  
-            } 
+        //dfs
+       int n=g.length;
+       boolean v[]=new boolean[n];
+       int col[]=new int[n];Arrays.fill(col,-1); 
+       for(int i=0;i<n;i++){
+           if(v[i]==false){
+               col[i]=0;
+               if(dfs(i,v,g,col)==false) return false;
+           }
+       }
+       return true;
+    }
+    boolean dfs(int i,boolean v[],int g[][],int col[]){
+        v[i]=true;
+        for(int k=0;k<g[i].length;k++){
+            int nei=g[i][k];
+            if(v[nei]==false){
+                col[nei]=1-col[i];
+                if(dfs(nei,v,g,col)==false) return false;
+            }else{
+                if(col[nei]==col[i]) return false;
+            }
         }
         return true;
     }
+    
+    
+    
+    // public boolean isBipartite(int[][] g) {
+    //     //bfs
+    //     int n=g.length;
+    //    boolean v[]=new boolean[n];
+    //     int col[]=new int[n];
+    //     Arrays.fill(col,-1);
+    //     // Code here
+    //     for(int i=0;i<n;i++){
+    //         if(v[i]==false){
+    //             Queue<Integer>q=new LinkedList<>();
+    //             q.add(i);
+    //             v[i]=true;
+    //             col[i]=0;
+    //             while(!q.isEmpty()){
+    //                 int t=q.poll();
+    //                 for(int k=0;k<g[t].length;k++){
+    //                     int nei=g[t][k];
+    //                     if(v[nei]==false){
+    //                         v[nei]=true;
+    //                         q.add(nei);
+    //                         col[nei]=1-col[t];
+    //                     }
+    //                     else{
+    //                         if(col[nei]==col[t]) return false;
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return true;
+    // }
 }
