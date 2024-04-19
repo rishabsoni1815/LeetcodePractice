b7dc277512b0998806a8b9367c18c547e51f1b6c
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2024-04-19 12:18:21 +0530

    Time: 5 ms (22.58%), Space: 44.4 MB (95.87%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,29 +1,26 @@
 class Solution {
-    public boolean isBipartite(int[][] a) {
-        ArrayList<ArrayList<Integer>>g=new ArrayList<>();
-        for(int i=0;i<a.length;i++){
-            ArrayList<Integer>c=new ArrayList<>();
-            for(int j=0;j<a[i].length;j++){
-                c.add(a[i][j]);
-            }
-            g.add(c);
-        }
+    public boolean isBipartite(int[][] g) {
+        Queue<Integer>q=new LinkedList<>();
         HashMap<Integer,Integer>v=new HashMap<>();
-        for(int i=0;i<a.length;i++){
-            if(!v.containsKey(i)){
-                if(dfs(i,0,g,v)==false) return false;
-            }
-        }
-        return true;
-    }
-    boolean dfs(int i,int c,ArrayList<ArrayList<Integer>>g,HashMap<Integer,Integer>v){
-        v.put(i,c);
-        for(int nei:g.get(i)){
-            if(!v.containsKey(nei)){
-                if(dfs(nei,1-c,g,v)==false) return false;
-            }else{
-                if(v.get(nei)==c) return false;
-            }
+        for(int j=0;j<g.length;j++){
+            if(!v.containsKey(j)){
+                v.put(j,0);
+                q.add(j);
+                while(q.size()>0){
+                    int s=q.size();
+                    for(int i=0;i<s;i++){
+                        int c=q.poll();
+                        for(int nei:g[c]){
+                            if(v.containsKey(nei)==false){
+                                v.put(nei,1-v.get(c));
+                                q.add(nei);
+                            }else{
+                                if(v.get(c)==v.get(nei)) return false;
+                            }
+                        }
+                    }
+                }  
+            } 
         }
         return true;
     }
