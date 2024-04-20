2e6cbfbc6050eb609dcdd960641750c483aed8ed
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2024-04-20 12:06:30 +0530

    Time: 5 ms (68.75%), Space: 43.6 MB (99.36%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,32 +1,41 @@
 class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-   List<Integer> [] g=new ArrayList[numCourses];//represent array of List<Integer> important 
-                    //        ^ 
-                    //        |
-             //     not ArrayList<Integer>[numCourses]
-    int[] indegree = new int[numCourses];
-    for(int i=0;i<numCourses;i++){
-        g[i]=new ArrayList<>();
-    }
-   for(int t[]:prerequisites){
-       g[t[0]].add(t[1]);
-       indegree[t[1]]++;
-   }
-    Queue<Integer> q = new LinkedList<Integer>();
-    for (int i = 0; i < numCourses; i++) {
-      if (indegree[i] == 0) {
-        q.add(i);
-      }
-    }
-        int i=0;
-    while (!q.isEmpty()) {
-      int node = q.remove();
-        i++;
-        for(int s:g[node]){
-            indegree[s]--;
-            if(indegree[s]==0) q.add(s);
+    public boolean canFinish(int n, int[][] a) {
+        int m=a.length;
+        ArrayList<ArrayList<Integer>>g=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            g.add(new ArrayList<>());
         }
+        
+        int in[]=new int[n];
+        for(int i=0;i<m;i++){
+            in[a[i][0]]++;
+            g.get(a[i][1]).add(a[i][0]);
+        }
+        
+        // HashSet<Integer>v=new HashSet<>();//can do without this also as we are checking in[nei]==0 which will be only once for every node
+        Queue<Integer>q=new LinkedList<>();
+        for(int i=0;i<n;i++){
+            if(in[i]==0) {
+                // v.add(i);
+                q.add(i);
+            }
+        }
+        int ci=0;
+        while(q.size()>0){
+            int s=q.size();
+            for(int i=0;i<s;i++){
+                int c=q.poll();
+                ci++;
+                for(int nei:g.get(c)){
+                    in[nei]--;
+                    // if(!v.contains(nei) && in[nei]==0){
+                    if(in[nei]==0){
+                        q.add(nei);
+                    }
+                }
+            }
+        }
+        if(ci==n) return true;
+        return false;
     }
-    return (i == numCourses);
-  }
 }
