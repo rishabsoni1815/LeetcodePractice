27ca0c46899fc372f6abc9a8d75612e6edc6b516
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2024-04-25 17:09:19 +0530

    Time: 64 ms (41.27%), Space: 61.1 MB (33.33%) - LeetHub

--- 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.java
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.java
@@ -1,18 +1,40 @@
 class Solution {
     public int longestIdealString(String s, int k) {
-        int DP[] = new int[26], ans = 1;
-        
-        for (int ch = 0, n = s.length(); ch < n; ch++) {
-            int i = s.charAt(ch) - 'a';
-            DP[i] = DP[i] + 1;
-            
-            for (int j = Math.max(0, i - k); j <= Math.min(25, i + k); j++)
-                if (j != i)
-                    DP[i] = Math.max(DP[i], DP[j] + 1);
-            
-            ans = Math.max(ans, DP[i]);
+        //o(n*26) -> dp[i][k] will check how much max len is possible till i having kas the char at i
+        int n=s.length();
+        int dp[][]=new int[n][26];
+        dp[0][s.charAt(0)-'a']=1;
+        int ans=1;
+        // for(int i=0;i<n;i++) Arrays.fill(dp[i],-1);
+        for(int i=1;i<n;i++){
+            int x=s.charAt(i)-'a';
+            dp[i][x]=1;
+            for(int j=Math.max(0,x-k);j<=(Math.min(25,x+k));j++){
+                dp[i][x]=Math.max(dp[i][x],1+dp[i-1][j]);
+            }
+            for(int j=0;j<26;j++){
+                dp[i][j]=Math.max(dp[i][j],dp[i-1][j]);
+                if(i==n-1) ans=Math.max(ans,dp[i][j]);
+            }
         }
-        
         return ans;
-    }
+    }    
+    
+    // o(n^2)
+    // public int longestIdealString(String s, int k) {
+    //     int n=s.length();
+    //     int dp[]=new int[n];
+    //     dp[0]=1;
+    //     int ans=1;
+    //     for(int i=1;i<n;i++){
+    //         dp[i]=1;
+    //         for(int j=0;j<i;j++){
+    //             if(Math.abs(s.charAt(i)-s.charAt(j)) <=k){
+    //                 dp[i]=Math.max(dp[i],1+dp[j]);
+    //             }
+    //         }
+    //         ans=Math.max(ans,dp[i]);
+    //     }
+    //     return ans;
+    // }
 }
