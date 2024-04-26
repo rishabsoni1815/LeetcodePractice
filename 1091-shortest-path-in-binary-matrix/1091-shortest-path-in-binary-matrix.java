3c25a4e3847c5b4fc635edd44fa54c5431b04283
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2024-04-26 15:57:14 +0530

    Time: 13 ms (89.81%), Space: 45.5 MB (37.52%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,31 +1,27 @@
 class Solution {
+    int dirx[]={1,1,1,-1,-1,-1,0,0};
+    int diry[]={0,1,-1,0,1,-1,1,-1};
     public int shortestPathBinaryMatrix(int[][] a) {
-        int n=a.length;
-        int dirx[]={0,0,1,-1,1,1,-1,-1};
-        int diry[]={-1,1,0,0,-1,1,-1,1};
+        int n=a.length,m=a[0].length;
+        if(a[0][0]==1 || a[n-1][m-1]==1) return -1;
         Queue<int []>q=new LinkedList<>();
-        boolean v[][]=new boolean [n][n];
-        if(a[0][0]==1||a[n-1][n-1]==1) return -1;
-        if(n-1==0&&a[0][0]==0) return 1;
         q.add(new int[]{0,0});
-        v[0][0]=true;
-        int ans=1;
+        int ans=0;
+        a[0][0]=1;
         while(q.size()>0){
-            int s=q.size();
             ans++;
-            while(s-->0){
-                int t[]=q.poll();
+            int ss=q.size();
+            for(int j=0;j<ss;j++){
+                int c[]=q.poll();
+                if(c[0]==n-1 && c[1]==m-1) return ans;
                 for(int i=0;i<8;i++){
-                    int x=dirx[i]+t[0];
-                    int y=diry[i]+t[1];
-                    if(x>=0&&y>=0&&x<n&&y<n&&a[x][y]!=1&&v[x][y]==false){
-                        if(x==n-1&&y==n-1) {
-                            return ans;
-                        }
-                        q.add(new int[]{x,y});
-                        v[x][y]=true;
+                    int nx=c[0]+dirx[i];
+                    int ny=c[1]+diry[i];
+                    if(nx >=0 && ny>=0 && nx<n && ny<m && a[nx][ny]==0){
+                        a[nx][ny]=1;
+                        q.add(new int[]{nx,ny});
                     }
-                }
+                }   
             }
         }
         return -1;
