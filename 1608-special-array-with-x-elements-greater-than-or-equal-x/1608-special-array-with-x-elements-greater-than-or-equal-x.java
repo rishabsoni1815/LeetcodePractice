a475fbc07a3e9d839888f0e1c2ab661b9cd4b8b0
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2024-05-27 14:01:13 +0530

    Time: 3 ms (13.96%), Space: 41.5 MB (21.41%) - LeetHub

---
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int specialArray(int[] a) {
+        int n=a.length;
+        Arrays.sort(a);
+        for(int i=0;i<=1002;i++){
+            if(find(a,n,i)) return i;
+        }
+        return -1;
+    }
+    boolean find(int a[],int n,int k){//finding smallest(want >= than k) index of k
+        int l=0,r=n-1,ans=n;
+        while(l<=r){
+            int m=(l+(r-l)/2);
+            if(a[m]>=k){
+                ans=m;
+                r=m-1;
+            }else{
+                l=m+1;
+            }
+        }
+        if((n-ans)==k) return true;
+        return false;
+    }
+}
