c377ef39e24ce75ad814a70709358af365f4f9ef
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2024-06-22 12:34:06 +0530

    Time: 42 ms (13.25%), Space: 55.4 MB (12.84%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -1,9 +1,5 @@
-
-/*
-// o(n) time and o(n) space ->sliding window space-> o(1)
 class Solution {
     public int numberOfSubarrays(int[] a, int k) {
-        //like number of subarrays with sum k here sum is analoged as number of odd elements
         int n=a.length;
         HashMap<Integer,Integer>h=new HashMap<>();
         h.put(0,1);
@@ -16,28 +12,5 @@ class Solution {
             h.put(c,h.getOrDefault(c,0)+1);
         }
         return ans;
-    }
-}
-*/
-
-//sliding window O(n) time and O(1) space
-class Solution {
-    public int numberOfSubarrays(int[] nums, int k) {
-        int oddcount=0;
-        int res=0;
-        int i=0;
-        int count=0;
-        for(int j=0;j<nums.length;j++){
-            if(nums[j]%2==1){
-                oddcount++;
-                count=0;
-            }
-            while(oddcount==k){
-                if(nums[i++]%2==1) oddcount--;
-                count++;
-            }
-            res+=count;
-        }
-        return res;
     }
 }
