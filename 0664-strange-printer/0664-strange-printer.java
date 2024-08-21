a892c15cb124b67b3f40d8542d259c6bd094a84e
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2024-08-21 20:05:57 +0530

    Time: 17 ms (51.02%), Space: 43.5 MB (22.04%) - LeetHub

--- 0664-strange-printer/0664-strange-printer.java
+++ 0664-strange-printer/0664-strange-printer.java
@@ -1,19 +1,23 @@
 class Solution {
     public int strangePrinter(String s) {
-        //other sol. in submissions
         int n=s.length();
         Integer dp[][]=new Integer[n][n];
         return help(0,n-1,n,s,dp);
     }
     int help(int i,int j,int n,String s,Integer dp[][]){
+        if(i>j) return Integer.MAX_VALUE-1;
         if(i==j) return 1;
-        //matrix chain multiplication
-        if(dp[i][j]!=null) return dp[i][j];
-        int min=Integer.MAX_VALUE;
-        for(int k=i;k<j;k++){
-            min=Math.min(min,help(i,k,n,s,dp)+help(k+1,j,n,s,dp));
+        if(s.charAt(i)==s.charAt(j)){
+            return dp[i][j]=help(i,j-1,n,s,dp);// abcda can be ans of abcd only as we can make aaaaa and then make it abcda so ans of abcda is of abcd only
+        }else{
+            if(dp[i][j]!=null) return dp[i][j];
+            int max=1+help(i,j-1,n,s,dp);  //extra s[j] after string s[i...j-1]
+            for(int k=i+1;k<j;k++){
+                if(s.charAt(k)==s.charAt(j)){
+                    max=Math.min(max,help(i,k-1,n,s,dp)+help(k,j-1,n,s,dp));// if s[k]=s[j] then above rule applies of abcda same as abcd so help(k,j-1) and ans is this + left string that is s[i...k-1] 
+                }
+            }
+            return dp[i][j]=max;
         }
-        if(s.charAt(i)==s.charAt(j)) min--;
-        return dp[i][j]=min;
     }
 }
