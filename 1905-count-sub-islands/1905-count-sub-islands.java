6990a30c0b7543951f9519c24ec733c82e441d03
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2024-08-28 20:18:13 +0530

    Time: 22 ms (56.06%), Space: 86 MB (24.53%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.java
+++ 1905-count-sub-islands/1905-count-sub-islands.java
@@ -1,30 +1,36 @@
 class Solution {
-    int y=0;
+    int dirx[]={1,-1,0,0};
+    int diry[]={0,0,1,-1};
     public int countSubIslands(int[][] a, int[][] b) {
-        int n=a.length;
-        int m=a[0].length;
+        int n=a.length,m=a[0].length;
         int ans=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(a[i][j]==1&&b[i][j]==1){
-                    dfs(i,j,a,b,n,m);
-                    if(y!=5) ans++;
-                    y=0;
+                if(b[i][j]==1){
+                    if(dfs(i,j,a,b,n,m)){
+                        ans++;
+                    }
                 }
             }
         }
         return ans;
     }
-   void dfs(int i,int j,int a[][],int b[][],int n,int m){
-        if(i<0||i>=n||j<0||j>=m||b[i][j]==0) return;
-       if(a[i][j]!=b[i][j]){
-          y=5;
-           return;
-       }
+    boolean dfs(int i,int j,int a[][],int b[][],int n,int m){
+        if(a[i][j]!=b[i][j]){
+            return false;
+        }
         b[i][j]=0;
-        dfs(i+1,j,a,b,n,m);
-         dfs(i-1,j,a,b,n,m);
-         dfs(i,j+1,a,b,n,m);
-         dfs(i,j-1,a,b,n,m);
+        boolean flag=false;
+        for(int k=0;k<4;k++){
+            int nx=i+dirx[k];
+            int ny=j+diry[k];
+            if(nx>=0 && nx<n && ny>=0 && ny<m && b[nx][ny]==1){
+                if(dfs(nx,ny,a,b,n,m)==false){
+                    flag|=true;
+                }
+            }
+        }
+        if(flag==true) return false;
+        return true;
     }
 }
