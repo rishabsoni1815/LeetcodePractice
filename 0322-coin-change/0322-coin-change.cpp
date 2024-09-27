d8c4489e88e6c77deb9f325e53a6e7d9c3451bfd
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2024-09-28 00:20:34 +0530

    Time: 74 ms (9.27%), Space: 47.9 MB (22.98%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int helper(vector<vector<int>> &dp,vector<int> &arr, int sum , int i )
+{
+    if(sum==0)
+    return 0;
+    if(sum<0 || i==arr.size())
+        return INT_MAX;
+    if(dp[i][sum]!=-2)
+    return dp[i][sum];
+    int Pick = helper(dp,arr,sum - arr[i],i);
+    int nonPick= helper(dp,arr ,sum ,i+1);
+        if (Pick != INT_MAX) 
+            Pick += 1;
+    dp[i][sum]=min( Pick, nonPick);
+    return dp[i][sum] ;
+}
+    int coinChange(vector<int>& coins, int amount) {
+        int n= coins.size();
+       vector<vector<int>> dp(n,vector<int>(amount+1,-2));
+        int result = helper(dp,coins,amount,0);
+        return (result == INT_MAX) ? -1 : result; 
+        
+    }
+};
