6f7f03d8ee6e7d81255eb0f5624230eff61392cc
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2024-09-29 22:09:48 +0530

    Time: 33 ms (9.65%), Space: 62.3 MB (5.05%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,24 +1,24 @@
 class Solution {
     public int longestCommonSubsequence(String a, String b) {
         //memoisation
-        // Integer dp[][]=new Integer[a.length()][b.length()];
-        // return help(a,b,a.length()-1,b.length()-1,dp);
+        Integer dp[][]=new Integer[a.length()][b.length()];
+        return help(a,b,a.length()-1,b.length()-1,dp);
         
          //tabulation with space optimisation as above only needed dp[i-1]
-        int n=a.length(),m=b.length();
-        int dp[]=new int[m+1];//as have to handle 0,0 in dp[j-1]
-        for(int i=1;i<=n;i++){
-            int prev[]=new int[m+1];
-            for(int j=1;j<=m;j++){
-                if(a.charAt(i-1)==b.charAt(j-1)){//as 1 based indexing
-                    prev[j]=1+dp[j-1];
-                }else{
-                    prev[j]=Math.max(prev[j-1],dp[j]);
-                }
-            }
-            dp=prev;
-        }
-        return dp[m];
+        // int n=a.length(),m=b.length();
+        // int dp[]=new int[m+1];//as have to handle 0,0 in dp[j-1]
+        // for(int i=1;i<=n;i++){
+        //     int prev[]=new int[m+1];
+        //     for(int j=1;j<=m;j++){
+        //         if(a.charAt(i-1)==b.charAt(j-1)){//as 1 based indexing
+        //             prev[j]=1+dp[j-1];
+        //         }else{
+        //             prev[j]=Math.max(prev[j-1],dp[j]);
+        //         }
+        //     }
+        //     dp=prev;
+        // }
+        // return dp[m];
         
         //tabulation
         // int n=a.length(),m=b.length();
@@ -40,7 +40,7 @@ class Solution {
     
     //memoisation 
     int help(String a,String b,int n,int m,Integer dp[][]){
-        if(n==-1 && m==-1) return 0;
+        // if(n==-1 && m==-1) return 0;
         if(n<0 || m<0){
             return 0;
         }
