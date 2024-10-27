6b9d6c8bc632670fb80df31f293ea172e019dda8
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2024-10-27 13:25:42 +0530

    Time: 64 ms (9.76%), Space: 54.6 MB (85.04%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
@@ -1,17 +1,43 @@
 class Solution {
-    public int countSquares(int[][] matrix) {
-          int rows = matrix.length, cols = rows > 0 ? matrix[0].length : 0;
-        int[][] dp = new int[rows + 1][cols + 1];
+
+    public int solve(int i, int j, int[][] grid, int[][] dp) {
+        // If the cell lies outside the grid, return 0.
+        if (i >= grid.length || j >= grid[0].length) {
+            return 0;
+        }
+        if (grid[i][j] == 0) {
+            return 0;
+        }
+
+        // If we have already visited this cell, return the memoized value.
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
+        // Find the answer for the cell to the right of the current cell.
+        int right = solve(i, j + 1, grid, dp);
+        // Find the answer for the cell to the diagonal of the current cell.
+        int diagonal = solve(i + 1, j + 1, grid, dp);
+        // Find the answer for the cell below the current cell.
+        int below = solve(i + 1, j, grid, dp);
+
+        return dp[i][j] = 1 + Math.min(right, Math.min(diagonal, below));
+    }
+
+    public int countSquares(int[][] grid) {
         int ans = 0;
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= cols; j++) {
-                if (matrix[i-1][j-1] ==1){
-                    dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
-                    //min as for dp[i][j-1] the a[i-1][j].a[i-2][j]... should also be 1
-                    ans+=dp[i][j];
-                }
+        int[][] dp = new int[grid.length][grid[0].length];
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                Arrays.fill(dp[i], -1);
             }
         }
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                ans += solve(i, j, grid, dp);
+            }
+        }
+
         return ans;
     }
 }
