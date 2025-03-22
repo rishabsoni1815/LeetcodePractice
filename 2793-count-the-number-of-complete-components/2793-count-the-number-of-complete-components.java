7b7e1e5e9d83d54a67d1a075316648bed8efe650
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2025-03-22 13:13:12 +0530

    Time: 13 ms (41.61%), Space: 45.2 MB (56.68%) - LeetHub

---
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int countCompleteComponents(int n, int[][] e) {
+        ArrayList<ArrayList<Integer>>g=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            g.add(new ArrayList<Integer>());
+        }
+        for(int i=0;i<e.length;i++){
+            g.get(e[i][0]).add(e[i][1]);
+            g.get(e[i][1]).add(e[i][0]);
+        }
+        HashSet<Integer>v=new HashSet<>();
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(!v.contains(i)){
+                int c[]=new int[2];//0->vertices,1->edges
+                dfs(i,v,g,c);
+                int cnt=(c[0]*(c[0]-1))/2;
+                if(2*cnt==c[1]) ans++;
+            }
+        }
+        return ans;
+    }
+    void dfs(int i,HashSet<Integer>v,ArrayList<ArrayList<Integer>>g,int c[]){
+        v.add(i);
+        c[0]++;
+        for(int nei:g.get(i)){
+            c[1]++;
+            if(!v.contains(nei)){
+                dfs(nei,v,g,c);
+            }
+        }
+    }
+}
