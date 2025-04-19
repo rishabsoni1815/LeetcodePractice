e216c487fdcccb12bc2af9abe1eb713bf68cf782
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2025-04-20 00:49:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.java
@@ -0,0 +1,9 @@
+class Solution {
+    public long countFairPairs(int[] a, int l, int r) {
+        int n=a.length;
+        Arrays.sort(a);
+        for(int i=0;i<n;i++){
+            
+        }
+    }
+}
