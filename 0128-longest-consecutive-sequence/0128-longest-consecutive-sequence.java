7f24deb74e70b0189ecc5d51094d13c2962f4304
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2025-04-30 02:03:35 +0530

    Time: 33 ms (35.19%), Space: 60.9 MB (67.72%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -7,16 +7,27 @@ class Solution {
            h.add(a[i]);
         }
         for(int i=0;i<n;i++){
-            int cans=0;
-            if(h.contains(a[i]+1)==false){
-                int c=a[i];
-                while(h.contains(c)){
-                    h.remove(c);
-                    c--;
-                    cans++;
+            if(!h.contains(a[i]+1)){
+                int u=a[i]-1,cmax=1;
+                while(h.contains(u)){
+                    h.remove(u);
+                    u--;
+                    cmax++;
                 }
+               max=Math.max(max,cmax);
             }
-            max=Math.max(max,cans);
+            /*
+                the idea of thinking is this the end of the lcs if it is then see all present less
+                than it and to prevent o(n^2) see if a[i]+1 is present if it is then this elemnt
+                can't be end as there is some other bigger end present 
+                by this set can be used to supplement the size of max of all elements array approach
+                {
+                 approach of  o(max. of all emements) to find max and then check if while(max-1) is 
+                 present if not then update cmax to 1 else cmax++
+                }
+                we can check all while(max-1) present in this set only by thinking of it as end of the final subsequence for more details check gfg article of this approach 
+                Link --->   https://www.geeksforgeeks.org/longest-consecutive-subsequence/
+            */
         }
         return max;
     }
