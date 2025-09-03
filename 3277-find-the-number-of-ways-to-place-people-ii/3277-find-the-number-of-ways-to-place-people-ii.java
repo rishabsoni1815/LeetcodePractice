599ee282547933c30904086c50a8bb4517e426df
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2025-09-04 01:59:33 +0530

    Time: 65 ms (60.9%), Space: 45 MB (92.48%) - LeetHub

---
+++ 3277-find-the-number-of-ways-to-place-people-ii/3277-find-the-number-of-ways-to-place-people-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int numberOfPairs(int[][] a) {
+        int n=a.length;
+        Arrays.sort(a,(x,y)->((x[0]==y[0] ? (y[1]-x[1]) :(x[0]-y[0]) )));
+        int ans=0;
+        for(int i=0;i<n;i++){
+            int cmax=-Integer.MIN_VALUE;
+            for(int j=i+1;j<n;j++){
+                int fr=a[i][0];
+                int fc=a[i][1];
+                int sr=a[j][0];
+                int sc=a[j][1];
+                if(fc<sc) continue;//not upper left
+                //if top right corner less than cur max then yes else no and update cmax now
+                if(sc>cmax) {
+                    cmax=sc;
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+}
