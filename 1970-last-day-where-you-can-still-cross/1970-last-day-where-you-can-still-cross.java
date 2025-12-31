af6397e32c2a88ce73ecc6d22b6e132f43050ee1
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2025-12-31 16:47:23 +0530

    Time: 130 ms (10.15%), Space: 76.7 MB (78.17%) - LeetHub

--- 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.java
+++ 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.java
@@ -1,52 +1,97 @@
 class Solution {
-    private int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    public boolean canCross(int row, int col, int[][] cells, int day) {
-        int[][] grid = new int[row][col];
-        for (int i = 0; i < day; ++i) {
-            int r = cells[i][0] - 1, c = cells[i][1] - 1;
-            grid[r][c] = 1;
-        }
-        
-        for (int i = 0; i < day; ++i) {
-            grid[cells[i][0] - 1][cells[i][1] - 1] = 1;
-        }
-        
-        for (int i = 0; i < col; ++i) {
-            if (grid[0][i] == 0 && dfs(grid, 0, i, row, col)) {
-                return true;
+    public int latestDayToCross(int r, int c, int[][] a) {
+        int ans=0,n=a.length;
+        int l=0,ri=n-1;
+        while(l<=ri){
+            int m=(l+(ri-l)/2);
+            if(help(m,a,r,c)){
+                ans=m;
+                l=m+1;
+            }else{
+                ri=m-1;
             }
         }
-        return false;
-        
+        return ans+1;
+    }
+    boolean help(int day, int[][] cells, int r, int c) {
+        int total = r * c;
+        int TOP = total;
+        int BOTTOM = total + 1;
+
+        DSU dsu = new DSU(total + 2);
+
+        boolean[][] water = new boolean[r][c];
+
+        // mark flooded cells till "day"
+        for (int i = 0; i <= day; i++) {
+            water[cells[i][0] - 1][cells[i][1] - 1] = true;
+        }
+
+        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+        // connect land cells
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (water[i][j]) continue;
+
+                int id = i * c + j;
+
+                // connect top row to TOP
+                if (i == 0) {
+                    dsu.add(id, TOP);
+                }
+
+                // connect bottom row to BOTTOM
+                if (i == r - 1) {
+                    dsu.add(id, BOTTOM);
+                }
+
+                // connect neighbors
+                for (int[] d : dirs) {
+                    int ni = i + d[0];
+                    int nj = j + d[1];
+
+                    if (ni >= 0 && ni < r && nj >= 0 && nj < c && !water[ni][nj]) {
+                        int nid = ni * c + nj;
+                        dsu.add(id, nid);
+                    }
+                }
+            }
+        }
+        return dsu.findParent(TOP) == dsu.findParent(BOTTOM);
     }
 
-    private boolean dfs(int[][] grid, int r, int c, int row, int col) {
-        if (r < 0 || r >= row || c < 0 || c >= col || grid[r][c] != 0) {
-            return false;
+    class DSU{
+        int p[];
+        int n;
+        int r[];
+        DSU(int n){
+            this.n=n;
+            p=new int[n];
+            for(int i=0;i<n;i++){
+                p[i]=i;
+            }
+            r=new int[n];
+            Arrays.fill(r,1);
         }
-        if (r == row - 1) {
+
+        int findParent(int i){
+            if(i==p[i]) return i;
+            return p[i]=findParent(p[i]);
+        }
+
+        boolean add(int i,int j){
+            int p1=findParent(i);
+            int p2=findParent(j);
+            if(p1==p2) return false;
+            if(r[p1]>r[p2]){
+                p[p2]=p1;
+            }else if(r[p2]>r[p1]){
+                p[p1]=p2;
+            }else{
+                p[p1]=p2;
+                r[p2]++;
+            }
             return true;
         }
-        grid[r][c] = -1;
-        for (int[] dir : directions) {
-            int newR = r + dir[0], newC = c + dir[1];
-            if (dfs(grid, newR, newC, row, col)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public int latestDayToCross(int row, int col, int[][] cells) {
-        int left = 1, right = row * col;
-        while (left < right) {
-            int mid = right - (right - left) / 2;
-            if (canCross(row, col, cells, mid)) {
-                left = mid;
-            } else {
-                right = mid - 1;
-            }
-        }
-        return left;
     }
 }
