3578e8be613606f4431b4df32fc68f0f2a99cbee
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2026-01-06 00:14:10 +0530

    Time: 1025 ms (25.57%), Space: 276.6 MB (14.08%) - LeetHub

--- 3791-number-of-balanced-integers-in-a-range/3791-number-of-balanced-integers-in-a-range.java
+++ 3791-number-of-balanced-integers-in-a-range/3791-number-of-balanced-integers-in-a-range.java
@@ -4,10 +4,10 @@ class Solution {
     }
     long countSpecialNumbers(long n) {
         char[] a = String.valueOf(n).toCharArray();
-        Long dp[][][][][]=new Long[a.length+1][2][2][150][150];
-        return help(0, a, true,true,dp,0,0) - 1 ;//to remove 000 case
+        Long dp[][][][]=new Long[a.length+1][2][150][150];
+        return help(0, a,true,dp,0,0) - 1 ;//to remove 000 case and no need of leadingzero here ans event if we append leading zero odd places and even places sum is same so no need to handle leading zero seperately
     }
-    long help(int i, char a[], boolean tight,boolean leadingZero,Long dp[][][][][],int odd,int even) {
+    long help(int i, char a[], boolean tight,Long dp[][][][],int odd,int even) {
         if (i == a.length) {
             return (odd==even)?1:0;
         }
@@ -16,15 +16,10 @@ class Solution {
         if(tight){
             cap=a[i]-'0';
         }
-        if(dp[i][tight?0:1][leadingZero?0:1][odd][even]!=null) return dp[i][tight?0:1][leadingZero?0:1][odd][even];
+        if(dp[i][tight?0:1][odd][even]!=null) return dp[i][tight?0:1][odd][even];
         for (int x = 0; x <= cap; x++) {
-            if(leadingZero && x==0){
-                // no need to remove as we didn't counted this since it is leading 0
-                ans+=help(i+1,a,false,true,dp,odd,even);
-            }else{
-                ans+=help(i+1,a,tight && (x==cap),false,dp,(i%2!=0)?odd+x:odd,(i%2==0)?even+x:even);
-            }
+                ans+=help(i+1,a,tight && (x==cap),dp,(i%2!=0)?odd+x:odd,(i%2==0)?even+x:even);
         }
-        return dp[i][tight?0:1][leadingZero?0:1][odd][even] = ans;
+        return dp[i][tight?0:1][odd][even] = ans;
     }
 }
