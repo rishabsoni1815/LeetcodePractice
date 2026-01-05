00219b828bb206b0c561b2751fd27db3de4da0e6
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2026-01-06 00:37:03 +0530

    Time: 2531 ms (5.01%), Space: 274.4 MB (19.6%) - LeetHub

--- 3791-number-of-balanced-integers-in-a-range/3791-number-of-balanced-integers-in-a-range.java
+++ 3791-number-of-balanced-integers-in-a-range/3791-number-of-balanced-integers-in-a-range.java
@@ -4,23 +4,22 @@ class Solution {
     }
     long countSpecialNumbers(long n) {
         char[] a = String.valueOf(n).toCharArray();
-        Long dp[][][][]=new Long[a.length+1][2][201][2];//maintaining diff and pos not odd,even
-        return help(0, a,true,dp,0,0) - 1 ;//to remove 000 case
+        Long dp[][][][]=new Long[a.length+1][2][150][150];
+        return help(0, a,true,dp,0,0) - 1 ;//to remove 000 case and no need of leadingzero here ans event if we append leading zero odd places and even places sum is same so no need to handle leading zero seperately
     }
-    long help(int i, char a[], boolean tight,Long dp[][][][],int diff,int pos) {
+    long help(int i, char a[], boolean tight,Long dp[][][][],int odd,int even) {
         if (i == a.length) {
-            return diff==0?1:0;
+            return (odd==even)?1:0;
         }
         long ans = 0;
         int cap=9;
         if(tight){
             cap=a[i]-'0';
         }
-        if(dp[i][tight?0:1][diff+100][pos]!=null) return dp[i][tight?0:1][diff+100][pos];//adding 20 offset to avoid negative values while storing in dp array
+        if(dp[i][tight?0:1][odd][even]!=null) return dp[i][tight?0:1][odd][even];
         for (int x = 0; x <= cap; x++) {
-            int ndiff=(pos==1)?diff+x:diff-x;
-            ans+=help(i+1,a,tight && (x==cap),dp,ndiff,1-pos);
+                ans+=help(i+1,a,tight && (x==cap),dp,(i%2!=0)?odd+x:odd,(i%2==0)?even+x:even);
         }
-        return dp[i][tight?0:1][diff+100][pos] = ans;
+        return dp[i][tight?0:1][odd][even] = ans;
     }
 }
