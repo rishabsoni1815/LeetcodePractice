432f4891d437e091ec2b372f3aa477c8aea30dd9
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2026-01-10 16:47:52 +0530

    Time: 32 ms (8.64%), Space: 46.6 MB (27.72%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
@@ -1,34 +1,57 @@
 class Solution {
     public int minimumDeleteSum(String a, String b) {
-        int n=a.length(),m=b.length();
-        Integer dp[][]=new Integer[n+1][m+1];
-        return help(0,0,n,m,a,b,dp);
-    }
-    int help(int i,int j,int n,int m,String a,String b,Integer dp[][]){
-        if(i>=n && j>=m) return 0;
-        else if(i>=n || j>=m){//imp if one finished other left add all left sum
-            if(i<n){
-                int x=0;
-                for(int k=i;k<n;k++){
-                    x+=a.charAt(k);
+        // Integer dp[][] = new Integer[a.length()][b.length()];
+        // return help(a, b, a.length() - 1, b.length() - 1, dp);
+        int n = a.length(), m = b.length();
+        int dp[][] = new int[n + 1][m + 1];
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = 0;
+                    continue;
+                } else if (i == 0) {
+                    dp[i][j] = b.charAt(j - 1) + dp[i][j - 1];
+                    continue;
+                } else if (j == 0) {
+                    dp[i][j] = a.charAt(i - 1) + dp[i - 1][j];
+                    continue;
                 }
-                return dp[i][j]=x;
-            }else{
-                int x=0;
-                for(int k=j;k<m;k++){
-                    x+=b.charAt(k);
+                if (a.charAt(i - 1) == b.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    int o1 = a.charAt(i - 1) + dp[i - 1][j];
+                    int o2 = b.charAt(j - 1) + dp[i][j - 1];
+                    int o3 = a.charAt(i - 1) + b.charAt(j - 1) + dp[i - 1][j - 1];
+                    dp[i][j] = Math.min(o1, Math.min(o2, o3));
                 }
-                return dp[i][j]=x;
             }
         }
-        if(dp[i][j]!=null) return dp[i][j];
-        if(a.charAt(i)==b.charAt(j)){
-            return help(i+1,j+1,n,m,a,b,dp);
-        }else{
-            int o1=((int)(a.charAt(i)+b.charAt(j))+help(i+1,j+1,n,m,a,b,dp));
-            int o2=((int)(a.charAt(i))+help(i+1,j,n,m,a,b,dp));
-            int o3=((int)(b.charAt(j))+help(i,j+1,n,m,a,b,dp));
-            return dp[i][j]=Math.min(o1,Math.min(o2,o3));
-        }
+        return dp[n][m];
     }
+
+    // int help(String a, String b, int i, int j, Integer dp[][]) {
+    //     if (i < 0 || j < 0) {
+    //         if (i < 0 && j < 0)
+    //             return 0;
+    //         else {
+    //             int c = 0;
+    //             if (i >= 0) {
+    //                 for (int k = i; k >= 0; k--)
+    //                     c += a.charAt(k);
+    //             } else {
+    //                 for (int k = j; k >= 0; k--)
+    //                     c += b.charAt(k);
+    //             }
+    //             return c;
+    //         }
+    //     }
+    //     if (dp[i][j] != null)
+    //         return dp[i][j];
+    //     if (a.charAt(i) == b.charAt(j))
+    //         return dp[i][j] = help(a, b, i - 1, j - 1, dp);
+    //     int o1 = a.charAt(i) + help(a, b, i - 1, j, dp);
+    //     int o2 = b.charAt(j) + help(a, b, i, j - 1, dp);
+    //     int o3 = a.charAt(i) + b.charAt(j) + help(a, b, i - 1, j - 1, dp);
+    //     return dp[i][j] = Math.min(o3, Math.min(o1, o2));
+    // }
 }
