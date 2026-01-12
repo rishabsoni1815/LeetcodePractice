d89d2614a0519ebc863b513c6f990d9b03ae9ba5
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2026-01-12 20:05:28 +0530

    Time: 1 ms (95.28%), Space: 44.9 MB (48.65%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.java
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.java
@@ -4,13 +4,8 @@ class Solution {
         for(int i=1;i<a.length;i++){
             int dx=(int)Math.abs(a[i][0]-a[i-1][0]);
             int dy=(int)Math.abs(a[i][1]-a[i-1][1]);
-            int min=Math.min(dx,dy);
-            ans+=min;
-            if(dx>dy){
-                ans+=(dx-min);
-            }else if(dx<dy){
-                ans+=(dy-min);
-            }
+            int max=Math.max(dx,dy);
+            ans+=max;
         }
         return ans;
     }
