434a0aabd09368c266c070a932e77ff27e526ff8
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2026-01-23 15:38:47 +0530

    Time: 763 ms (23.53%), Space: 126.1 MB (98.4%) - LeetHub

--- 3510-minimum-pair-removal-to-sort-array-ii/3510-minimum-pair-removal-to-sort-array-ii.java
+++ 3510-minimum-pair-removal-to-sort-array-ii/3510-minimum-pair-removal-to-sort-array-ii.java
@@ -1,106 +1,63 @@
-import java.util.*;
-
 class Solution {
     public int minimumPairRemoval(int[] a) {
-        int n = a.length;
-        // 1. TreeSet to store pairs ordered by sum (and then index)
-        TreeSet<Node> set = new TreeSet<>((x, y) -> 
-            (x.nextSum != y.nextSum) ? Long.compare(x.nextSum, y.nextSum) : Integer.compare(x.idx, y.idx)
-        );
-
-        Node head = new Node(a[0], 0);
-        Node prev = head;
-        int badPairs = 0; // TRACKER for unsorted pairs
-        
-        // 2. Build List, Set, and Initial Bad Count
-        for (int i = 0; i < n - 1; i++) {
-            Node nextNode = new Node(a[i + 1], i + 1);
-            prev.next = nextNode;
-            nextNode.prev = prev;
-            
-            // Check if this specific pair is "bad" (unsorted)
-            if (prev.val > nextNode.val) badPairs++;
-
-            // Calculate sum and add to set
-            prev.nextSum = prev.val + nextNode.val;
-            set.add(prev);
-            
-            prev = nextNode;
+        TreeSet<Node>set=new TreeSet<>((x,y)-> (x.nextSum!=y.nextSum) ? (Long.compare(x.nextSum,y.nextSum)) : (Integer.compare(x.idx,y.idx)));
+        int bad=0;
+        Node prev=null;
+        for(int i=0;i<a.length;i++){
+            Node node=new Node((long)a[i],(long)a[i],i);
+            if(i<a.length-1) node.nextSum+=a[i+1];
+            node.prev=prev;
+            if(prev!=null) prev.next=node;
+            prev=node;
+            if(i<a.length-1 && a[i]>a[i+1]) bad++;
+            if(i<a.length-1) set.add(node);//tail cant be in set since its next is null so no next sum
         }
-
-        int cnt = 0;
-        Node dummy = new Node(-1, -1);
-        dummy.next = head;
-        head.prev = dummy;
-
-        // 3. Loop until badPairs is 0 (Array is non-decreasing)
-        while (badPairs > 0) {
-            Node cur = set.first(); // O(1) retrieval
-            
-            // Logic to update pointers, set, AND badPairs count
-            badPairs = updateListSetAndCount(cur, set, dummy, badPairs);
+        int cnt=0;
+        while(bad>0){
+            Node min=set.first();
+            bad = update(min,set,bad);
             cnt++;
         }
         return cnt;
     }
-
-    int updateListSetAndCount(Node cur, TreeSet<Node> set, Node dummy, int badPairs) {
-        Node left = cur.prev;
-        Node right = cur.next;
-        Node farRight = right.next;
-
-        // --- A. Remove Old Bad Counts ---
-        // 1. Check Left -> Cur
-        if (left != dummy && left.val > cur.val) badPairs--;
-        // 2. Check Cur -> Right (The pair being merged)
-        if (cur.val > right.val) badPairs--;
-        // 3. Check Right -> FarRight
-        if (farRight != null && right.val > farRight.val) badPairs--;
-
-        // --- B. Remove Nodes from Set ---
-        set.remove(cur); 
-        if (right.next != null) set.remove(right);
-        if (left != dummy) set.remove(left);
-
-        // --- C. Merge Logic (Cur merged into Right) ---
-        right.val = cur.val + right.val; // Sum value
+    int update(Node min,TreeSet<Node>set,int bad){
+        Node left=min.prev;
+        Node right=min.next;
+        Node farRight=right.next;
         
-        // Pointer updates
-        right.prev = left;
-        left.next = right;
-
-        // --- D. Update Sums and Set ---
-        if (right.next != null) {
-            right.nextSum = right.val + right.next.val;
-            set.add(right);
-        }
-        if (left != dummy) {
-            left.nextSum = left.val + right.val;
-            set.add(left);
-        }
+        if(min.val>right.val) bad--;
+        if(left!=null && left.val>min.val) bad--;
+        if (farRight != null && right.val > farRight.val) bad--;
         
-        // --- E. Add New Bad Counts ---
-        // 1. Check Left -> Right (New connection)
-        if (left != dummy && left.val > right.val) badPairs++;
-        // 2. Check Right -> FarRight (Right has new value)
-        if (farRight != null && right.val > farRight.val) badPairs++;
+        set.remove(min);
+        if(left!=null) set.remove(left);
+        set.remove(right);
 
-        // Cleanup
-        cur.next = null;
-        cur.prev = null;
+        long nsum=min.nextSum+right.nextSum-right.val;
+        min.val=min.nextSum;
+        min.nextSum=nsum;
+        if(left!=null) left.nextSum=left.val+min.val;
+        min.next=farRight;
+        if(farRight!=null) farRight.prev=min;
 
-        return badPairs;
+        if(min.next!=null && min.val>min.next.val) bad++;
+        if(left!=null && left.val>left.next.val) bad++;
+
+        if(left != null) set.add(left);
+        if(min.next != null) set.add(min);//tail cant be in set since its next is null so no next sum
+
+        return bad;
     }
-
-    class Node {
+    class Node{
+        long val;
+        long nextSum;
         Node prev;
         Node next;
-        long nextSum;
-        long val;
         int idx;
-        Node(long val, int idx) {
-            this.val = val;
-            this.idx = idx;
+        Node(long val,long nextSum,int idx){
+            this.val=val;
+            this.nextSum=nextSum;
+            this.idx=idx;
         }
     }
 }
