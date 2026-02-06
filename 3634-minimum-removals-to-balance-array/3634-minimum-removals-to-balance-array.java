b8850569b699a6b87231d3278da12ae1d28b6e78
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2026-02-06 12:35:07 +0530

    Time: 25 ms (58.63%), Space: 86.3 MB (19.68%) - LeetHub

--- 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.java
+++ 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.java
@@ -3,15 +3,27 @@ class Solution {
         int n = a.length;
         Arrays.sort(a);
         int ans = n-1;
-        for (int i = 0; i < n; i++) {
-            int idx = cost(i, a, k);
-            int c = n - idx - 1;
-            c += i;//prev ele
-            ans = Math.min(ans, c);
+        // for (int i = 0; i < n; i++) {
+        //     int idx = cost(i, a, k);
+        //     int c = n - idx - 1;
+        //     c += i;//prev ele
+        //     ans = Math.min(ans, c);
+        // }
+
+
+        //greedy 2 pointer first inc right pointer then no choice left so inc l and take min
+        int i=0,j=0;
+        while(i<n){
+            while(j<n && (long)a[i]*k>=a[j]){
+                j++;
+            }
+            ans=Math.min(ans,i+n-j);
+            i++;
         }
         return ans;
     }
 
+//binary search
     int cost(int x, int a[], int k) {
         int l = x, r = a.length - 1, ans = -1;
         while (l <= r) {
