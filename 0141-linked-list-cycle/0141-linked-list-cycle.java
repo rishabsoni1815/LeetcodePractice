8a175bf575cbaae56e536540c022d550a142c4ab
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2026-02-10 23:16:40 +0530

    Time: 0 ms (100%), Space: 46.8 MB (70.7%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -6,21 +6,17 @@
  *     ListNode(int x) {
  *         val = x;
  *         next = null;
- *     }    https://www.youtube.com/watch?v=8NBkGnY5iJ8&list=PLBTCXiV4KOCRFRn0ZIoaNPX8TYHgbAVPZ&index=4&t=0s
+ *     }
  * }
  */
 public class Solution {
-    /*
-     let say when slow entern the cycle the distance between s and f is k after this f is +2 ans s is +1 so resultant dis b/w them is k+1 then k+2 ...k+x and one time k+x will be n where n is length of loop so they will meet as k is increasing by 1 always not by 2 or 3 so bount to become n (for some x k+x=n ) 
-    
-    */
     public boolean hasCycle(ListNode head) {
-        ListNode t1=head,t2=head;
-        while(t1!=null&&t1.next!=null){
-            // if(t1==t2) return true;//wrong for intial cond. we haave initialised t1 ans t2 both with head so same always
-            t2=t2.next;
-            t1=t1.next.next;
-            if(t1==t2) return true;
+        if(head==null) return false;
+        ListNode slow=head,fast=head;
+        while(fast.next!=null && fast.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast) return true;
         }
         return false;
     }
