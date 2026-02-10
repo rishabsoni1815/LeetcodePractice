bda6a26525ef8d41ebc2d00cc02eb33399005267
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2026-02-10 20:42:06 +0530

    Time: 18 ms (67.52%), Space: 51.2 MB (71.98%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.java
+++ 0706-design-hashmap/0706-design-hashmap.java
@@ -1,81 +1,128 @@
-class MyHashMap
-{
-	ListNode[] nodes = new ListNode[10000];
+class MyHashMap {
+    MyHashMaps<Integer, Integer> map;
 
-	public int get(int key)
-	{
-		int index = getIndex(key);
-		ListNode prev = findElement(index, key);
-		return prev.next == null ? -1 : prev.next.val;
-	}
-	
-	public void put(int key, int value)
-	{
-		int index = getIndex(key);
-		ListNode prev = findElement(index, key);
-		
-		if (prev.next == null)
-			prev.next = new ListNode(key, value);
-		else 
-			prev.next.val = value;
-	}
+    public MyHashMap() {
+         map = new MyHashMaps<>(2);
+    }
 
-	public void remove(int key)
-	{
-		int index = getIndex(key);
-        ListNode prev = findElement(index, key);
-			
-        if(prev.next != null)
-		    prev.next = prev.next.next;
-	}
+    public void put(int key, int value) {
+        map.put(key, value);
+    }
 
-	private int getIndex(int key)
-	{	
-		return Integer.hashCode(key) % nodes.length;
-	}
+    public int get(int key) {
+        return map.get(key)==null ? -1 : map.get(key);
+    }
 
-	private ListNode findElement(int index, int key)
-	{
-		if(nodes[index] == null)
-			return nodes[index] = new ListNode(-1, -1);
-        
-        ListNode prev = nodes[index];
-		
-		while(prev.next != null && prev.next.key != key)
-		{
-			prev = prev.next;
-		}
-		return prev;
-	}
-
-	private static class ListNode
-	{
-		int key, val;
-		ListNode next;
-
-		ListNode(int key, int val)
-		{
-			this.key = key;
-			this.val = val;
-		}
-	}
+    public void remove(int key) {
+        map.remove(key);
+    }
 }
 
-       
+class MyHashMaps<K, V> {
+    double load_factor = 0.75f;
+    int curSize;
+    Entry<K, V>[] map;
 
-    
-  
-        
-    
-    
-  
-        
-   
-    
-  
-        
- 
+    public MyHashMaps(int cap) {
+        int size = getSize(cap);
+        map = new Entry[size];
+        this.curSize = 0;
+    }
 
+    int getSize(int cap) {//getting nearest power of 2 >= cap
+        int size = 1;
+        while (size < cap)
+            size <<= 1;
+        return size;
+    }
+
+    void put(K key, V value) {
+        int idx = getIndex(key);
+        Entry<K, V> entry = map[idx];
+        boolean found = false;
+        Entry prev = null;
+        while (entry != null) {
+            if (entry.key.equals(key)) {
+                entry.value = value;
+                found = true;
+                break;
+            }
+            prev = entry;
+            entry = entry.next;
+        }
+        if (!found) {
+            if (prev == null)
+                map[idx] = new Entry<>(key, value);
+            else
+                prev.next = new Entry<>(key, value);
+            curSize++;
+        }
+        if (curSize > map.length * load_factor)
+            resize();
+    }
+
+    void resize() {
+        Entry<K, V>[] oldMap = map;
+        curSize = 0;
+        map = new Entry[map.length * 2];
+        for (Entry<K, V> entry : oldMap) {
+            while (entry != null) {
+                this.put(entry.key, entry.value);
+                entry = entry.next;
+            }
+        }
+    }
+
+    V get(K key) {
+        int idx = getIndex(key);
+        Entry<K, V> entry = map[idx];
+        while (entry != null) {
+            if (entry.key.equals(key)) {
+                return entry.value;
+            }
+            entry = entry.next;
+        }
+        return null;
+    }
+
+    void remove(K key) {
+        int idx = getIndex(key);
+        Entry<K, V> entry = map[idx];
+        boolean found = false;
+        Entry prev = null;
+        while (entry != null) {
+            if (entry.key.equals(key)) {
+                Entry next = entry.next;
+                if (prev != null)
+                    prev.next = next;
+                else
+                    map[idx] = next;
+                entry = null;
+                curSize--;
+                return;
+            }
+            prev = entry;
+            entry = entry.next;
+        }
+    }
+
+    int getIndex(K key) {
+        int hash = Math.abs(key.hashCode()) % map.length;
+        return hash;
+        //            return hash&(map.length-1); & also return btw 1 to 2^n since size is power of 2
+    }
+
+    class Entry<K, V> {
+        K key;
+        V value;
+        Entry<K, V> next;
+
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+}
 
 /**
  * Your MyHashMap object will be instantiated and called as such:
