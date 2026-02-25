428315b1c154a27c57981c80d50fac76aa2498bf
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2026-02-25 18:44:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,24 +1,20 @@
 class Solution {
-    /*
-    
-    Do it in two directions.
-    The first loop makes sure children with a higher rating get more candy than its left neighbor; the second loop makes sure children with a higher rating get more candy than its right neighbor.
-    
-    */
-
     public int candy(int[] a) {
-        int n = a.length;
-        int dp[] = new int[n];
-        int sum = 0;
+        int dp[]=new int[a.length];
         Arrays.fill(dp,1);
-        for (int i = 1; i < n; i++) {
-            if(a[i]>a[i-1]) dp[i] = Math.max(dp[i], dp[i - 1] + 1);//adding only 1 since we want to minimise the no of candies
+        for(int i=1;i<a.length;i++){
+            if(a[i]>a[i-1]){
+                dp[i]=dp[i-1]+1;
+            }
         }
-        for (int i = n - 2; i >= 0; i--) {
-            if(a[i]>a[i+1]) dp[i] = Math.max(dp[i], dp[i + 1] + 1);//taking max since we want both conditions to satisfy (i's candies are >right and >left person)
+
+        for(int i=a.length-2;i>=0;i--){
+            if(a[i]>a[i+1]){
+                dp[i]=dp[i+1]+1;
+            }
         }
-        for (int i : dp)
-            sum+=i;
-        return sum;
+        int cnt=0;
+        for(int c:dp) cnt+=c;
+        return cnt;
     }
 }
