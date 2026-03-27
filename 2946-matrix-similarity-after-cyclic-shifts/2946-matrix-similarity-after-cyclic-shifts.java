86dbee7422dbb20ec35e31a583faa50eb0b0fc85
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2026-03-27 16:46:16 +0530

    Time: 1 ms (99.47%), Space: 46.7 MB (91.2%) - LeetHub

--- 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.java
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.java
@@ -1,23 +1,15 @@
 class Solution {
-    public boolean areSimilar(int[][] a, int k) {
-        int n=a.length;
-        int m=a[0].length;
-        int na[][]=new int[n][m];
-        k%=m;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-               if(i%2!=0){
-                int nj=(j+k)%m;
-                na[i][nj]=a[i][j];
-               }else{
-                int nj=(j-k+m)%m;
-                na[i][nj]=a[i][j];
-               }
-            }
-        }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(a[i][j]!=na[i][j]) return false;
+
+    public boolean areSimilar(int[][] mat, int k) {
+        int m = mat.length;
+        int n = mat[0].length;
+        k %= n;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] != mat[i][(j + k) % n]) {
+                    return false;
+                }
             }
         }
         return true;
