f79b27faa19e6860c680f0114bed9745775144ba
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2026-04-02 18:48:29 +0530

    Time: 123 ms (27.84%), Space: 46.9 MB (49.51%) - LeetHub

--- 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.java
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.java
@@ -1,42 +1,38 @@
 class Solution {
-    public int maximumDetonation(int[][] bombs) {
-        Map<Integer, List<Integer>> graph = new HashMap<>();
-        int n = bombs.length;
+    public int maximumDetonation(int[][] a) {
+        ArrayList<ArrayList<Integer>>g=new ArrayList<>();
+        int n=a.length;
+        for(int i=0;i<n;i++) g.add(new ArrayList<>());
 
-        // Build the graph
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == j) {
-                    continue;
-                }
-                int xi = bombs[i][0], yi = bombs[i][1], ri = bombs[i][2];
-                int xj = bombs[j][0], yj = bombs[j][1];
-
-                // Create a path from node i to node j, if bomb i detonates bomb j.
-                if ((long)ri * ri >= (long)(xi - xj) * (xi - xj) + (long)(yi - yj) * (yi - yj)) {
-                    graph.computeIfAbsent(i, k -> new ArrayList<>()).add(j);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(reachable(i,j,a) && i!=j){
+                    g.get(i).add(j);
                 }
             }
         }
-
-        int answer = 0;
-        for (int i = 0; i < n; i++) {
-            int count = dfs(i, new HashSet<>(), graph);
-            answer = Math.max(answer, count);
+        int max=0;
+        for(int i=0;i<n;i++){
+            HashSet<Integer>v=new HashSet<>();
+            dfs(i,g,v);
+            max=Math.max(max,v.size());
         }
-
-        return answer;
+        return max;
     }
-
-    // DFS to get the number of nodes reachable from a given node cur
-    private int dfs(int cur, Set<Integer> visited, Map<Integer, List<Integer>> graph) {
-        visited.add(cur);
-        int count = 1;
-        for (int neib : graph.getOrDefault(cur, new ArrayList<>())) {
-            if (!visited.contains(neib)) {
-                count += dfs(neib, visited, graph);
+    boolean reachable(int i,int j,int a[][]){
+        long x1=a[i][0];
+        long y1=a[i][1];
+        long x2=a[j][0];
+        long y2=a[j][1];
+        long d=(long)((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
+        return d<=((long)(a[i][2])* (a[i][2]));
+    }
+    void dfs(int i,ArrayList<ArrayList<Integer>>g,HashSet<Integer>v){
+        v.add(i);
+        for(int nei:g.get(i)){
+            if(!v.contains(nei)) {
+                dfs(nei,g,v);
             }
         }
-        return count;
     }
 }
