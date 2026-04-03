ecf7219e2bcc11609c8f85ac428c7b4514f43cd8
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2026-04-04 01:00:48 +0530

    Time: 0 ms (100%), Space: 46.5 MB (79.88%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -12,66 +12,36 @@ class Node {
     }
 }
 */
-// o(n) space (map) sol.
-// public class Solution {
-//     public Node copyRandomList(Node head) {
-//         if (head == null) {
-//             return null;
-//         }
-        
-//        Map<Node, Node> map = new HashMap<>();
 
-//         Node cur = head;
-//         while(cur != null) {
-//             map.put(cur, new Node(cur.val));
-//             cur = cur.next;
-//         }
-        
-//         for (Map.Entry<Node, Node> entry : map.entrySet()) {
-//             Node newNode = entry.getValue();
-//             newNode.next = map.get(entry.getKey().next);
-//             newNode.random = map.get(entry.getKey().random);
-//         }
-        
-//         return map.get(head);
-//     }
-// }
-
-
-///O(1)space 
 class Solution {
     public Node copyRandomList(Node head) {
         if(head==null) return null;
-        // First round: make copy of each node,
-          // and link them together side-by-side in a single list.
         Node temp=head;
         while(temp!=null){
-            Node n=temp.next;
             Node copy=new Node(temp.val);
-            copy.next=temp.next;
+            Node nNext=temp.next;
             temp.next=copy;
-            temp=n;
+            copy.next=nNext;
+            temp=nNext;
         }
-        
-          // Second round: assign random pointers for the copy nodes.
         temp=head;
+
         while(temp!=null){
-            Node copy=temp.next;
-           if(temp.random!=null) copy.random=temp.random.next;
-            temp=copy.next;
+            Node cur=temp.next;
+            Node nNext=cur.next;
+            if(temp.random!=null) cur.random=temp.random.next;
+            temp=temp.next.next;
         }
-        
-        
-          // Third round: restore the original list, and extract the copy list.
-        Node nhead=head.next;
+
         temp=head;
+        Node dhead=head.next;
         while(temp!=null){
-            Node copy=temp.next;
-            Node n=copy.next;
-            temp.next=n;
-           if(n!=null) copy.next=n.next;
-            temp=n;
+            Node cur=temp.next;
+            Node nNext=cur.next;
+            if(nNext!=null) cur.next=cur.next.next;
+            temp.next=nNext;
+            temp=nNext;
         }
-        return nhead;
+        return dhead;
     }
 }
