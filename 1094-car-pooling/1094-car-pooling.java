dd38ee266642f4e1bf28a4900a0b455b0eac8bfd
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2026-04-04 01:16:21 +0530

    Time: 1 ms (100%), Space: 46.4 MB (51.82%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.java
+++ 1094-car-pooling/1094-car-pooling.java
@@ -1,11 +1,36 @@
 class Solution {
     public boolean carPooling(int[][] trips, int capacity) {    
-      int stops[] = new int[1001]; 
-      for (int t[] : trips) {
-          stops[t[1]] += t[0];
-          stops[t[2]] -= t[0];
-      }
-      for (int i = 0; capacity >= 0 && i < 1001; ++i) capacity -= stops[i];
-      return capacity >= 0;
+        int[] locs = new int[1001];
+        int sum = 0;
+        for (int[] trip: trips) {
+            locs[trip[1]] += trip[0];
+            // locs[trip[2]+1] -= trip[0];not as on end time the passenger on board has left
+            // so if [2 1 5] and [3 5 7] then on x=5 there were 3 passenger not 5 as 2 one have left
+            locs[trip[2]] -= trip[0];
+        }
+        for (int loc : locs) {
+            sum += loc;
+            if (sum > capacity) {
+                return false;
+            }
+        }
+        return true;
     }
+    /*
+    
+    public boolean carPooling(int[][] trips, int capacity) {
+        Map<Integer, Integer> m = new TreeMap<>();
+        for (int[] t : trips) {
+            m.put(t[1], m.getOrDefault(t[1], 0) + t[0]);
+            m.put(t[2], m.getOrDefault(t[2], 0) - t[0]);
+        }
+        for (int v : m.values()) {
+            capacity -= v;
+            if (capacity < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+     */
 }
