a70c3a4a5faabd1d23903efa70bc2b38367832fd
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2026-04-24 00:34:11 +0530

    Time: 0 ms (100%), Space: 42.8 MB (58.23%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,28 +1,28 @@
 class Solution {
     
     //space optimised
-        public int rob(int[] a) {
-        int n=a.length;
-        int next=0,nextToNext=0;
-        for(int i=n-1;i>=0;i--){
-            int cur=a[i];
-            if(i+1<n) cur=Math.max(cur,next);
-            if(i+2<n) cur=Math.max(cur,a[i]+nextToNext);
-            nextToNext=next;
-            next=cur;
-        }
-        return next;
-    }
-    
-    
-    // public int rob(int[] a) {
+    //     public int rob(int[] a) {
     //     int n=a.length;
-    //     int dp[]=new int[n];
+    //     int next=0,nextToNext=0;
     //     for(int i=n-1;i>=0;i--){
-    //         dp[i]=a[i];
-    //         if(i+1<n) dp[i]=Math.max(dp[i],dp[i+1]);
-    //         if(i+2<n) dp[i]=Math.max(dp[i],a[i]+dp[i+2]);
+    //         int cur=a[i];
+    //         if(i+1<n) cur=Math.max(cur,next);
+    //         if(i+2<n) cur=Math.max(cur,a[i]+nextToNext);
+    //         nextToNext=next;
+    //         next=cur;
     //     }
-    //     return dp[0];
+    //     return next;
     // }
+    
+    
+    public int rob(int[] a) {
+        int n=a.length;
+        int dp[]=new int[n];
+        for(int i=0;i<n;i++){
+            dp[i]=a[i];
+            if(i-1>=0) dp[i]=Math.max(dp[i],dp[i-1]);
+            if(i-2>=0) dp[i]=Math.max(dp[i],a[i]+dp[i-2]);
+        }
+        return dp[n-1];
+    }
 }
