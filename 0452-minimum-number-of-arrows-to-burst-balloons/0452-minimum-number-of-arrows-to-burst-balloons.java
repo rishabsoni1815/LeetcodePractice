b892b8af7115c911a355771e73b4f379702a2fc9
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2026-04-23 11:57:05 +0530

    Time: 53 ms (49.4%), Space: 95.5 MB (81.55%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
@@ -1,16 +1,15 @@
 class Solution {
     public int findMinArrowShots(int[][] a) {
-        int n=a.length;
-        Arrays.sort(a,(x,y)->(x[0]!=y[0] ? Integer.compare(x[0],y[0]):Integer.compare(x[1],y[1])));//not using x[0]-y[0] as this can be out of bount in case of int_min so use Integer.compare as this internally does not subtracts to compare rather directly compares so does not return in overflow
-        int e=a[0][1],ans=0;
-        for(int i=0;i<n;){
-            e=a[i][1];
-            i++;
-            while(i<n && e>=a[i][0]){
-                e=Math.min(e,a[i][1]);//imp as end of next to a[i] maybe less
+        Arrays.sort(a,(x,y)->Integer.compare(x[0],y[0]));
+        int i=0,n=a.length,ans=0,end=0;
+        while(i<n){
+            ans++;
+            end=a[i][1];
+            while(i+1<n && end>=a[i+1][0]){
+                end=Math.min(end,a[i+1][1]);
                 i++;
             }
-            ans++;
+            i++;
         }
         return ans;
     }
