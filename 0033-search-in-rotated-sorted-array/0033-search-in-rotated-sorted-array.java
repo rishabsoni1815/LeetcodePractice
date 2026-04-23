f1f761f8b50ec0b22649bd08f42cf4dcf04014df
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2026-04-23 23:49:00 +0530

    Time: 0 ms (100%), Space: 44 MB (29.73%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,23 +1,21 @@
 class Solution {
     public int search(int[] a, int t) {
-        //option1 - find min in sorted rotated then search in left/right half acc to condition in which target lies
-        //optio 2- direct check if which side is sorted and if t lies in it or not then apply cases
         int n=a.length;
         int l=0,r=n-1;
         while(l<=r){
-            int m=l+(r-l)/2;
-            if(a[m]==t) return m;
-            if(a[l]<=a[m]){//left sorted
-                if(t>=a[l] && t<=a[m]){//t in left sorted half so search in it
-                    r=m-1;
-                }else{//t not lie in left sorted half
-                    l=m+1;
+            int mid=l+(r-l)/2;
+            if(a[mid]==t) return mid;
+            if(a[l]<=a[mid]){
+                if(a[l]<=t && t<=a[mid]){
+                    r=mid-1;
+                }else{
+                    l=mid+1;
                 }
-            }else{//right sorted
-                if(t>=a[m] && t<=a[r]){//t in right sorted half so search in it
-                    l=m+1;
-                }else{//t not lie in right sorted half
-                    r=m-1;
+            }else{
+                if(t<=a[r] && t>=a[mid]){
+                    l=mid+1;
+                }else{
+                    r=mid-1;
                 }
             }
         }
