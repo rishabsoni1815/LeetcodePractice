593eed47fbd177e6c4df9f80d748eaf5039e37d2
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2026-04-25 03:06:19 +0530

    Time: 31 ms (85.99%), Space: 99.1 MB (9.27%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.java
+++ 0456-132-pattern/0456-132-pattern.java
@@ -1,7 +1,7 @@
 public class Solution {
     public boolean find132pattern(int[] nums) {
         if (nums.length < 3) return false;
-        TreeSet<Integer> set = new TreeSet<>();
+        Stack<Integer> stack = new Stack<>();
         int[] min = new int[nums.length];
         min[0] = nums[0];
         for (int i = 1; i < nums.length; i++) min[i] = Math.min(
@@ -9,10 +9,12 @@ public class Solution {
             nums[i]
         );
         for (int j = nums.length - 1; j >= 0; j--) {
+            //we have min as 1 now 3 we are iterating (a[j]) from right to left now only need elements which are btw 1 and 3 so only keeping ele which are > 1 and which ever comes first ele which is greater than 1 and less than 3 that's the ans so stack will have ele in order are they appear in array from right to left but all will be >1 and when ge get any which is >1 and <3 (a[j]) we return
+
             if (nums[j] > min[j]) {
-                Integer cur=set.higher(min[j]);
-                if(cur!=null && cur<nums[j]) return true;
-                set.add(nums[j]);
+                while (!stack.isEmpty() && stack.peek() <= min[j]) stack.pop();
+                if (!stack.isEmpty() && stack.peek() < nums[j]) return true;
+                stack.push(nums[j]);
             }
         }
         return false;
