bc9003e73da233ebda0a04826c35a48b5d0328e9
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2026-04-24 15:07:43 +0530

    Time: 6 ms (91.53%), Space: 45.3 MB (63.86%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,25 +1,30 @@
-class Solution {
-    public int evalRPN(String[] a) {
-        int n=a.length;
-        //if next is operation do that with pre elemtns else new ele will be processed with operand after that
-        Stack<Integer>s=new Stack<>();
-        for(int i=0;i<n;i++){
-            String c=a[i];
-            if(isOp(c)){
-                int x=s.pop();
-                int y=s.pop();
-                if(c.equals("+")) s.add(y+x);
-                else if(c.equals("/")) s.add(y/x);
-                else if(c.equals("-")) s.add(y-x);
-                else s.add(y*x);
-            }else{
-                s.add(Integer.valueOf(c));
-            }
-        }
-        return s.pop();
-    }
-    boolean isOp(String c){
-        return c.equals("+") ||c.equals("*")||c.equals("/")||c.equals("-");
-    }
-}
+public class Solution {
+    public int evalRPN(String[] tokens) {
+        int a,b;
+                //if next is operation do that with pre elemtns else new ele will be processed with operand after that
 
+		Stack<Integer> S = new Stack<Integer>();
+		for (String s : tokens) {
+			if(s.equals("+")) {
+				S.add(S.pop()+S.pop());
+			}
+			else if(s.equals("/")) {
+				b = S.pop();
+				a = S.pop();
+				S.add(a / b);
+			}
+			else if(s.equals("*")) {
+				S.add(S.pop() * S.pop());
+			}
+			else if(s.equals("-")) {
+				b = S.pop();
+				a = S.pop();
+				S.add(a - b);
+			}
+			else {
+				S.add(Integer.parseInt(s));
+			}
+		}	
+		return S.pop();
+	}
+}
