32cabd199d3f9075ee88aafb134089c8d02c94e2
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2026-04-25 02:37:32 +0530

    Time: 214 ms (45.81%), Space: 43.1 MB (53.87%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.java
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.java
@@ -9,13 +9,13 @@ class Solution {
         int sum = Arrays.stream(a).sum();//to remove tle finding sum of array
         if(sum%k!=0) return false;
         boolean v[]=new boolean[n];
-        return help(0,0,a,k,v,sum/k);
+        return help(0,a[0],a,k,v,sum/k);
     }
     boolean help(int i,int sum,int a[],int k,boolean v[],int t){
         if(k==0 && sum==0) return true;
         if(sum>t) return false;
         if(sum==t) return help(0,0,a,k-1,v,t);
-        for(int j=i;j<a.length;j++){
+        for(int j=i+1;j<a.length;j++){
             if(v[j]==false){
                 v[j]=true;
                 if(help(j,sum+a[j],a,k,v,t)) return true;
