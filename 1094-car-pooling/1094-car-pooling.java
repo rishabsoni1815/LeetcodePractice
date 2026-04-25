f1f76acd0b54440bb8256a29ef8f3b60c8e43b55
Rishab Soni <65413147+rishabsoni1815@users.noreply.github.com>
2026-04-26 01:02:00 +0530

    Time: 24 ms (5.17%), Space: 46.8 MB (5.9%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.java
+++ 1094-car-pooling/1094-car-pooling.java
@@ -1,36 +1,18 @@
 class Solution {
-    public boolean carPooling(int[][] trips, int capacity) {    
-        int[] locs = new int[1001];
-        int sum = 0;
-        for (int[] trip: trips) {
-            locs[trip[1]] += trip[0];
-            // locs[trip[2]+1] -= trip[0];not as on end time the passenger on board has left
-            // so if [2 1 5] and [3 5 7] then on x=5 there were 3 passenger not 5 as 2 one have left
-            locs[trip[2]] -= trip[0];
-        }
-        for (int loc : locs) {
-            sum += loc;
-            if (sum > capacity) {
-                return false;
+    public boolean carPooling(int[][] t, int c) {
+        PriorityQueue<int []>q=new PriorityQueue<>((x,y)->(x[0]-y[0]));//0->end, 1->cap
+        Arrays.sort(t,(x,y)->(x[1]-y[1]));
+        int dis=0,cap=0;
+        for(int i=0;i<t.length;i++){
+            dis=Math.max(dis,t[i][1]);
+            System.out.println(dis);
+            while(q.size()>0 && q.peek()[0]<=dis){
+                cap-=q.poll()[1];
             }
+            cap+=t[i][0];
+            if(cap>c) return false;
+            q.add(new int[]{t[i][2],t[i][0]});
         }
         return true;
     }
-    /*
-    
-    public boolean carPooling(int[][] trips, int capacity) {
-        Map<Integer, Integer> m = new TreeMap<>();//in iteration can iterate like array
-        for (int[] t : trips) {
-            m.put(t[1], m.getOrDefault(t[1], 0) + t[0]);
-            m.put(t[2], m.getOrDefault(t[2], 0) - t[0]);
-        }
-        for (int v : m.values()) {
-            capacity -= v;
-            if (capacity < 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-     */
 }
